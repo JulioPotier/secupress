fbcb52619c5ca890ea138ef42fe1a8e8dba55387
Screenfeed <git@screenfeed.fr>
2015-09-09 01:00:29 +0200

    Introduce login errors disclose scan class.

---
+++ inc/classes/class-secupress-scan-login-errors-disclose.php
@@ -0,0 +1,71 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+/**
+ * Login Errors Disclose scan class.
+ *
+ * @package SecuPress
+ * @subpackage SecuPress_Scan
+ * @since 1.0
+ */
+
+class SecuPress_Scan_Login_Errors_Disclose extends SecuPress_Scan {
+
+	const VERSION = '1.0';
+
+	protected static $name = 'login_errors_disclose';
+	public    static $prio = 'low';
+
+
+	public function __construct() {
+		if ( self::$instance ) {
+			return self::$instance;
+		}
+
+		self::$type  = 'WordPress';
+		self::$title = __( 'Check if your WordPress site discloses some login errors.', 'secupress' );
+		self::$more  = __( 'Don\'t let them easily find these informations.', 'secupress' ); ////
+	}
+
+
+	public static function get_messages( $id = null ) {
+		$messages = array(
+			// good
+			0   => __( 'You are currently not displaying <strong>Login errors</strong>.', 'secupress' ),
+			// bad
+			200 => __( '<strong>Login errors</strong> shouldn\'t be displayed.', 'secupress' ),
+			// cantfix
+			300 => __( 'I can not fix this, you have to do it yourself, have fun.', 'secupress' ),
+		);
+
+		if ( isset( $id ) ) {
+			return isset( $messages[ $id ] ) ? $messages[ $id ] : __( 'Unknown message', 'secupress' );
+		}
+
+		return $messages;
+	}
+
+
+	public function scan() {
+
+		$check = apply_filters( 'login_errors', 'errors' );
+
+		if ( 'errors' === $check ) {
+			// bad
+			$this->add_message( 200 );
+		} else {
+			// good
+			$this->add_message( 0 );
+		}
+
+		return parent::scan();
+	}
+
+
+	public function fix() {
+
+		// include the fix here.
+
+		return parent::fix();
+	}
+}
