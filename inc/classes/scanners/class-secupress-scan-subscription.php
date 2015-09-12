e5aec5cf278965ccff237e0c6c3483480e611b82
Screenfeed <git@screenfeed.fr>
2015-09-12 02:54:19 +0200

    Introduce Subscription scan class.

---
+++ inc/classes/scanners/class-secupress-scan-subscription.php
@@ -0,0 +1,104 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+/**
+ * Subscription scan class.
+ *
+ * @package SecuPress
+ * @subpackage SecuPress_Scan
+ * @since 1.0
+ */
+
+class SecuPress_Scan_Subscription extends SecuPress_Scan implements iSecuPress_Scan {
+
+	const VERSION = '1.0';
+
+	/**
+	 * @var Singleton The reference to *Singleton* instance of this class
+	 */
+	protected static $_instance;
+	protected static $name = 'subscription';
+	public    static $prio = 'high';
+
+
+	protected static function init() {
+		self::$type  = 'WordPress';
+		self::$title = __( 'Check if the subscription setting is correctly set.', 'secupress' );
+		self::$more  = __( '???', 'secupress' );
+	}
+
+
+	public static function get_messages( $message_id = null ) {
+		$messages = array(
+			// good
+			0   => __( 'The subscription is correctly set on your site.', 'secupress' ),
+			// warning
+			100 => __( 'Unable to determine status of your homepage.', 'secupress' ),
+			// bad
+			200 => __( 'The default role in your installation is <strong>%s</strong> and it should be <strong>Subscriber</strong>, or registrations should be <strong>closed</strong>.', 'secupress' ),
+			201 => __( 'The registration page is <strong>not protected</strong> from bots.', 'secupress' ),
+			// cantfix
+			300 => __( 'I can not fix this, you have to do it yourself, have fun.', 'secupress' ),
+		);
+
+		if ( isset( $message_id ) ) {
+			return isset( $messages[ $message_id ] ) ? $messages[ $message_id ] : __( 'Unknown message', 'secupress' );
+		}
+
+		return $messages;
+	}
+
+
+	public function scan() {
+		global $wp_roles;
+
+		// Open subscriptions
+		if ( get_option( 'users_can_register' ) ) {
+
+			// Default role
+			$role = get_option( 'default_role' );
+
+			if ( 'subscriber' !== $role ) {
+				// bad
+				$role = isset( $wp_roles->role_names[ $role ] ) ? translate_user_role( $wp_roles->role_names[ $role ] ) : __( 'None' );
+				$this->add_message( 200, array( $role ) );
+			}
+
+			// Bots
+			$user_login = 'secupress_' . time();
+			$response   = wp_remote_post( wp_registration_url(), array(
+				'body' => array(
+					'user_login' => $user_login,
+					'user_email' => 'secupress_no_mail@fakemail.' . time(),
+				),
+			) );
+
+			if ( ! is_wp_error( $response ) ) {
+
+				if ( username_exists( $user_login ) || 'failed' === get_transient( 'secupress_registration_test' ) ) {
+					// bad
+					$this->add_message( 201 );
+				}
+
+			} else {
+				// warning
+				$this->add_message( 100 );
+			}
+
+			delete_transient( 'secupress_registration_test' );
+		}
+
+		// good
+		$this->maybe_set_status( 0 );
+
+		return parent::scan();
+	}
+
+
+	public function fix() {
+
+		// include the fix here.
+
+		return parent::fix();
+	}
+}
