9fbc3a588530bc4d9d2210c4aae3cbd0b7984ab3
Screenfeed <git@screenfeed.fr>
2015-09-14 15:59:00 +0200

    Introduce Directory Listing scan class.

---
+++ inc/classes/scanners/class-secupress-scan-directory-listing.php
@@ -0,0 +1,81 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+/**
+ * Directory Listing scan class.
+ *
+ * @package SecuPress
+ * @subpackage SecuPress_Scan
+ * @since 1.0
+ */
+
+class SecuPress_Scan_Directory_Listing extends SecuPress_Scan implements iSecuPress_Scan {
+
+	const VERSION = '1.0';
+
+	/**
+	 * @var Singleton The reference to *Singleton* instance of this class
+	 */
+	protected static $_instance;
+	protected static $name = 'directory_listing';
+	public    static $prio = 'high';
+
+
+	protected static function init() {
+		self::$type  = 'WordPress';
+		self::$title = __( 'Check if your WordPress site discloses files in directory (known as Directory Listing).', 'secupress' );
+		self::$more  = __( 'Don\'t let them easily find these informations.', 'secupress' );
+	}
+
+
+	public static function get_messages( $message_id = null ) {
+		$messages = array(
+			// good
+			0   => __( 'Your site doesn\'t reveal the the file list.', 'secupress' ),
+			// warning
+			100 => __( 'Unable to determine status of <code>%s</code>.', 'secupress' ),
+			// bad
+			200 => __( '<code>%s</code> shouldn\'t be accessible by anyone.', 'secupress' ),
+			// cantfix
+			300 => __( 'I can not fix this, you have to do it yourself, have fun.', 'secupress' ),
+		);
+
+		if ( isset( $message_id ) ) {
+			return isset( $messages[ $message_id ] ) ? $messages[ $message_id ] : __( 'Unknown message', 'secupress' );
+		}
+
+		return $messages;
+	}
+
+
+	public function scan() {
+		$upload_dir = wp_upload_dir();
+		$base_url   = $upload_dir['baseurl'];
+		$response   = wp_remote_get( $base_url, array( 'redirection' => 0 ) );
+
+		if ( ! is_wp_error( $response ) ) {
+
+			if ( 200 === wp_remote_retrieve_response_code( $response ) ) {
+				// bad
+				$this->add_message( 200, array( $base_url ) );
+			}
+
+		} else {
+			// warning
+			$this->add_message( 100, array( $base_url ) );
+		}
+
+		// good
+		$this->maybe_set_status( 0 );
+
+		return parent::scan();
+	}
+
+
+	public function fix() {
+
+		// include the fix here.
+
+		return parent::fix();
+	}
+}
