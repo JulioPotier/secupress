064ce82c437b69151f3e1e24684feeec311e01ab
Julio Potier <juliobosk@gmail.com>
2015-09-22 11:25:05 +0200

    Remove White Label module content.
    + add a hook for the premium version to add the real fields.

--- inc/admin/modules/white-label.php
+++ inc/admin/modules/white-label.php
@@ -3,110 +3,36 @@ defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 add_settings_section( 'secupress_display_white_label', __( 'White Label', 'secupress' ), '__secupress_whitelabel_info', 'secupress_white-label' );
 
-add_settings_field(
-	'secupress_plugin_name',
-	__( 'Plugin Name', 'secupress' ),
-	'secupress_field',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
-		array(
-			'type'         => 'text',
-			'label'        => '',
-			'default'      => SECUPRESS_PLUGIN_NAME,
-			'label_for'    => 'plugin_name',
-			'label_screen' => __( 'Plugin Name', 'secupress' ),
-		),
-	)
-);
+/**
+ * Used by premium version to add the right fields. Developpers: don't use it ;)
+ *
+ * @since 1.0
+ */
+do_action( 'premium.white-label-module' );
 
-add_settings_field(
-	'secupress_plugin_uri',
-	__( 'Plugin URI', 'secupress' ),
-	'secupress_field',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
-		array(
-			'type'         => 'text',
-			'label'        => '',
-			'default'      => 'http://secupress.me',
-			'label_for'    => 'plugin_uri',
-			'label_screen' => __( 'Plugin UTI', 'secupress' ),
-		),
-	)
-);
+if ( ! has_action( 'premium.white-label-module' ) ) {
 
-add_settings_field(
-	'secupress_plugin_desc',
-	__( 'Description', 'secupress' ),
-	'secupress_field',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
+	add_settings_field(
+		'secupress_need_premium',
+		__( 'Premium Upgrade', 'secupress' ),
+		'secupress_button',
+		'secupress_white-label',
+		'secupress_display_white_label',
 		array(
-			'type'         => 'textarea',
-			'label'        => '',
-			'default'      => 'The best and easier way to protect all your websites.',
-			'label_for'    => 'plugin_desc',
-			'label_screen' => __( 'Description', 'secupress' ),
-		),
-	)
-);
+			'button' =>
+			array(
+				'button_label' => __( 'Premium Upgrade', 'secupress' ),
+				'url'   => '#', ////
+			),
+			'helper_help' =>
+			array(
+				'name'			=> 'need_premium',
+				'description'	=> __( 'This feature is only available in the <b>Premium Version</b>.', 'secupress' )
+			),
+		)
+	);
 
-add_settings_field(
-	'secupress_plugin_author',
-	__( 'Author', 'secupress' ),
-	'secupress_field',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
-		array(
-			'type'         => 'text',
-			'label'        => '',
-			'default'      => 'SecuPress',
-			'label_for'    => 'plugin_author',
-			'label_screen' => __( 'Author', 'secupress' ),
-		),
-	)
-);
-
-add_settings_field(
-	'secupress_plugin_author_uri',
-	__( 'Author URI', 'secupress' ),
-	'secupress_field',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
-		array(
-			'type'         => 'text',
-			'label'        => '',
-			'default'      => 'http://secupress.me',
-			'label_for'    => 'plugin_author_uri',
-			'label_screen' => __( 'Author URI', 'secupress' ),
-		),
-	)
-);
-
-add_settings_field(
-	'secupress_plugin_reset',
-	'',
-	'secupress_button',
-	'secupress_white-label',
-	'secupress_display_white_label',
-	array(
-		'button' =>
-		array(
-			'button_label' => __( 'Reset White Label values to default', 'secupress' ),
-			'url'   => wp_nonce_url( admin_url( 'admin-post.php?action=secupress_resetwl' ), 'secupress_resetwl' ), //// todo secupress_resetwl admin-post
-		),
-		'helper_warning' =>
-		array(
-			'name'			=> 'reset_default',
-			'description'	=> __( ' If you change anything, the tutorial + FAQ + Support tabs will be hidden.', 'secupress' )
-		),
-	)
-);
+}
 
 function __secupress_whitelabel_info() {
 	_e( 'You can change the name of the plugin, this will be shown on the plugins page, only when activated.', 'secupress' );
