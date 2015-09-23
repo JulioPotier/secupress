4d37a798601b71076e39dc1b8de49e95e0cc98b4
Julio Potier <juliobosk@gmail.com>
2015-09-23 16:45:30 +0200

    Forgot this file 2 commit before

--- inc/modules/sensitive-data/callbacks.php
+++ inc/modules/sensitive-data/callbacks.php
@@ -3,7 +3,7 @@ defined( 'ABSPATH' ) or	die( 'Cheatin&#8217; uh?' );
 
 add_action( 'admin_init', 'secupress_register_sensitive_data_settings' );
 function secupress_register_sensitive_data_settings() {
-	register_setting( "secupress_sensitive_data_settings", "secupress_sensitive_data_settings", "__secupress_sensitive_data_settings_callback" );
+	register_setting( "secupress_sensitive-data_settings", "secupress_sensitive-data_settings", "__secupress_sensitive_data_settings_callback" );
 }
 
 /**
@@ -13,24 +13,24 @@ function secupress_register_sensitive_data_settings() {
  * @since 1.0
  */
 function __secupress_sensitive_data_settings_callback( $settings ) {
-	$modulenow = 'sensitive_data';
+	$modulenow = 'sensitive-data';
 
 	if ( ! isset( $settings['profile_protect_page_protect'] ) && ! isset( $settings['settings_protect_page_protect'] ) ) {
 		secupress_deactivate_submodule( $modulenow, 'page_protect' );
 	}
 
 	if ( isset( $settings['profile_protect_page_protect'] ) ) {
-		secupress_activate_submodule( $modulenow, 'page_protect' );
-		secupress_activate_submodule( $modulenow, 'profile_protect' );
+		secupress_activate_submodule( $modulenow, 'page-protect' );
+		secupress_activate_submodule( $modulenow, 'profile-protect' );
 	} else {
-		secupress_deactivate_submodule( $modulenow, 'profile_protect' );
+		secupress_deactivate_submodule( $modulenow, 'profile-protect' );
 	}
 
 	if ( isset( $settings['settings_protect_page_protect'] ) ) {
-		secupress_activate_submodule( $modulenow, 'page_protect' );
-		secupress_activate_submodule( $modulenow, 'options_protect' );
+		secupress_activate_submodule( $modulenow, 'page-protect' );
+		secupress_activate_submodule( $modulenow, 'options-protect' );
 	} else {
-		secupress_deactivate_submodule( $modulenow, 'options_protect' );
+		secupress_deactivate_submodule( $modulenow, 'options-protect' );
 	}
 
 	return $settings;
