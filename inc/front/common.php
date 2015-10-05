6f354648dc0ac6f9bd93378965f1547af586d59d
Screenfeed <git@screenfeed.fr>
2015-10-05 20:55:20 +0200

    In `secupress_rename_admin_username_logout()`, set a good scan status after fixing the test.

--- inc/front/common.php
+++ inc/front/common.php
@@ -5,7 +5,7 @@ add_action( 'plugins_loaded', 'secupress_check_ban_ips' );
 /**
  * Will add or remove banned IPs
  *
- * @since 1.0 
+ * @since 1.0
  * @return void
  **/
 function secupress_check_ban_ips() {
@@ -48,30 +48,41 @@ add_action( 'plugins_loaded', 'secupress_rename_admin_username_logout', 50 );
 /**
  * Will rename the "admin" account after the rename-admin-username manual fix
  *
- * @since 1.0 
+ * @since 1.0
  * @return void
  **/
 function secupress_rename_admin_username_logout() {
-	global $current_user, $pagenow;
-	$current_user = wp_get_current_user();
+	global $current_user, $pagenow, $wpdb;
 
+	$current_user    = wp_get_current_user();
 	$current_user_ID = $current_user->ID;
-	if ( empty( $_POST ) && ( ! isset( $pagenow ) || 'admin-post.php' != $pagenow ) && ! defined( 'DOING_AJAX' ) && ! defined( 'DOING_AUTOSAVE' ) && ! defined( 'DOING_CRON' ) && 
+
+	if ( empty( $_POST ) && ( ! isset( $pagenow ) || 'admin-post.php' != $pagenow ) && ! defined( 'DOING_AJAX' ) && ! defined( 'DOING_AUTOSAVE' ) && ! defined( 'DOING_CRON' ) &&
 		is_user_logged_in() && $data = get_transient( 'secupress-rename-admin-username' )
 	) {
 		delete_transient( 'secupress-rename-admin-username' );
+
 		if ( ! is_array( $data ) || ! isset( $data['ID'], $data['username'] ) || $current_user->ID != $data['ID'] || 'admin' != $current_user->user_login ) {
 			return;
 		}
-		global $wpdb;
+
 		$wpdb->update( $wpdb->users, array( 'user_login' => $data['username'] ), array( 'user_login' => 'admin' ) );
 		// Current user auth cookie is now invalid, log in again is mandatory
-		
+
 		wp_clear_auth_cookie();
 		wp_destroy_current_session();
 
 		$token = md5( time() );
 		set_transient( 'secupress_auto_login_' . $token, $data['username'] );
+
+		// Store a good scan result.
+		set_transient( 'secupress_scan_admin_user', array(
+			'msgs'   => array(
+				0 => array( '<em>admin</em>' ),
+			),
+			'status' => 'good',
+		) );
+
 		wp_safe_redirect( add_query_arg( 'secupress_auto_login_token', $token, secupress_get_current_url( 'raw' ) ) );
 		die();
 	}
@@ -81,7 +92,7 @@ add_action( 'plugins_loaded', 'secupress_rename_admin_username_login', 60 );
 /**
  * Will rename the "admin" account after the rename-admin-username manual fix
  *
- * @since 1.0 
+ * @since 1.0
  * @return void
  **/
 function secupress_rename_admin_username_login() {
@@ -93,7 +104,7 @@ function secupress_rename_admin_username_login() {
 			add_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
 			$user = wp_signon( array( 'user_login' => $username ) );
 			remove_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
-			
+
 			if ( is_a( $user, 'WP_User' ) ) {
 				wp_set_current_user( $user->ID, $user->user_login );
 				wp_set_auth_cookie( $user->ID );
