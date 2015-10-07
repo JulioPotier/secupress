487061e946dd950737d4373e6633d26b78e060c6
Screenfeed <git@screenfeed.fr>
2015-10-08 01:38:46 +0200

    Use a hash instead of simply `42.0` for the styles/scripts version.

--- inc/modules/discloses/plugins/wp-version-css.php
+++ inc/modules/discloses/plugins/wp-version-css.php
@@ -15,6 +15,11 @@ add_filter( 'style_loader_src', 'secupress_replace_wp_version_in_src', PHP_INT_M
 
 if ( ! function_exists( 'secupress_replace_wp_version_in_src' ) ) :
 	function secupress_replace_wp_version_in_src( $src ) {
-		return str_replace( 'ver=' . get_bloginfo( 'version' ), 'ver=42.0', $src );
+		static $hash;
+
+		$ver  = get_bloginfo( 'version' );
+		$hash = isset( $hash ) ? $hash : substr( md5( wp_salt( 'nonce' ) . $ver ), 2, 6 );
+
+		return str_replace( 'ver=' . $ver, 'ver=' . $hash, $src );
 	}
 endif;
