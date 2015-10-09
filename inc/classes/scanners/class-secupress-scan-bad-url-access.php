d6004e486250b348c480d114f6cb78aa19c8daad
Screenfeed <git@screenfeed.fr>
2015-10-09 15:12:27 +0200

    `SecuPress_Scan_Bad_URL_Access`: added 2 more URLs to test during scan, created the `fix()`.

--- inc/classes/scanners/class-secupress-scan-bad-url-access.php
+++ inc/classes/scanners/class-secupress-scan-bad-url-access.php
@@ -31,12 +31,16 @@ class SecuPress_Scan_Bad_URL_Access extends SecuPress_Scan implements iSecuPress
 		$messages = array(
 			// good
 			0   => __( 'Your site does not reveal sensitive informations.', 'secupress' ),
+			1   => sprintf( __( 'Your %s file has been successfully edited.', 'secupress' ), '<code>.htaccess</code>' ),
 			// warning
 			100 => __( 'Unable to determine status of %s.', 'secupress' ),
 			// bad
 			200 => _n_noop( '%s should not be accessible by anyone.', '%s should not be accessible by anyone.', 'secupress' ),
 			// cantfix
-			300 => __( 'I can not fix this, you have to do it yourself, have fun.', 'secupress' ),
+			300 => sprintf( __( 'You run a nginx system, I cannot fix these sensitive informations disclosures but you can do it yourself with the following code: %s.', 'secupress' ), '<code>(add nginx code here)</code>' ), ////
+			301 => sprintf( __( 'You run an IIS7 system, I cannot fix these sensitive informations disclosures but you can do it yourself with the following code: %s.', 'secupress' ), '<code>(add IIS code here)</code>' ), //// iis7_url_rewrite_rules ?
+			302 => __( 'You don\'t run an Apache system, I cannot fix these sensitive informations disclosures.', 'secupress' ),
+			303 => __( 'Your %1$s file is not writable. Please add the following lines to the file: %2$s.', 'secupress' ),
 		);
 
 		if ( isset( $message_id ) ) {
@@ -57,7 +61,9 @@ class SecuPress_Scan_Bad_URL_Access extends SecuPress_Scan implements iSecuPress
 		$urls = array(
 			home_url( 'php.ini' ),
 			admin_url( 'install.php' ),
+			admin_url( 'menu.php' ),
 			admin_url( 'menu-header.php' ),
+			admin_url( 'setup-config.php' ),	//// wp_die(), donc erreur 500 (et non 200). De plus, je ne vois pas d'info sensible. Sachant que c'est probablement toi qui m'avais indiqué de bloquer l'accès à ce fichier, tu te souviens pourquoi ?
 			admin_url( 'includes/menu.php' ),
 		);
 		$bads     = array();
@@ -97,8 +103,50 @@ class SecuPress_Scan_Bad_URL_Access extends SecuPress_Scan implements iSecuPress
 
 
 	public function fix() {
+		global $is_apache, $is_nginx, $is_iis7;
 
-		// include the fix here.
+		// Not Apache system, bail out.
+		if ( ! $is_apache ) {
+
+			if ( $is_nginx ) {
+				$this->add_fix_message( 300 );
+			} elseif ( $is_iis7 ) {
+				$this->add_fix_message( 301 ); //// iis7_url_rewrite_rules
+			} else {
+				$this->add_fix_message( 302 );
+			}
+
+			return parent::fix();
+		}
+
+		// Edit `.htaccess` file.
+		$base = parse_url( trailingslashit( get_option( 'home' ) ), PHP_URL_PATH );
+
+		// Trigger a 404 error, because forbidding access to a file is nice, but making it also invisible is more fun :)
+		$rules  = "<IfModule mod_rewrite.c>\n";
+		$rules .= "    RewriteEngine On\n";
+		$rules .= "    RewriteBase $base\n";
+		$rules .= "    RewriteRule php.ini$ [R=404,L]\n";
+		$rules .= "    RewriteRule wp-admin/install.php$ [R=404,L]\n";
+		$rules .= "    RewriteRule wp-admin/menu.php$ [R=404,L]\n";
+		$rules .= "    RewriteRule wp-admin/menu-header.php$ [R=404,L]\n";
+		$rules .= "    RewriteRule wp-admin/setup-config.php$ [R=404,L]\n";
+		$rules .= "    RewriteRule wp-admin/includes/menu.php$ [R=404,L]\n";
+		$rules .= "</IfModule>\n";
+		// But if rewrite is disabled, forbid access.
+		$rules .= "<IfModule !mod_rewrite.c>\n";
+		$rules .= "<FilesMatch \"^(php\.ini|install\.php|menu\.php|menu-header\.php\setup-config\.php)$\">\n";
+		$rules .= "    deny from all\n";
+		$rules .= "</FilesMatch>\n";
+		$rules .= "</IfModule>";
+
+		if ( secupress_write_htaccess( 'bad_url_access', $rules ) ) {
+			// good
+			$this->add_fix_message( 1, array( '<code>.htaccess</code>' ) );
+		} else {
+			// cantfix
+			$this->add_fix_message( 303, array( '<code>.htaccess</code>', "<pre># BEGIN SecuPress bad_url_access\n$rules\n# END SecuPress</pre>" ) );
+		}
 
 		return parent::fix();
 	}
