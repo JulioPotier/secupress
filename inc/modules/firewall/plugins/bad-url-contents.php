ef0e3297055806e2b18dcc28378547466b9e2ba1
Julio Potier <juliobosk@gmail.com>
2015-10-12 23:35:54 +0200

    Fatal bazooka

--- inc/modules/firewall/plugins/bad-url-contents.php
+++ inc/modules/firewall/plugins/bad-url-contents.php
@@ -17,7 +17,7 @@ add_action( 'secupress_plugins_loaded', 'secupress_block_bad_url_contents', 0 );
 function secupress_block_bad_url_contents() {
 
 	$bad_url_contents = secupress_get_module_option( 'bbq-url-content_bad-contents-list', '', 'firewall' );
-	$bad_url_contents = str_replace( array( ' , ', ', ', ' ,', ',' ), '|', preg_quote( $bad_url_contents, '/' ) );
+	$bad_url_contents = preg_replace( '/\s*,\s*/', '|', preg_quote( $bad_url_contents, '/' ) );
 	$request_uri      = isset( $_SERVER['QUERY_STRING'] ) ? $_SERVER['QUERY_STRING'] : '';
 
 	if ( ! $bad_url_contents || 
