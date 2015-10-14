e43509122b253f0fc41db451139b96b30e3528f7
Screenfeed <git@screenfeed.fr>
2015-10-14 23:09:17 +0200

    Fix "Block Bad User-Agents" module: don't block if empty option.

--- inc/modules/firewall/plugins/user-agents-header.php
+++ inc/modules/firewall/plugins/user-agents-header.php
@@ -14,25 +14,28 @@ add_action( 'secupress_plugins_loaded', 'secupress_block_bad_user_agents', 0 );
  * @return void
  **/
 function secupress_block_bad_user_agents() {
-
-	$bad_user_agents = secupress_get_module_option( 'bbq-headers_user-agents-list', '', 'firewall' );
+	$bad_user_agents = trim( secupress_get_module_option( 'bbq-headers_user-agents-list', '', 'firewall' ) );
 	$bad_user_agents = preg_replace( '/\s*,\s*/', '|', preg_quote( $bad_user_agents, '/' ) );
-	$user_agent = isset( $_SERVER['HTTP_USER_AGENT'] ) ? trim( $_SERVER['HTTP_USER_AGENT'] ) : '';
-	
-	if ( preg_match( '/' . $bad_user_agents . '/i', $user_agent ) ) {
-		secupress_block( 'UAHB' );
+	$user_agent      = isset( $_SERVER['HTTP_USER_AGENT'] ) ? trim( $_SERVER['HTTP_USER_AGENT'] ) : '';
+
+	if ( empty( $bad_user_agents ) ) {
+		return;
+	}
+
+	if ( ! $user_agent ) {
+		secupress_block( 'UAHE' );
 	}
 
 	if ( strlen( $user_agent ) > 255 ) {
 		secupress_block( 'UAHL' );
 	}
 
-	if ( wp_strip_all_tags( $user_agent, true ) !== $user_agent ) {
-		secupress_block( 'UAHT' );
+	if ( preg_match( '/' . $bad_user_agents . '/i', $user_agent ) ) {
+		secupress_block( 'UAHB' );
 	}
 
-	if ( ! $user_agent ) {
-		secupress_block( 'UAHE' );
+	if ( wp_strip_all_tags( $user_agent, true ) !== $user_agent ) {
+		secupress_block( 'UAHT' );
 	}
 
 }
