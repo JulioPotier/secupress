da304a9a8aae7c88237c384bc74c8799c4029d65
Screenfeed <git@screenfeed.fr>
2015-11-19 17:45:54 +0100

    Admin notices: use of the new class + some improvements. Still many things to do.

--- inc/admin/notices.php
+++ inc/admin/notices.php
@@ -2,260 +2,233 @@
 defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
 
 /**
- * This warnings are displayed when the plugin can not be deactivated correctly
+ * This warning is displayed when the plugin can not be deactivated correctly.
  *
  * @since 1.0
  */
-add_action( 'admin_notices', 'secupress_bad_deactivations' );
+add_action( 'admin_init', 'secupress_bad_deactivations' );
 
 function secupress_bad_deactivations() {
-	global $current_user;
+	global $status, $page, $s;
+	$current_user_id = get_current_user_id();
 
-	if ( ! current_user_can( secupress_get_capability() ) || ! ( $msgs = get_transient( $current_user->ID . '_donotdeactivatesecupress' ) ) ) {
+	if ( ! current_user_can( secupress_get_capability() ) || ! ( $msgs = secupress_get_site_transient( $current_user_id . '_donotdeactivatesecupress' ) ) ) {
 		return;
 	}
 
-	delete_transient( $current_user->ID . '_donotdeactivatesecupress' );
+	secupress_delete_site_transient( $current_user_id . '_donotdeactivatesecupress' );
+
 	$errors = array();
-	?>
 
-	<div class="error">
-		<?php
-		foreach ( $msgs as $msg ) {
-
-			switch ( $msg ) {
-
-				case 'htaccess' :
-					$errors['htaccess'] = '<p>' . sprintf( __( '<strong>%s</strong> can not be deactivated because of <code>%s</code>.', 'rocket' ), SECUPRESS_PLUGIN_NAME, '.htaccess' ) . '<br>' . __( 'This file is not writable and we can not remove these directives.', 'rocket' ) . ' ' . sprintf( __( 'Maybe we do not have writing permissions for <code>%s</code>.', 'rocket' ), '.htaccess' ) . '<br>' . __( 'Please give us permissions or resolve the problem yourself. Then retry deactivation.', 'rocket' ) . '</p>';
-					break;
-			}
-
-			/**
-			  * Filter the output messages for each bad deactivation attempt.
-			  *
-			  * @since 2.0.0
-			  *
-			  * @param array $errors Contains the error messages to be filtered
-			  * @param string $msg Contains the error type (wpconfig or htaccess)
-			 */
-			$errors = apply_filters( 'secupress_bad_deactivations', $errors, $msg );
-
+	foreach ( $msgs as $msg ) {
+		switch ( $msg ) {
+			case 'htaccess' :
+				$errors['htaccess']  = sprintf( __( '%1$s can not be deactivated because of %2$s.', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>', '<code>.htaccess</code>' ) . '<br>';
+				$errors['htaccess'] .= sprintf( __( 'This file is not writable and we can not remove these directives. Maybe we do not have writing permissions for %s.', 'secupress' ), '<code>.htaccess</code>' ) . '<br>';
+				$errors['htaccess'] .= __( 'Please give us permissions or resolve the problem yourself. Then retry deactivation.', 'secupress' );
+				break;
+			case 'webconfig' :
+				$errors['webconfig']  = sprintf( __( '%1$s can not be deactivated because of %2$s.', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>', '<code>web.config</code>' ) . '<br>';
+				$errors['webconfig'] .= sprintf( __( 'This file is not writable and we can not remove these directives. Maybe we do not have writing permissions for %s.', 'secupress' ), '<code>web.config</code>' ) . '<br>';
+				$errors['webconfig'] .= __( 'Please give us permissions or resolve the problem yourself. Then retry deactivation.', 'secupress' );
+				break;
+			case 'wp-config' :
+				$errors['wp-config']  = sprintf( __( '%1$s can not be deactivated because of %2$s.', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>', '<code>wp-config.php</code>' ) . '<br>';
+				$errors['wp-config'] .= sprintf( __( 'This file is not writable and we can not remove these directives. Maybe we do not have writing permissions for %s.', 'secupress' ), '<code>wp-config.php</code>' ) . '<br>';
+				$errors['wp-config'] .= __( 'Please give us permissions or resolve the problem yourself. Then retry deactivation.', 'secupress' );
+				break;
 		}
+	}
 
-		// Display errors
-		if ( count( $errors ) ) {
-			array_map( 'printf', $errors );
+	/**
+	  * Filter the output messages for each bad deactivation attempt.
+	  *
+	  * @since 2.0.0
+	  *
+	  * @param array $errors Contains the error messages to be filtered
+	  * @param string $msg Contains the error type (wpconfig or htaccess)
+	 */
+	$errors = apply_filters( 'secupress_bad_deactivations', $errors, $msgs );
+
+	if ( $errors ) {
+		foreach ( $errors as $nessage ) {
+			secupress_add_notice( $message, 'error' );
 		}
+	}
 
-		/**
-		  * Allow a "force deactivation" link to be printed, use at your own risks
-		  *
-		  * @since 2.0.0
-		  *
-		  * @param bool true will print the link
-		 */
-		$permit_force_deactivation = apply_filters( 'secupress_permit_force_deactivation', true );
+	/**
+	  * Allow a "force deactivation" link to be printed, use at your own risks
+	  *
+	  * @since 2.0.0
+	  *
+	  * @param bool true will print the link
+	 */
+	$permit_force_deactivation = apply_filters( 'secupress_permit_force_deactivation', true );
 
-		// We add a link to permit "force deactivation", use at your own risks.
-		if ( $permit_force_deactivation ) {
-			global $status, $page, $s;
-			$plugin_file = SECUPRESS_PLUGIN_FILE;
-			$secupress_nonce = wp_create_nonce( 'force_deactivation' );
+	// We add a link to permit "force deactivation", use at your own risks.
+	if ( ! $permit_force_deactivation ) {
+		return;
+	}
 
-			echo '<p><a href="' . wp_nonce_url( 'plugins.php?action=deactivate&amp;secupress_nonce=' . $secupress_nonce . '&amp;plugin=' . $plugin_file . '&amp;plugin_status=' . $status . '&amp;paged=' . $page . '&amp;s=' . $s, 'deactivate-plugin_' . $plugin_file ).'">' . __( 'You can still force the deactivation by clicking here.', 'secupress' ) . '</a></p>';
-		}
-		?>
-	</div>
+	$message = wp_nonce_url( 'plugins.php?action=deactivate&amp;secupress_nonce=' . wp_create_nonce( 'force_deactivation' ) . '&amp;plugin=' . SECUPRESS_PLUGIN_FILE . '&amp;plugin_status=' . $status . '&amp;paged=' . $page . '&amp;s=' . $s, 'deactivate-plugin_' . SECUPRESS_PLUGIN_FILE );
+	$message = '<a href="' . $message . '">' . __( 'You can still force the deactivation by clicking here.', 'secupress' ) . '</a>';
 
-	<?php
+	secupress_add_notice( $message, 'error' );
 }
 
 
 /**
- * This warning is displayed when some plugins may conflict with WP Rocket
+ * This warning is displayed when some plugins may conflict with SecuPress.
  *
  * @since 1.0
  */
-add_action( 'admin_notices', 'secupress_plugins_to_deactivate' );
+add_action( 'admin_init', 'secupress_plugins_to_deactivate' );
 
 function secupress_plugins_to_deactivate() {
-	$plugins_to_deactivate = array();
+	if ( ! current_user_can( secupress_get_capability() ) ) {
+		return;
+	}
 
-	// Deactivate all plugins who can cause conflicts with WP Rocket
 	$plugins = array(
 		'wordfence/wordfence.php'
 	);
 
 	$plugins_to_deactivate = array_filter( $plugins, 'is_plugin_active' );
 
-	if ( current_user_can( secupress_get_capability() ) && count( $plugins_to_deactivate ) ) { ?>
-
-		<div class="error">
-			<p><?php printf( __( '<strong>%s</strong>: The following plugins are not compatible with this plugin and may cause unexpected results:', 'rocket' ), SECUPRESS_PLUGIN_NAME ); ?></p>
-			<ul class="rocket-plugins-error">
-			<?php
-			foreach ( $plugins_to_deactivate as $plugin ) {
-
-				$plugin_data = get_plugin_data( WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin );
-				echo '<li>' . $plugin_data['Name'] . '</span> <a href="' . wp_nonce_url( admin_url( 'admin-post.php?action=deactivate_plugin&plugin=' . urlencode( $plugin ) ), 'deactivate_plugin' ) . '" class="button-secondary alignright">' . __( 'Deactivate', 'secupress' ) . '</a></li>';
-
-			}
-			?>
-			</ul>
-		</div>
-
-		<?php
+	if ( ! $plugins_to_deactivate ) {
+		return;
 	}
+
+	$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+	$message .= __( 'The following plugins are not compatible with this plugin and may cause unexpected results:', 'secupress' );
+	$message .= '<ul>';
+	foreach ( $plugins_to_deactivate as $plugin ) {
+		$plugin_data = get_plugin_data( WP_PLUGIN_DIR . DIRECTORY_SEPARATOR . $plugin );
+		$message .= '<li>' . $plugin_data['Name'] . '</span> <a href="' . wp_nonce_url( admin_url( 'admin-post.php?action=deactivate_plugin&plugin=' . urlencode( $plugin ) ), 'deactivate_plugin' ) . '" class="button-secondary alignright">' . __( 'Deactivate' ) . '</a></li>';
+	}
+	$message .= '</ul>';
+
+	secupress_add_notice( $message, 'error', 'deactivate-plugin' );
 }
 
+
 /**
- * This warning is displayed when the wp-config.php file isn't writable
+ * This warning is displayed when the wp-config.php file isn't writable.
  *
  * @since 1.0
  */
-add_action( 'admin_notices', 'secupress_warning_wp_config_permissions' );
+add_action( 'admin_init', 'secupress_warning_wp_config_permissions' );
 
 function secupress_warning_wp_config_permissions() {
-	global $pagenow, $current_user;
-	$config_file = secupress_find_wpconfig_path();
+	global $pagenow;
 
-	if ( ! isset( $current_user->ID ) || 'plugins.php' == $pagenow && isset( $_GET['activate'] ) || ! current_user_can( secupress_get_capability() ) || is_writable( $config_file ) ) {
+	if ( 'plugins.php' === $pagenow && isset( $_GET['activate'] ) ) {
 		return;
 	}
 
-	$boxes = get_user_meta( $current_user->ID, 'secupress_boxes', true );
-
-	if ( in_array( __FUNCTION__, (array) $boxes ) ) {
+	if ( ! current_user_can( secupress_get_capability() ) || wp_is_writable( secupress_find_wpconfig_path() ) ) {
 		return;
 	}
-	?>
 
-	<div class="error">
-		<a href="<?php echo wp_nonce_url( admin_url( 'admin-post.php?action=secupress_ignore&box='.__FUNCTION__ ), 'secupress_ignore_'.__FUNCTION__ ); ?>" class="secupress-cross"><div class="dashicons dashicons-no"></div></a>
-		<p>
-			<?php
-			printf( __( '%s: It seems we don\'t have <a href="%s" target="_blank">writing permissions</a> on <code>wp-config.php</code> file.', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>', 'http://codex.wordpress.org/Changing_File_Permissions' );
-			echo '<br>';
-			_e( 'To fix this you have to set writing permissions for <code>wp-config.php</code> and then save the settings again.', 'secupress' );
-			echo '<br>';
-			_e( 'If the message persists, you have to put the following code in your <code>wp-config.php</code> file so that it works correctly. Click on the field and press Ctrl-A to select all.', 'secupress' );
-			?>
-		</p>
+	$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+	$message .= sprintf( __( 'It seems we don\'t have <a href="%1$s" target="_blank">writing permissions</a> on %2$s file.', 'secupress' ), 'http://codex.wordpress.org/Changing_File_Permissions', '<code>wp-config.php</code>' );
 
-		<?php
-		// Get the content of the WP_CACHE constant added by WP Rocket
-		// $define = "/** SecuPress */\r\ndefine( 'WP_CACHE', true );\r\n";
-		?>
-
-		<p><textarea readonly="readonly" id="rules" name="rules" class="large-text readonly" rows="2"><?php echo esc_textarea( $define ); ?></textarea></p>
-	</div>
-
-	<?php
+	secupress_add_notice( $message, 'error', 'wpconfig-not-writable' );
 }
 
 
 /**
- * This warning is displayed when the .htaccess file doesn't exist or isn't writeable
+ * This warning is displayed when the .htaccess file or the web.config file doesn't exist or isn't writable.
  *
  * @since 1.0
  */
-add_action( 'admin_notices', 'secupress_warning_htaccess_permissions' );
+add_action( 'admin_admin', 'secupress_warning_htaccess_permissions' );
 
 function secupress_warning_htaccess_permissions() {
-	global $pagenow, $current_user, $is_apache;
-	$htaccess_file = secupress_get_home_path() . '.htaccess';
-
-	if ( ! $is_apache || ! isset( $current_user->ID ) || ! current_user_can( secupress_get_capability() ) || is_writable( $htaccess_file ) ) {
-		return;
-	}
-
-	$boxes = get_user_meta( $current_user->ID, 'secupress_boxes', true );
-
-	if ( in_array( __FUNCTION__, (array) $boxes ) ) {
-		return;
-	}
-	?>
-
-	<div class="error">
-		<a href="<?php echo wp_nonce_url( admin_url( 'admin-post.php?action=secupress_ignore&box='.__FUNCTION__ ), 'secupress_ignore_'.__FUNCTION__ ); ?>" class="secupress-cross"><div class="dashicons dashicons-no"></div></a>
-		<p><strong><?php echo SECUPRESS_PLUGIN_NAME; ?></strong>: <?php printf( __( 'If you had <a href="%1$s" target="_blank">writing permissions</a> on <code>.htaccess</code> file, %2$s could do this automatically. This is not the case, so here are the rewrite rules you have to put in your <code>.htaccess</code> file for %2$s to work correctly. Click on the field and press Ctrl-A to select all.', 'rocket' ), 'http://codex.wordpress.org/Changing_File_Permissions', '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>' ) . '<br>' . __( '<strong>Warning:</strong> This message will popup again and its content may be updated when saving the options', 'secupress' ); ?></p>
-		<p><textarea readonly="readonly" id="rules" name="rules" class="large-text readonly" rows="6"><?php echo esc_textarea( get_rocket_htaccess_marker() ); ?></textarea></p>
-	</div>
-
-	<?php
-}
-
-
-/**
- * This warnings are displayed when a module has been activated/deactivated
- *
- * @since 1.0
- */
-add_action( 'admin_notices', 'secupress_warning_module_activity' );
-
-function secupress_warning_module_activity() {
-	global $current_user;
+	global $pagenow, $is_apache, $is_iis7;
 
 	if ( ! current_user_can( secupress_get_capability() ) ) {
 		return;
 	}
 
-	$activated_modules   = get_site_transient( 'secupress_module_activation_' . $current_user->ID );
-	$deactivated_modules = get_site_transient( 'secupress_module_deactivation_' . $current_user->ID );
-	delete_site_transient( 'secupress_module_activation_' . $current_user->ID );
-	delete_site_transient( 'secupress_module_deactivation_' . $current_user->ID );
+	if ( $is_apache ) {
+		$file = '.htaccess';
+		$htaccess_file = secupress_get_home_path() . $file;
 
-	if ( $activated_modules ) {
-		?>
-		<div class="updated">
-			<p>
-				<?php
-				printf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
-				echo _n( 'This module has been activated:', 'These modules have been activated:', count( $activated_modules ), 'secupress' );
-				printf( '<ul><li>%s</li></ul>', implode( '</li><li>', $activated_modules ) );
-				?>
-			</p>
-		</div>
-		<?php
+		if ( is_writable( $htaccess_file ) ) {
+			return;
+		}
+	} elseif ( $is_iis7 ) {
+		$file = 'web.config';
+		$web_config_file = secupress_get_home_path() . $file;
+
+		if ( wp_is_writable( $web_config_file ) ) {
+			return;
+		}
+	} else {
+		return;
 	}
 
-	if ( $deactivated_modules ) {
-		?>
-		<div class="updated">
-			<p>
-				<?php
-				printf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
-				echo _n( 'This module has been deactivated:', 'These modules have been deactivated:', count( $deactivated_modules ), 'secupress' );
-				printf( '<ul><li>%s</li></ul>', implode( '</li><li>',  $deactivated_modules ) );
-				?>
-			</p>
-		</div>
-		<?php
+	$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+	$message .= sprintf( __( 'If you had <a href="%1$s" target="_blank">writing permissions</a> on %2$s file, %3$s could do more things automatically.', 'secupress' ), 'http://codex.wordpress.org/Changing_File_Permissions', '<code>' . $file . '</code>', '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong>' );
+
+	secupress_add_notice( $message, 'error', 'htaccess-not-writable' );
+}
+
+
+/**
+ * These warnings are displayed when a module has been activated/deactivated.
+ *
+ * @since 1.0
+ */
+add_action( 'admin_init', 'secupress_warning_module_activity' );
+
+function secupress_warning_module_activity() {
+	$current_user_id = get_current_user_id();
+
+	if ( ! current_user_can( secupress_get_capability() ) ) {
+		return;
+	}
+
+	$activated_modules   = get_site_transient( 'secupress_module_activation_' . $current_user_id );
+	$deactivated_modules = get_site_transient( 'secupress_module_deactivation_' . $current_user_id );
+
+	if ( false !== $activated_modules ) {
+		$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+		$message .= _n( 'This module has been activated:', 'These modules have been activated:', count( $activated_modules ), 'secupress' );
+		$message .= sprintf( '<ul><li>%s</li></ul>', implode( '</li><li>', $activated_modules ) );
+
+		secupress_add_notice( $message );
+		delete_site_transient( 'secupress_module_activation_' . $current_user_id );
+	}
+
+	if ( false !== $deactivated_modules ) {
+		$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+		$message .= _n( 'This module has been deactivated:', 'These modules have been deactivated:', count( $deactivated_modules ), 'secupress' );
+		$message .= sprintf( '<ul><li>%s</li></ul>', implode( '</li><li>', $deactivated_modules ) );
+
+		secupress_add_notice( $message );
+		delete_site_transient( 'secupress_module_deactivation_' . $current_user_id );
 	}
 }
 
 
 /**
- * This warnings are displayed when the backup email is not set
+ * This warning is displayed when the backup email is not set.
  *
  * @since 1.0
  */
-add_action( 'admin_notices', 'secupress_warning_no_backup_email' );
+add_action( 'admin_init', 'secupress_warning_no_backup_email' );
 
 function secupress_warning_no_backup_email() {
-	global $current_user;
-
-	/** This filter is documented in inc/admin-bar.php */
-	if ( ! get_user_meta( $current_user->ID, 'backup_email', true ) ) {
-		?>
-		<div class="error">
-			<p>
-				<strong><?php echo SECUPRESS_PLUGIN_NAME; ?></strong>:
-				<?php echo sprintf( __( 'Your <a href="%s#secupress_backup_email">Backup E-mail</a> isn\'t yet set. Please do it.', 'secupress' ), get_edit_profile_url() ); ?>
-			</p>
-		</div>
-		<?php
+	if ( get_user_meta( get_current_user_id(), 'backup_email', true ) ) {
+		return;
 	}
+
+	$message  = sprintf( __( '%s:', 'secupress' ), '<strong>' . SECUPRESS_PLUGIN_NAME . '</strong> ' );
+	$message .= sprintf( __( 'Your <a href="%s">Backup E-mail</a> isn\'t yet set. Please do it.', 'secupress' ), get_edit_profile_url() . '#secupress_backup_email' );
+
+	secupress_add_notice( $message, 'error', false );
 }
 
 
