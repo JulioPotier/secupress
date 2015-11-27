a3bacf7bfb76b15a84fe5855503ecd99ac9ff3fd
Julio Potier <juliobosk@gmail.com>
2015-11-27 16:04:15 +0100

    New settings skeletons for bruteforce and geoip-system

--- inc/modules/firewall/settings.php
+++ inc/modules/firewall/settings.php
@@ -3,3 +3,5 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 $this->load_plugin_settings( 'bbq-headers' );
 $this->load_plugin_settings( 'bbq-url-content' );
+$this->load_plugin_settings( 'bruteforce' );
+$this->load_plugin_settings( 'geoip-system' );
