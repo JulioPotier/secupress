7efdf6c07c28987269478d84b40fbfea866d8abf
Julio Potier <juliobosk@gmail.com>
2015-11-27 15:55:47 +0100

    pre pro version managment, add "update", + "autoupdate"

--- inc/modules/plugins-themes/settings/themes.php
+++ inc/modules/plugins-themes/settings/themes.php
@@ -8,8 +8,30 @@ $this->add_section( __( 'Themes Page', 'secupress' ) );
 
 
 $plugin = $this->get_current_plugin(); // 'themes'
-$field_name = $this->get_field_name( 'installation' );
 
+$field_name = $this->get_field_name( 'update' );
+$this->add_field(
+	__( 'Theme update', 'secupress' ),
+	array(
+		'name'        => $field_name,
+	),
+	array(
+		array(
+			'type'         => 'checkbox',
+			'name'         => $field_name,
+			'label'        => __( 'Yes, disable updates for themes', 'secupress' ),
+			'label_for'    => $field_name,
+			'label_screen' => __( 'Yes, disable updates for themes', 'secupress' ),
+		),
+		array(
+			'type'         => 'helper_description',
+			'name'         => $field_name,
+			'description'  => __( 'You will still be notified when an update is available.', 'secupress' ),
+		),
+	)
+);
+
+$field_name = $this->get_field_name( 'installation' );
 $this->add_field(
 	__( 'Theme installation', 'secupress' ),
 	array(
@@ -35,6 +57,7 @@ $this->add_field(
 	__( 'Theme switch', 'secupress' ),
 	array(
 		'name'        => $field_name,
+		'description' => secupress_is_pro() ? '' : secupress_get_pro_version_string()
 	),
 	array(
 		array(
@@ -43,6 +66,7 @@ $this->add_field(
 			'label'        => __( 'Yes, disable switch theme', 'secupress' ),
 			'label_for'    => $field_name,
 			'label_screen' => __( 'Yes, disable switch theme', 'secupress' ),
+			'readonly'     => ! secupress_is_pro(),
 		),
 		array(
 			'type'         => 'helper_description',
@@ -56,6 +80,7 @@ $this->add_field(
 	__( 'Theme deletion', 'secupress' ),
 	array(
 		'name'        => $field_name,
+		'description' => secupress_is_pro() ? '' : secupress_get_pro_version_string()
 	),
 	array(
 		array(
@@ -64,6 +89,7 @@ $this->add_field(
 			'label'        => __( 'Yes, disable delete for theme', 'secupress' ),
 			'label_for'    => $field_name,
 			'label_screen' => __( 'Yes, disable delete for theme', 'secupress' ),
+			'readonly'     => ! secupress_is_pro(),
 		),
 		array(
 			'type'         => 'helper_description',
@@ -72,24 +98,51 @@ $this->add_field(
 	)
 );
 
-$field_name = $this->get_field_name( 'update' );
+$field_name      = $this->get_field_name( 'detect_bad_themes' );
+$main_field_name = $field_name;
 $this->add_field(
-	__( 'Theme update', 'secupress' ),
+	__( 'Detect Bad Themes', 'secupress' ),
 	array(
 		'name'        => $field_name,
+		'description' => __( 'Work for any public theme, premium or free.', 'secupress' ),
 	),
 	array(
 		array(
 			'type'         => 'checkbox',
 			'name'         => $field_name,
-			'label'        => __( 'Yes, disable updates for themes', 'secupress' ),
+			'label'        => __( 'Yes, enable the detection if a theme I use is known as vulnerable', 'secupress' ),
 			'label_for'    => $field_name,
-			'label_screen' => __( 'Yes, disable updates for themes', 'secupress' ),
+			'label_screen' => __( 'Yes, enable the detection if a theme I use is known as vulnerable', 'secupress' ),
 		),
 		array(
 			'type'         => 'helper_description',
 			'name'         => $field_name,
-			'description'  => __( 'You will still be notified when an update is available.', 'secupress' ),
+			'description'  => __( 'Based on our Daily Security Monitoring, we will push notices for themes newly known as vulnerables.', 'secupress' ),
+		),
+	)
+);
+
+$field_name = $this->get_field_name( 'autoupdate_bad_themes' );
+$this->add_field(
+	__( 'Auto-Update Bad Themes', 'secupress' ),
+	array(
+		'name'        => $field_name,
+		'description' => __( 'Only for themes from official repository.', 'secupress' ) . ( secupress_is_pro() ? '' : secupress_get_pro_version_string( '<br>%s') )
+	),
+	array(
+		'depends_on'       => $main_field_name,
+		array(
+			'type'         => 'checkbox',
+			'name'         => $field_name,
+			'label'        => __( 'Yes, enable the auto-update if a theme I use is known as vulnerable', 'secupress' ),
+			'label_for'    => $field_name,
+			'label_screen' => __( 'Yes, enable the auto-update if a theme I use is known as vulnerable', 'secupress' ),
+			'readonly'     => ! secupress_is_pro(),
+		),
+		array(
+			'type'         => 'helper_description',
+			'name'         => $field_name,
+			'description'  => __( 'Based on our Daily Security Monitoring, we will push updates for themes newly known as vulnerables.', 'secupress' ),
 		),
 	)
 );
