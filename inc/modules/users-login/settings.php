56d2fd2a72b806a8ebc9668791a686145c890f86
Julio Potier <juliobosk@gmail.com>
2015-11-27 15:44:26 +0100

    New module settings, password-policy, move login

--- inc/modules/users-login/settings.php
+++ inc/modules/users-login/settings.php
@@ -3,4 +3,6 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 $this->load_plugin_settings( 'double-auth' );
 $this->load_plugin_settings( 'login-protection' );
+$this->load_plugin_settings( 'password-policy' );
 $this->load_plugin_settings( 'bad-logins' );
+$this->load_plugin_settings( 'move-login' );
