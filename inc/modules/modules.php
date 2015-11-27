5b6e54d08e1b99e58888d82fe6494e4b4a65fa36
Julio Potier <juliobosk@gmail.com>
2015-11-27 16:03:53 +0100

    Better desc, icons, names, etc :)

--- inc/modules/modules.php
+++ inc/modules/modules.php
@@ -31,7 +31,7 @@ function secupress_get_modules() {
 				__( 'WordPress can be tweak by so many ways. But are you using the right ones. We will help', 'secupress' ),
 				__( '', 'secupress' ),
 			),
-		),
+		),		
 		'sensitive-data'  => array(
 			'title'       => esc_html__( 'Sensitive Data', 'secupress' ),
 			'dashicon'    => 'lock',
@@ -40,36 +40,28 @@ function secupress_get_modules() {
 				__( 'Do not hesitate to lock as much as you can to improve the security of your website.', 'secupress' ),
 			),
 		),
-		'server-settings' => array(
-			'title'       => esc_html__( 'Server Settings', 'secupress' ),
-			'dashicon'    => 'admin-home',
+		'file-system'     => array(
+			'title'       => esc_html__( 'File System', 'secupress' ),
+			'dashicon'    => 'portfolio',
 			'description' => array(
-				__( '', 'secupress' ),
-				__( '', 'secupress' ),
+				__( 'Check the file permissions <em>(chmod)</em> at a glance and run a file monitoring on your installation', 'secupress' ),
+				__( 'Also, an antivus scanner can be performed on your installation, this may taka time but it\'s more efficient.', 'secupress' ),
 			),
 		),
 		'backups'         => array(
 			'title'       => esc_html__( 'Backups', 'secupress' ),
 			'dashicon'    => 'media-archive',
 			'description' => array(
-				__( '', 'secupress' ),
-				__( '', 'secupress' ),
+				__( 'Backuping your database daily and you files weekly can reduce the risks to lose your content because of an attack.', 'secupress' ),
+				sprintf( __( 'Don\'t forget to <a href="%s">schedule backups</a> as soon as possible.', 'secupress' ), secupress_admin_url( 'modules', 'schedules' ) ),
 			),
 		),
 		'antispam'        => array(
 			'title'       => esc_html__( 'Anti Spam', 'secupress' ),
 			'dashicon'    => 'email-alt',
 			'description' => array(
-				__( '', 'secupress' ),
-				__( '', 'secupress' ),
-			),
-		),
-		'alerts'          => array(
-			'title'       => esc_html__( 'Alerts', 'secupress' ),
-			'dashicon'    => 'megaphone',
-			'description' => array(
-				__( '', 'secupress' ),
-				__( '', 'secupress' ),
+				__( 'Comments are great for your website, but bot traffic represent about 60 % of the internet. Spams are done by these bots, and they just want to add their content in your website. Don\'t let them do that!', 'secupress' ),
+				sprintf( __( 'Do not forget to visit the <a href="%s">Settings > Discussion</a> area to add words to the blacklist and other usual settings regarding comments.', 'secupress' ), admin_url( 'options-discussion.php' ) ),
 			),
 		),
 		'firewall'     => array(
@@ -79,11 +71,19 @@ function secupress_get_modules() {
 				__( 'Malicious requests are badly common. This will checks all incoming requests and quietly blocks all of these containing bad stuff.', 'secupress' ),
 			),
 		),
+		'alerts'          => array(
+			'title'       => esc_html__( 'Alerts', 'secupress' ),
+			'dashicon'    => 'megaphone',
+			'description' => array(
+				__( 'Each time we that an action is a possible attack vector, we add it here, so you can see what happened and when we blocked it or not.', 'secupress' ),
+				__( '', 'secupress' ),
+			),
+		),		
 		'logs'            => array(
 			'title'       => esc_html__( 'Logs', 'secupress' ),
 			'dashicon'    => 'list-view',
 			'description' => array(
-				__( '', 'secupress' ),
+				__( 'Logs are very usefull, it acts like a history of what happened on your website, filtered and at any time. You can also read and delete banned IPs from our modules here.', 'secupress' ),
 				__( '', 'secupress' ),
 			),
 		),
@@ -91,7 +91,7 @@ function secupress_get_modules() {
 			'title'       => esc_html__( 'Tools', 'secupress' ),
 			'dashicon'    => 'admin-tools',
 			'description' => array(
-				__( '', 'secupress' ),
+				__( 'The page contains our tools designed to help you with the plugin. Export and import settings, set your API key, rollback a version or even monitor your website can be done from here.', 'secupress' ),
 				__( '', 'secupress' ),
 			),
 		),
@@ -99,7 +99,7 @@ function secupress_get_modules() {
 			'title'       => esc_html__( 'Schedules', 'secupress' ),
 			'dashicon'    => 'calendar-alt',
 			'description' => array(
-				__( '', 'secupress' ),
+				__( 'Scheduling recurrent tasks can be very usefull to gain time and stay safe. At least each week a backup should be done, same for a full scan of vulnerabilities and file changes.', 'secupress' ),
 				__( '', 'secupress' ),
 			),
 		),
