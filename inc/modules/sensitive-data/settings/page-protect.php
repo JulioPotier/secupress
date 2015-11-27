908e3b68ee336271478bd78d165742839e94a815
Julio Potier <juliobosk@gmail.com>
2015-11-27 15:45:34 +0100

    pre pro version managment

--- inc/modules/sensitive-data/settings/page-protect.php
+++ inc/modules/sensitive-data/settings/page-protect.php
@@ -12,7 +12,7 @@ $this->add_field(
 	__( 'Protect the profile page', 'secupress' ),
 	array(
 		'name'        => $field_name,
-		'description' => __( 'You can easily protect the user\'s profile settings page by asking them to enter their password.', 'secupress' ),
+		'description' => __( 'You can easily protect the user\'s profile settings page by asking them to enter their password.', 'secupress' ) . ( secupress_is_pro() ? '' : secupress_get_pro_version_string( '<br>%s') ),
 	),
 	array(
 		array(
@@ -21,6 +21,7 @@ $this->add_field(
 			'label'        => __( 'Yes, protect the profile pages', 'secupress' ),
 			'label_for'    => $field_name,
 			'label_screen' => __( 'Yes, protect the profile pages', 'secupress' ),
+			'readonly'     => ! secupress_is_pro(),
 		),
 		array(
 			'type'         => 'helper_description',
@@ -35,8 +36,9 @@ $field_name = $this->get_field_name( 'settings' );
 
 $this->add_field(
 	sprintf( __( 'Protect %s settings page', 'secupress' ), SECUPRESS_PLUGIN_NAME ),
-	array(	'description' => __( 'We recommand to protect the settings page once your settings are done.', 'secupress' ),
+	array(	
 			'name' => $field_name,
+			'description' => __( 'We recommand to protect the settings page once your settings are done.', 'secupress' ) . ( secupress_is_pro() ? '' : secupress_get_pro_version_string( '<br>%s') ),
 		),
 	array(
 		array(
@@ -45,6 +47,7 @@ $this->add_field(
 			'label'        => __( 'Yes, protect the settings pages', 'secupress' ),
 			'label_for'    => $field_name,
 			'label_screen' => __( 'Yes, protect the settings pages', 'secupress' ),
+			'readonly'     => ! secupress_is_pro(),
 		),
 		array(
 			'type'         => 'helper_description',
