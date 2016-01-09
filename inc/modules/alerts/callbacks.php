47e82ae325364c858a538ffa62c59a3acb483c6c
Screenfeed <git@screenfeed.fr>
2016-01-09 02:53:02 +0100

    Alerts: first draft for the settings.

--- inc/modules/alerts/callbacks.php
+++ inc/modules/alerts/callbacks.php
@@ -1,2 +1,93 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+/*------------------------------------------------------------------------------------------------*/
+/* ON MODULE SETTINGS SAVE ====================================================================== */
+/*------------------------------------------------------------------------------------------------*/
+
+/**
+ * Callback to filter, sanitize, validate and de/activate submodules.
+ *
+ * @since 1.0
+ *
+ * @param (array) $settings The module settings.
+ *
+ * @return (array) The sanitized and validated settings.
+ */
+function __secupress_alerts_settings_callback( $settings ) {
+	$modulenow = 'alerts';
+	$settings  = $settings ? $settings : array();
+
+	// Activate/deactivate.
+	if ( empty( $settings['alerts_type'] ) || ! is_array( $settings['alerts_type'] ) ) {
+		$settings['alerts_type'] = array();
+	} else {
+		$types = array_flip( secupress_alerts_labels( secupress_is_pro() ) );
+
+		$settings['alerts_type'] = array_intersect( $settings['alerts_type'], $types );
+	}
+
+	secupress_manage_submodule( $modulenow, 'alerts', ! empty( $settings['alerts_type'] ) );
+
+	// Email
+	if ( ! empty( $settings['alerts_email'] ) ) {
+		if ( secupress_is_pro() ) {
+			$settings['alerts_email'] = explode( ',', $settings['alerts_email'] );
+		} else {
+			$settings['alerts_email'] = (array) $settings['alerts_email'];
+		}
+
+		$settings['alerts_email'] = array_map( 'trim', $settings['alerts_email'] );
+		$settings['alerts_email'] = array_map( 'is_email', $settings['alerts_email'] );
+		$settings['alerts_email'] = array_filter( $settings['alerts_email'] );
+		$settings['alerts_email'] = implode( ', ', $settings['alerts_email'] );
+	} else {
+		unset( $settings['alerts_email'] );
+	}
+
+	// Other types
+	$types = array( 'alerts_sms_number', 'alerts_push', 'alerts_slack', 'alerts_twitter' );
+
+	foreach ( $types as $type ) {
+		if ( ! empty( $settings[ $type ] ) ) {
+			$settings[ $type ] = sanitize_text_field( $settings[ $type ] );
+		} else {
+			unset( $settings[ $type ] );
+		}
+	}
+
+	// Frequency
+	$settings['alerts_frequency'] = secupress_minmax_range( $settings['alerts_frequency'], 5, 60 );
+
+	return $settings;
+}
+
+
+/*------------------------------------------------------------------------------------------------*/
+/* TOOLS ======================================================================================== */
+/*------------------------------------------------------------------------------------------------*/
+
+/*
+ * Get available alert types.
+ *
+ * @since 1.0
+ *
+ * @param (bool) $all Set to `true` to return all free and pro types. Will return only free types otherwise.
+ *
+ * @return (array) Return an array with identifiers as keys and field labels as values.
+ */
+function secupress_alerts_labels( $all = false ) {
+	if ( ! $all ) {
+		return array(
+			'email' => __( 'By Email', 'secupress' ),
+		);
+	}
+
+	return array(
+		'email'   => __( 'By Email', 'secupress' ),
+		'sms'     => __( 'By SMS', 'secupress' ),
+		'push'    => __( 'By push notification', 'secupress' ),
+		'slack'   => __( 'With Slack', 'secupress' ),
+		'twitter' => __( 'With Twitter', 'secupress' ),
+	);
+}
--- inc/modules/alerts/settings/alerts.php
+++ inc/modules/alerts/settings/alerts.php
@@ -3,11 +3,116 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 
 $this->set_current_section( 'alerts' );
-$this->add_section( __( 'Alerts Manager', 'secupress' ), array( 'with_save_button' => false ) );
+$this->add_section( __( 'Alerts Manager', 'secupress' ) );
+
+
+$main_field_name = $this->get_field_name( 'type' );
+$options         = secupress_alerts_labels( true );
 
 $this->add_field( array(
-	'title'        => __( 'Alerts', 'secupress' ),
-	'description'  => __( '', 'secupress' ),
-	'name'         => $this->get_field_name( 'alerts' ),
-	'type'         => 'alerts', //// décompiler le tout et faire un module par alerte genre 404, login, requests etc
+	'title'        => __( 'How to alert you?', 'secupress' ),
+	'name'         => $main_field_name,
+	'type'         => 'checkboxes',
+	'value'        => ( secupress_is_submodule_active( 'alerts', 'alerts' ) ? null : array() ),
+	'label_screen' => __( 'Choose which service that will be used:', 'secupress' ),
+	'options'      => $options,
+) );
+
+
+if ( ! secupress_is_pro() ) :
+
+	$this->add_field( array(
+		'title'        => __( 'By Email', 'secupress' ),
+		'depends'      => $main_field_name . '_email',
+		'label_for'    => $this->get_field_name( 'email' ),
+		'type'         => 'email',
+		'default'      => get_option( 'admin_email' ),
+		'label'        => __( 'Email Address:', 'secupress' ),
+	) );
+
+else :
+
+	$this->add_field( array(
+		'title'        => __( 'By Email', 'secupress' ),
+		'depends'      => $main_field_name . '_email',
+		'label_for'    => $this->get_field_name( 'email' ),
+		'type'         => 'textarea',
+		'label'        => __( 'Email Addresses:', 'secupress' ),
+		'default'      => get_option( 'admin_email' ),
+		'attributes'   => array( 'rows' => 2, ),
+		'helpers'      => array(
+			array(
+				'type'        => 'description',
+				'description' => __( 'Separate addresses with a comma.', 'secupress' ),
+			),
+		),
+	) );
+
+
+	$this->add_field( array(
+		'title'        => __( 'By SMS', 'secupress' ),
+		'depends'      => $main_field_name . '_sms',
+		'label_for'    => $this->get_field_name( 'sms_number' ),
+		'type'         => 'tel',
+		'label'        => __( 'Phone number:', 'secupress' ),
+	) );
+
+
+	$this->add_field( array(
+		'title'        => __( 'By push notification', 'secupress' ),
+		'depends'      => $main_field_name . '_push',
+		'label_for'    => $this->get_field_name( 'push' ),
+		'type'         => 'text',
+		'label'        => __( 'I don\'t know what I need yet:', 'secupress' ),////
+	) );
+
+
+	$this->add_field( array(
+		'title'        => __( 'With Slack', 'secupress' ),
+		'depends'      => $main_field_name . '_slack',
+		'label_for'    => $this->get_field_name( 'slack' ),
+		'type'         => 'text',
+		'label'        => __( 'I don\'t know what I need yet:', 'secupress' ),////
+	) );
+
+
+	$this->add_field( array(
+		'title'        => __( 'With Twitter', 'secupress' ),
+		'depends'      => $main_field_name . '_twitter',
+		'label_for'    => $this->get_field_name( 'twitter' ),
+		'type'         => 'text',
+		'label'        => __( 'I don\'t know what I need yet:', 'secupress' ),////
+	) );
+
+endif;
+
+
+$label   = __( 'Every %d minutes.', 'secupress' );
+$label   = explode( '%d', $label );
+$label[] = '';
+
+$this->add_field( array(
+	'title'        => __( 'How often will you be alerted?', 'secupress' ),
+	'description'  => __( 'It would be a bad idea to alert you everytime something happens, so we\'ll need to group messages.', 'secupress' ),
+	'depends'      => $main_field_name . '_' . implode( ' ' . $main_field_name . '_', array_keys( $options ) ),
+	'name'         => $this->get_field_name( 'frequency' ),
+	'row_class'    => 'row-nopad-top',
+	'type'         => 'number',
+	'default'      => 15,
+	'label_before' => $label[0],
+	'label_after'  => $label[1],
+	'attributes'   => array(
+		'min' => 5,
+		'max' => 60,
+	),
+	'helpers'      => array(
+		array(
+			'type'        => 'description',
+			'description' => __( 'You will be notified <strong>only</strong> if something happens.', 'secupress' ),
+		),
+		array(
+			'type'        => 'description',
+			'description' => __( 'For some important events, a notification will be sent right away.', 'secupress' ),
+		),
+	),
 ) );
