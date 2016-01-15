969fd00701868327118ae53e5ba9113c72edb097
Julio Potier <juliobosk@gmail.com>
2016-01-15 16:21:03 +0100

    Fix bug, too many restrictive

--- inc/modules/users-login/plugins/nonlogintimeslot.php
+++ inc/modules/users-login/plugins/nonlogintimeslot.php
@@ -9,26 +9,28 @@ Version: 1.0
 defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
 
 add_action( 'plugins_loaded', 'secupress_donttrytologin' );
-
 function secupress_donttrytologin() {
-	$timings = secupress_get_module_option( 'login_protection_nonlogintimeslot', 10, 'users_login' );
+	$timings = secupress_get_module_option( 'login-protection_nonlogintimeslot', false, 'users-login' );
+
+	if ( false === $timings ) {
+		return;
+	}
+	
+	// server hour
+	$UTC        = new DateTimeZone( 'UTC' );
+	$newTZ      = new DateTimeZone( ini_get( 'date.timezone' ) );
+	$date       = new DateTime( '', $UTC );
+	$date->setTimezone( $newTZ );
+	$server_hour = strtotime( $date->format( 'Y-m-d H:i:s' ) );
+
 	// from
 	$setting_from     = strtotime( date( sprintf( 'Y-m-d %s:%s:00', $timings['from_hour'], $timings['from_minute'] ) ) );
-	$UTC              = new DateTimeZone( 'UTC' );
-	$newTZ            = new DateTimeZone( ini_get( 'date.timezone' ) );
-	$date             = new DateTime( '', $UTC );
-	$date->setTimezone( $newTZ );
-	$server_hour_from = strtotime( $date->format( 'Y-m-d H:i:s' ) );
 
 	// to
 	$setting_to     = strtotime( date( sprintf( 'Y-m-d %s:%s:00', $timings['to_hour'], $timings['to_minute'] ) ) );
-	$UTC            = new DateTimeZone( 'UTC' );
-	$newTZ          = new DateTimeZone( ini_get( 'date.timezone' ) );
-	$date           = new DateTime( '', $UTC );
-	$date->setTimezone( $newTZ );
-	$server_hour_to = strtotime( $date->format( 'Y-m-d H:i:s' ) );
 
-	if ( $server_hour_from > $setting_from || $server_hour_to < $setting_to ) {
+	if ( ( $setting_from < $setting_to ) && ( ( $server_hour > $setting_from ) && ( $server_hour < $setting_to ) ) ||
+		 ( $setting_from > $setting_to ) && ( ( $server_hour > $setting_from ) || ( $server_hour < $setting_to ) ) ) {
 		add_action( 'login_form_login', '__secupress_cant_login_now' );
 		remove_all_filters( 'authenticate' );
 		add_filter( 'authenticate', '__return_false', PHP_INT_MAX );
