fa2f9830791bfffe986477d38c6ba0ef18ffbe2b
Screenfeed <git@screenfeed.fr>
2016-01-26 01:23:42 +0100

    One hour in this code is 7 years on Earth.

---
+++ inc/modules/sensitive-data/plugins/blackhole.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Module Name: Blackhole
+Description: Catch bots that don't respect your <code>robots.txt</code> rules.
+Main Module: sensitive_data
+Author: SecuPress
+Version: 1.0
+*/
+defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
+
+
+/**
+ * Add forbidden URI in `robots.txt` file.
+ *
+ * @since 1.0
+ *
+ * @param (string) File content.
+ *
+ * @return (string) File content.
+ */
+add_filter( 'robots_txt', 'secupress_blackhole_robots_txt' );
+
+function secupress_blackhole_robots_txt( $output ) {
+	$dirname = '/secupress-' . substr( wp_hash( 'blackhole', 'nonce' ), 10, 8 ) . '/';
+
+	if ( false !== strpos( $output, "User-agent: *\n" ) ) {
+		$output  = str_replace( "User-agent: *\n", "User-agent: *\nDisallow: $dirname\n", $output );
+	} else {
+		$output .= "\nUser-agent: *\nDisallow: $dirname\n";
+	}
+
+	return $output;
+}
+
+
+/**
+ * Use a custom template for our trap.
+ *
+ * @since 1.0
+ *
+ * @param (string) Template path.
+ *
+ * @return (string) Template path.
+ */
+add_filter( 'template_include', 'secupress_blackhole_please_click_me', 1 );
+
+function secupress_blackhole_please_click_me( $template ) {
+	$url     = secupress_get_current_url();
+	$dirname = '/secupress-' . substr( wp_hash( 'blackhole', 'nonce' ), 10, 8 ) . '/';
+
+	if ( substr( $url, - strlen( $dirname ) ) === $dirname ) {
+		return dirname( __FILE__ ) . '/inc/php/blackhole/warning-template.php';
+	}
+
+	return $template;
+}
+
+
+/**
+ * Ban an IP address and die.
+ *
+ * @since 1.0
+ */
+add_action( 'admin_post_nopriv_secupress-ban-me-please', 'secupress_blackhole_ban_ip' );
+
+function secupress_blackhole_ban_ip() {
+	$IP      = secupress_get_ip();
+	$ban_ips = get_site_option( SECUPRESS_BAN_IP );
+
+	if ( ! is_array( $ban_ips ) ) {
+		$ban_ips = array();
+	}
+
+	$ban_ips[ $IP ] = time() + MONTH_IN_SECONDS; // Now you got 1 month to think about your future, kiddo. In the meantime, go clean your room.
+
+	update_site_option( SECUPRESS_BAN_IP, $ban_ips );
+
+	/* This hook is documented in /inc/functions/admin.php */
+	do_action( 'secupress.ip_banned', $IP, $ban_ips );
+
+	if ( apply_filters( 'write_ban_in_htaccess', true ) ) {
+		secupress_write_htaccess( 'ban_ip', secupress_get_htaccess_ban_ip() );
+	}
+
+	$msg = sprintf( __( 'Your IP address %s has been banned.', 'secupress' ), '<code>' . esc_html( $IP ) . '</code>' );
+	secupress_die( $msg );
+}
---
+++ inc/modules/sensitive-data/plugins/inc/php/blackhole/warning-template.php
@@ -0,0 +1,22 @@
+<?php
+defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php echo esc_attr( strtolower( get_bloginfo( 'charset' ) ) ); ?>" />
+		<title><?php esc_html_e( 'STOP', 'secupress' ); ?></title>
+		<meta content="initial-scale=1.0" name="viewport" />
+	</head>
+	<body>
+		<p><?php
+		printf(
+			/* translators: 1 is a file name, 2 is a "click here" link */
+			__( 'The aim of this page is to catch robots that don\'t respect the rules set in the %1$s file. <strong>Don\'t %2$s or you will be banned from this site.</strong>', 'secupress' ),
+			'<code>robots.txt</code>',
+			'<a href="' . esc_url( admin_url( 'admin-post.php?action=secupress-ban-me-please' ) ) . '">' . __( 'click this link', 'secupress' ) . '</a>'
+		);
+		?></p>
+	</body>
+</html><?php
+die();
