7d046ff7c569c258f2de6938d07e1a7bd0b5d86a
Screenfeed <git@screenfeed.fr>
2016-01-28 15:53:28 +0100

    Backups: - use `secupress_is_pro()` in `__secupress_do_backup_db()` to make sure non-pro users can't use the filter `secupress.do_backup.file`. - use `secupress_admin_die()`. - use `secupress_admin_send_response_or_redirect()` when relevant.

--- inc/modules/backups/callbacks.php
+++ inc/modules/backups/callbacks.php
@@ -21,16 +21,14 @@ function __secupress_backups_settings_callback( $settings ) {
 	return $settings;
 }
 
-add_action( 'wp_ajax_secupress_backup_db', '__secupress_do_backup_db' );
+
+add_action( 'wp_ajax_secupress_backup_db',    '__secupress_do_backup_db' );
 add_action( 'admin_post_secupress_backup_db', '__secupress_do_backup_db' );
+
 function __secupress_do_backup_db() {
 
 	if ( ! isset( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'secupress_backup_db' ) ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	$wp_tables      = secupress_get_wp_tables();
@@ -38,23 +36,19 @@ function __secupress_do_backup_db() {
 	$backup_storage = secupress_get_module_option( 'backups-storage_location', 'local', 'backups' );
 	$backup_file    = '';
 
-	if ( 'local' == $backup_storage ) {
-		$backup_file  = secupress_get_backup_path() . secupress_get_db_backup_filename();
+	if ( 'local' === $backup_storage ) {
+		$backup_file = secupress_get_backup_path() . secupress_get_db_backup_filename();
 
 		if ( secupress_pre_backup() ) {
 			file_put_contents( $backup_file, secupress_get_db_tables_content( array_merge( $wp_tables, $other_tables ) ) );
 			$backup_file = secupress_zip_backup_file( $backup_file );
 		}
-	} else {
+	} elseif ( secupress_is_pro() ) {
 		$backup_file = apply_filters( 'secupress.do_backup.file', $backup_file, $backup_storage );
 	}
 
 	if ( ! $backup_file ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
@@ -65,8 +59,10 @@ function __secupress_do_backup_db() {
 	}
 }
 
+
 // No AJAX support needed here
 add_action( 'admin_post_secupress_download_backup', '__secupress_download_backup_ajax_post_cb' );
+
 function __secupress_download_backup_ajax_post_cb() {
 
 	if ( ! isset( $_GET['_wpnonce'], $_GET['file'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'secupress_download_backup-' . $_GET['file'] ) ) {
@@ -74,15 +70,17 @@ function __secupress_download_backup_ajax_post_cb() {
 	}
 
 	$file = glob( secupress_get_backup_path() . '*' . $_GET['file'] . '*.{zip,sql}', GLOB_BRACE );
+
 	if ( $file ) {
 		$file = reset( $file );
 	} else {
 		wp_nonce_ays( '' );
 	}
 
-	if( ini_get( 'zlib.output_compression' ) ) {
+	if ( ini_get( 'zlib.output_compression' ) ) {
 		ini_set( 'zlib.output_compression', 'Off' );
 	}
+
 	header( 'Pragma: public' );
 	header( 'Expires: 0' );
 	header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0');
@@ -95,72 +93,48 @@ function __secupress_download_backup_ajax_post_cb() {
 	header( 'Connection: close' );
 	readfile($file);
 	die();
-
 }
 
 
-add_action( 'wp_ajax_secupress_delete_backup', '__secupress_delete_backup_ajax_post_cb' );
+add_action( 'wp_ajax_secupress_delete_backup',    '__secupress_delete_backup_ajax_post_cb' );
 add_action( 'admin_post_secupress_delete_backup', '__secupress_delete_backup_ajax_post_cb' );
+
 function __secupress_delete_backup_ajax_post_cb() {
 
 	if ( ! isset( $_GET['_wpnonce'], $_GET['file'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'secupress_delete_backup-' . $_GET['file'] ) ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	$files = glob( secupress_get_backup_path() . '*' . $_GET['file'] . '*.{zip,sql}', GLOB_BRACE );
+
 	if ( ! $files ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	@array_map( 'unlink', $files );
 
-	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-		wp_send_json_success( $_GET['file'] );
-	} else {
-		wp_redirect( wp_get_referer() );
-		die();
-	}
-
+	secupress_admin_send_response_or_redirect( $_GET['file'] );
 }
 
-add_action( 'wp_ajax_secupress_delete_backups', '__secupress_delete_backups_ajax_post_cb' );
+
+add_action( 'wp_ajax_secupress_delete_backups',    '__secupress_delete_backups_ajax_post_cb' );
 add_action( 'admin_post_secupress_delete_backups', '__secupress_delete_backups_ajax_post_cb' );
+
 function __secupress_delete_backups_ajax_post_cb() {
 
 	if ( ! isset( $_GET['_wpnonce'] ) || ! wp_verify_nonce( $_GET['_wpnonce'], 'secupress_delete_backups' ) ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	$files = glob( secupress_get_backup_path() . '*.{zip,sql}', GLOB_BRACE );
+
 	if ( ! $files ) {
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			wp_send_json_error();
-		} else {
-			wp_nonce_ays( '' );
-		}
+		secupress_admin_die();
 	}
 
 	@array_map( 'unlink', $files );
 
-	if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-		wp_send_json_success();
-	} else {
-		wp_redirect( wp_get_referer() );
-		die();
-	}
-
+	secupress_admin_send_response_or_redirect( 1 );
 }
 
 
