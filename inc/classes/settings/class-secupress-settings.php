483a814beb9dc250c3ce5f146bc64b15a08cf896
Julio Potier <juliobosk@gmail.com>
2016-01-29 11:20:33 +0100

    2nd hand on the settings UI, still needs to be modified, no worry :)

--- inc/classes/settings/class-secupress-settings.php
+++ inc/classes/settings/class-secupress-settings.php
@@ -886,7 +886,13 @@ abstract class SecuPress_Settings extends SecuPress_Singleton {
 	 * @since 1.0
 	 */
 	protected function file_scanner() {
-		echo '<a href="' . wp_nonce_url( admin_url( 'admin-post.php?action=secupress_get_file_scan' ), 'secupress_get_file_scan' ) . '" class="button button-secondary">' . __( 'Search for modified files', 'secupress' ) . '</a>';
+		if ( false !== ( $time = get_transient( 'secupress_run_file_scan' ) ) ) {
+			echo '<p class="working">' . sprintf( __( 'Job running since %s.', 'secupress' ), human_time_diff( $time, time() ) ) . ' <span class="secupress-inline-spinner"></span></p>';
+			echo '<p><a href="' . wp_nonce_url( admin_url( 'admin-post.php?action=secupress_stop_file_scan' ), 'secupress_stop_file_scan' ) . '" class="button button-secondary">' . __( 'Stop task', 'secupress' ) . '</a></p>';
+		} else {
+			echo '<p>' . __( 'No job running.', 'secupress' ) . '</p>';
+			echo '<p><a href="' . wp_nonce_url( admin_url( 'admin-post.php?action=secupress_run_file_scan' ), 'secupress_run_file_scan' ) . '" class="button button-secondary">' . __( 'Search for modified files', 'secupress' ) . '</a></p>';
+		}
 	}
 
 
