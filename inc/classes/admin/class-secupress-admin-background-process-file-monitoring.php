a4500c5b538461459e3619a86168ccd092de2946
Julio Potier <juliobosk@gmail.com>
2016-02-10 11:27:03 +0100

    Get core file hashes from distant .zip when needed

--- inc/classes/admin/class-secupress-admin-background-process-file-monitoring.php
+++ inc/classes/admin/class-secupress-admin-background-process-file-monitoring.php
@@ -18,11 +18,11 @@ class SecuPress_Background_Process_File_Monitoring extends WP_Background_Process
 
 		$version = $version ? $version : $wp_version;
 
-		if ( false !== ( $result = get_option( self::WP_CORE_FILES_HASHES ) ) && isset( $result[ $version ] ) ) {
+		if ( false !== ( $result = get_option( self::WP_CORE_FILES_HASHES ) ) && isset( $result[ $version ]['checksums'] ) ) {
 			return $result[ $version ];
 		}
 
-		update_option( self::WP_CORE_FILES_HASHES, array() ); // do this early to act as a lock toa void multiple background process
+		update_option( self::WP_CORE_FILES_HASHES, array() ); // do this early to act as a lock to avoid multiple background processes
 
 		$result = array( $version => array() );
 		$locale = isset( $wp_local_package ) ? $wp_local_package : 'en_US';
@@ -44,16 +44,47 @@ class SecuPress_Background_Process_File_Monitoring extends WP_Background_Process
 				$result[ $version ]['url'] = $url;
 				break;
 			}
+			$result[ $version ]['foo'] = 'bar';
 		}
 
-		if ( ! isset( $result[ $version ]['checksums'] ) ) {
-			$result[ $version ]['checksums'] = false;
-			//// from zip
-		} else {
-			update_option( self::WP_CORE_FILES_HASHES, $result );
+		if ( ! isset( $result[ $version ]['checksums'] ) || ! $result[ $version ]['checksums'] ) {
+
+			$file = "http://wordpress.org/wordpress-$version-no-content.zip";
+			$file_md5 = "http://wordpress.org/wordpress-$version.zip.md5";
+			$response = wp_remote_get( $file_md5 );
+			if ( ! is_wp_error( $response ) && 200 == wp_remote_retrieve_response_code( $response ) ) {
+				$zip_md5 = wp_remote_retrieve_body( $response );
+				require_once( ABSPATH . 'wp-admin/includes/file.php' );
+				$tmpfname = download_url( $file );
+				$result[ $version ]['foo'] = $tmpfname;
+				if ( ! is_wp_error( $tmpfname ) && is_readable( $tmpfname ) ) {
+					$file = $tmpfname;
+
+					$result[ $version ]['checksums'] = array();
+					$zip = zip_open( $file );
+					if ( is_resource( $zip ) ) {
+						while ( $zip_entry = zip_read( $zip ) ) {
+							zip_entry_open( $zip, $zip_entry, "r" );
+							$zfile = zip_entry_read( $zip_entry, zip_entry_filesize( $zip_entry ) );
+							list( $wp, $filename ) = explode( '/', zip_entry_name( $zip_entry ), 2 );
+							if ( $filename ) {
+								$md5tmp = md5( $zfile );
+								$result[ $version ]['checksums'][ $filename ] = $md5tmp;
+							}
+							zip_entry_close( $zip_entry );
+						}
+						zip_close( $zip );
+					}
+					unlink( $tmpfname );
+				}
+			} else {
+				$result[ $version ]['checksums'] = $response;
+			}
+
 		}
 
-		// return $result;
+		update_option( self::WP_CORE_FILES_HASHES, $result );
+
 	}
 
 	/**
