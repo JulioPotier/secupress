fb0642b6bea1e89bc3d5c2a12db37a88d83f1f56
Julio Potier <juliobosk@gmail.com>
2016-02-24 11:32:41 +0100

    Replace "file" by" slug"
    the value is not a file like "file.php" or even with a path, but just a
    slug like "file", it confuses me. #maniac

--- inc/functions/modules.php
+++ inc/functions/modules.php
@@ -10,13 +10,13 @@ function secupress_deactivate_submodule( $module, $plugins, $args = array() ) {
 	}
 
 	foreach ( $plugins as $plugin ) {
-		$plugin_file = sanitize_key( $plugin );
+		$plugin_slug = sanitize_key( $plugin );
 
-		if ( ! $active_plugins || ! isset( $active_plugins[ $module ] ) || ! in_array_deep( $plugin_file, $active_plugins ) ) {
+		if ( ! $active_plugins || ! isset( $active_plugins[ $module ] ) || ! in_array_deep( $plugin_slug, $active_plugins ) ) {
 			continue;
 		}
 
-		$key = array_search( $plugin_file, $active_plugins[ $module ] );
+		$key = array_search( $plugin_slug, $active_plugins[ $module ] );
 
 		if ( false === $key ) {
 			continue;
@@ -29,26 +29,26 @@ function secupress_deactivate_submodule( $module, $plugins, $args = array() ) {
 		}
 
 		update_site_option( SECUPRESS_ACTIVE_SUBMODULES, $active_plugins );
-		secupress_add_module_notice( $module, $plugin_file, 'deactivation' );
+		secupress_add_module_notice( $module, $plugin_slug, 'deactivation' );
 
-		do_action( 'secupress_deactivate_plugin_' . $plugin_file, $args );
+		do_action( 'secupress_deactivate_plugin_' . $plugin_slug, $args );
 
-		do_action( 'secupress_deactivate_plugin', $plugin_file, $args );
+		do_action( 'secupress_deactivate_plugin', $plugin_slug, $args );
 	}
 }
 
 
 function secupress_activate_submodule( $module, $plugin, $incompatibles_modules = array() ) { //// add the possiblity to activate it in "silent mode" (from a scanner fix and not from a user checkbox)?
-	$plugin_file    = sanitize_key( $plugin );
+	$plugin_slug    = sanitize_key( $plugin );
 	$active_plugins = get_site_option( SECUPRESS_ACTIVE_SUBMODULES );
 	$active_plugins = is_array( $active_plugins ) ? $active_plugins : array();
-	$file_path      = SECUPRESS_MODULES_PATH . $module . '/plugins/' . $plugin_file . '.php';
+	$file_path      = SECUPRESS_MODULES_PATH . $module . '/plugins/' . $plugin_slug . '.php';
 
 	if ( ! file_exists( $file_path ) ) {
 		return false;
 	}
 
-	if ( ! in_array_deep( $plugin_file, $active_plugins ) ) {
+	if ( ! in_array_deep( $plugin_slug, $active_plugins ) ) {
 		if ( ! empty( $incompatibles_modules ) ) {
 			secupress_deactivate_submodule( $module, $incompatibles_modules );
 
@@ -57,15 +57,15 @@ function secupress_activate_submodule( $module, $plugin, $incompatibles_modules 
 		}
 
 		$active_plugins[ $module ]   = isset( $active_plugins[ $module ] ) ? $active_plugins[ $module ] : array();
-		$active_plugins[ $module ][] = $plugin_file;
+		$active_plugins[ $module ][] = $plugin_slug;
 
 		update_site_option( SECUPRESS_ACTIVE_SUBMODULES, $active_plugins );
 		require_once( $file_path );
-		secupress_add_module_notice( $module, $plugin_file, 'activation' );
+		secupress_add_module_notice( $module, $plugin_slug, 'activation' );
 
-		do_action( 'secupress_activate_plugin_' . $plugin_file );
+		do_action( 'secupress_activate_plugin_' . $plugin_slug );
 
-		do_action( 'secupress_activate_plugin', $plugin_file );
+		do_action( 'secupress_activate_plugin', $plugin_slug );
 
 		return true;
 	}
