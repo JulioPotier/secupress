9db12a0e73f750e16194423bbda88e9094192225
Screenfeed <git@screenfeed.fr>
2016-02-25 17:28:23 +0100

    No Plugin Installation: - Added style to hide "Install" link in page heading. - Removed unnecessarily added `<style>` tag on install page (we can't reach the page, so it's useless).

--- inc/modules/plugins-themes/plugins/plugin-installation.php
+++ inc/modules/plugins-themes/plugins/plugin-installation.php
@@ -11,35 +11,35 @@ defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
 if ( is_admin() ) {
 
 	add_action( 'admin_print_styles-plugins.php', 'secupress_no_plugin_install_tab_css' );
+
 	function secupress_no_plugin_install_tab_css() {
-		?><style>h2 a.add-new-h2{display:none}</style><?php
+		?><style>h1 a.page-title-action,h2 a.add-new-h2{display:none}</style><?php
 	}
 
-	add_action( 'admin_print_styles-plugin-install.php', 'secupress_no_plugin_upload_tab_css' );
-	function secupress_no_plugin_upload_tab_css() {
-		?><style>h2 a.upload{display:none}</style><?php
-	}
 
 	add_action( 'load-plugin-install.php', 'secupress_no_plugin_install_page_redirect' );
+
 	function secupress_no_plugin_install_page_redirect() {
-		if ( ! isset( $_GET['tab'] ) || 'plugin-information' != $_GET['tab'] ) {
+		if ( ! isset( $_GET['tab'] ) || 'plugin-information' !== $_GET['tab'] ) {
 			secupress_die( __( 'You do not have sufficient permissions to install plugins on this site.' ) );
 		}
 	}
 
+
 	add_action( 'check_admin_referer', 'secupress_avoid_install_plugin' );
+
 	function secupress_avoid_install_plugin( $action ) {
 		if ( 'plugin-upload' === $action || strpos( $action, 'install-plugin_' ) === 0 ) {
 			secupress_die( __( 'You do not have sufficient permissions to install plugins on this site.' ) );
 		}
 	}
 
+
 	add_action( 'admin_menu', 'secupress_remove_new_plugins_link', 100 );
+
 	function secupress_remove_new_plugins_link() {
 		global $submenu;
-		if ( isset( $submenu['plugins.php'][10] ) ) {
-			unset( $submenu['plugins.php'][10] );
-		}
+		unset( $submenu['plugins.php'][10] );
 	}
 
 }
