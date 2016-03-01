16106412557e23354e66fa3acc2a496e7cb9ff15
Screenfeed <git@screenfeed.fr>
2016-03-01 15:55:19 +0100

    Blackhole plugin: introduced `secupress_blackhole_is_whitelisted()`, a filterable way to whitelist users/bots.

--- inc/modules/sensitive-data/plugins/blackhole.php
+++ inc/modules/sensitive-data/plugins/blackhole.php
@@ -21,6 +21,10 @@ defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
 add_filter( 'robots_txt', 'secupress_blackhole_robots_txt' );
 
 function secupress_blackhole_robots_txt( $output ) {
+	if ( secupress_blackhole_is_whitelisted() ) {
+		return $output;
+	}
+
 	$dirname = secupress_get_hashed_folder_name( 'blackhole' );
 
 	if ( false !== strpos( $output, "User-agent: *\n" ) ) {
@@ -45,6 +49,10 @@ function secupress_blackhole_robots_txt( $output ) {
 add_filter( 'template_include', 'secupress_blackhole_please_click_me', 1 );
 
 function secupress_blackhole_please_click_me( $template ) {
+	if ( secupress_blackhole_is_whitelisted() ) {
+		return $template;
+	}
+
 	$url     = trailingslashit( secupress_get_current_url() );
 	$dirname = secupress_get_hashed_folder_name( 'blackhole' );
 
@@ -64,6 +72,10 @@ function secupress_blackhole_please_click_me( $template ) {
 add_action( 'admin_post_nopriv_secupress-ban-me-please', 'secupress_blackhole_ban_ip' );
 
 function secupress_blackhole_ban_ip() {
+	if ( secupress_blackhole_is_whitelisted() ) {
+		return;
+	}
+
 	$IP      = secupress_get_ip();
 	$ban_ips = get_site_option( SECUPRESS_BAN_IP );
 
@@ -78,10 +90,39 @@ function secupress_blackhole_ban_ip() {
 	/* This hook is documented in /inc/functions/admin.php */
 	do_action( 'secupress.ip_banned', $IP, $ban_ips );
 
+	/* This hook is documented in /inc/functions/admin.php */
 	if ( apply_filters( 'write_ban_in_htaccess', true ) ) {
 		secupress_write_htaccess( 'ban_ip', secupress_get_htaccess_ban_ip() );
 	}
 
 	$msg = sprintf( __( 'Your IP address %s has been banned.', 'secupress' ), '<code>' . esc_html( $IP ) . '</code>' );
 	secupress_die( $msg );
+}
+
+
+/**
+ * Tell if the current user is whitelisted.
+ *
+ * @since 1.0
+ *
+ * @return (bool) True if whitelisted, false otherwize.
+ */
+function secupress_blackhole_is_whitelisted() {
+	$ip = secupress_get_ip();
+	$ua = ! empty( $_SERVER['HTTP_USER_AGENT'] ) ? esc_html( $_SERVER['HTTP_USER_AGENT'] ) : '';
+
+	if ( '127.0.0.1' === $ip ) {
+		return true;
+	}
+
+	/**
+	 * Filter the "whitelist".
+	 *
+	 * @since 1.0
+	 *
+	 * @param (bool)       True if whitelisted, false otherwize.
+	 * @param (string) $ip The user's IP.
+	 * @param (string) $ua The user's User-Agent.
+	 */
+	return apply_filters( 'secupress.plugin.blackhole.is_whitelisted', false, $ip, $ua );
 }
