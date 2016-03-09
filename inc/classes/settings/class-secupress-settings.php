b3048343bdf14c4f746e2e698a09c1968b0bbe5a
Julio Potier <juliobosk@gmail.com>
2016-03-09 17:35:02 +0100

    Replace " " by "_" in #anchors for "learn more fix"

--- inc/classes/settings/class-secupress-settings.php
+++ inc/classes/settings/class-secupress-settings.php
@@ -1210,10 +1210,10 @@ abstract class SecuPress_Settings extends SecuPress_Singleton {
 					// Row title.
 					if ( $field['title'] ) {
 						if ( ! empty( $field['args']['label_for'] ) ) {
-							echo '<h4 class="throw" id="' . sanitize_html_class( $field['title'] ) . '">' . $field['title'] . '</h4>';
+							echo '<h4 class="throw" id="' . sanitize_html_class( str_replace( ' ', '_', $field['title'] ) ) . '">' . $field['title'] . '</h4>';
 							echo '<label for="' . esc_attr( $field['args']['label_for'] ) . '">' . $field['title'] . '</label>';
 						} else {
-							echo '<h4 id="' . sanitize_html_class( $field['title'] ) . '">' . $field['title'] . '</h4>';
+							echo '<h4 id="' . sanitize_html_class( str_replace( ' ', '_', $field['title'] ) ) . '">' . $field['title'] . '</h4>';
 						}
 					}
 					// Row description.
