2a300ef0202996a5ef01049f8211573bc2233cf9
Screenfeed <git@screenfeed.fr>
2016-03-10 14:44:28 +0100

    Solve revert conflict.

--- inc/classes/scanners/class-secupress-scan-admin-as-author.php
+++ inc/classes/scanners/class-secupress-scan-admin-as-author.php
@@ -24,7 +24,7 @@ class SecuPress_Scan_Admin_As_Author extends SecuPress_Scan implements iSecuPres
 		self::$type     = 'WordPress';
 		self::$title    = __( 'Check if any Administrator already created public posts.', 'secupress' );
 		self::$more     = __( 'The <strong>Administrator</strong> role is to fully manage the website but not to create posts. There are other roles for that like <strong>Editor</strong> or <strong>Author</strong>. But mainly, it means that your Administrator account is always logged in. An attacker could then perform actions on your behalf (<abbr title="Cross-Site Request Forgery">CSRF</abbr> flaw).', 'secupress' );
-		self::$more_fix = __( 'This will ask you to assign the administrators\' posts to other users (and create them if needed), or downgrade those administrators as Editors.', 'secupress' );
+		self::$more_fix = __( 'This will ask you to assign the administrators\' posts to another user (and create it if needed), or downgrade this administrator as an Editor.', 'secupress' );
 	}
 
 
@@ -111,6 +111,12 @@ class SecuPress_Scan_Admin_As_Author extends SecuPress_Scan implements iSecuPres
 	 */
 	public function fix() {
 
+		// MULTISITE ===============
+		if ( $this->is_network_admin() ) {
+			$this->fix_network();
+			return parent::fix();
+		}
+
 		// MONOSITE ================
 		$admins = static::get_admins( 'user_login' );
 
@@ -153,6 +159,12 @@ class SecuPress_Scan_Admin_As_Author extends SecuPress_Scan implements iSecuPres
 	 * Manual fixes.
 	 */
 	public function manual_fix() {
+
+		// MULTISITE ===============
+		if ( $this->is_network_admin() ) {
+			$this->fix_network();
+			return parent::manual_fix();
+		}
 
 		// MONOSITE ================
 		if ( $this->has_fix_action_part( 'admin-as-author' ) ) {
@@ -907,8 +919,17 @@ class SecuPress_Scan_Admin_As_Author extends SecuPress_Scan implements iSecuPres
 
 			// bad
 			$this->add_message( 201, array( count( $blog_names ), $blog_names ) );
+		} else {
+			// good
+			$this->add_message( 0 );
+		}
+	}
 
-			// Messages for sub-sites.
+
+	protected function fix_network() {
+		$admins = static::get_usernames_per_blog();
+
+		if ( $admins ) {
 			$blogs = static::get_blog_ids();
 
 			foreach ( $blogs as $site_id ) {
@@ -922,14 +943,13 @@ class SecuPress_Scan_Admin_As_Author extends SecuPress_Scan implements iSecuPres
 					$this->set_empty_data_for_subsite( $site_id );
 				}
 			}
-
 			// cantfix
-			$this->add_pre_fix_message( 303 );
+			$this->add_fix_message( 303 );
 		} else {
-			// good
-			$this->add_message( 0 );
 			// Remove all previously stored messages for sub-sites.
 			$this->set_empty_data_for_subsites();
+			// good
+			$this->add_fix_message( 1 );
 		}
 	}
 
