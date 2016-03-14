fa582c02a7f78fcf9b4b3ebb833d2a2d4365c6e1
Screenfeed <git@screenfeed.fr>
2016-03-14 18:34:56 +0100

    Removed unused message.

--- inc/classes/scanners/class-secupress-scan-anti-scanner.php
+++ inc/classes/scanners/class-secupress-scan-anti-scanner.php
@@ -41,8 +41,6 @@ class SecuPress_Scan_Anti_Scanner extends SecuPress_Scan implements iSecuPress_S
 			100 => __( 'Unable to determine status of your homepage.', 'secupress' ),
 			// bad
 			200 => __( 'Your website should block <strong>automated scanning</strong>.', 'secupress' ),
-			// cantfix
-			300 => __( 'I can not fix this, you have to do it yourself, have fun.', 'secupress' ),
 		);
 
 		if ( isset( $message_id ) ) {
