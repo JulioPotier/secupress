5e1be768c501bc4ca14083b76838a5587b9def5c
Screenfeed <git@screenfeed.fr>
2016-03-15 23:02:56 +0100

    `secupress_add_module_notice()`: - user `get_current_user_id()` instead of the global var. - use "site transient" functions, we use them everywhere else.

--- inc/functions/modules.php
+++ inc/functions/modules.php
@@ -101,15 +101,13 @@ function secupress_get_submodule_activations( $module ) {
 
 
 function secupress_add_module_notice( $module, $submodule, $action ) {
-	global $current_user;
-
-	$transient_name = "secupress_module_{$action}_{$current_user->ID}";
-	$submodule_data = secupress_get_module_data( $module , $submodule );
-	$current        = secupress_get_transient( $transient_name );
+	$transient_name = 'secupress_module_' . $action . '_' . get_current_user_id();
+	$submodule_data = secupress_get_module_data( $module, $submodule );
+	$current        = secupress_get_site_transient( $transient_name );
 	$current        = is_array( $current ) ? $current : array();
 	$current[]      = $submodule_data['Name'];
 
-	secupress_set_transient( $transient_name, $current );
+	secupress_set_site_transient( $transient_name, $current );
 
 	do_action( 'module_notice_' . $action, $module, $submodule );
 }
