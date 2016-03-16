d5bb55be94ec8db1cf2f6ef2047330029294b0f6
Julio Potier <juliobosk@gmail.com>
2016-03-16 23:40:22 +0100

    Use `$wpdb->secupress_bruteforce` instead of the constant.

--- inc/modules/firewall/plugins/bruteforce.php
+++ inc/modules/firewall/plugins/bruteforce.php
@@ -9,7 +9,9 @@ Version: 1.0
 
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
-define( 'SECUPRESS_BRUTEFORCE_TABLE', $GLOBALS['wpdb']->prefix . 'secupress_bruteforce' );
+global $wpdb;
+
+$wpdb->secupress_bruteforce = $wpdb->prefix . 'secupress_bruteforce';
 
 /**
  * Create our brute force table to contains every IP address that reach the website 
@@ -22,19 +24,19 @@ define( 'SECUPRESS_BRUTEFORCE_TABLE', $GLOBALS['wpdb']->prefix . 'secupress_brut
 add_action( 'secupress_activate_plugin_' . basename( __FILE__, '.php' ), 'secupress_bruteforce_activation' );
 function secupress_bruteforce_activation() {
 	global $wpdb;
-
-	if ( $wpdb->get_var( 'SHOW TABLES LIKE "' . SECUPRESS_BRUTEFORCE_TABLE .'"' ) != SECUPRESS_BRUTEFORCE_TABLE ) {
-		$sql = 'CREATE TABLE ' . SECUPRESS_BRUTEFORCE_TABLE . ' (
-			id varchar(32) NOT NULL,
-			timestamp bigint(20) NOT NULL,
-			hits bigint(20) DEFAULT 1 NOT NULL,
-			UNIQUE KEY id (id)
-		);';
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-		dbDelta( $sql );
-		update_option( 'secupress_bruteforce_installed', 1 );
-		wp_schedule_event( time() + HOUR_IN_SECONDS, 'hourly', 'secupress_bruteforce_purge_old_timestamps' );
+	if ( $wpdb->get_var( "SHOW TABLES LIKE '$wpdb->secupress_bruteforce'" ) === $wpdb->secupress_bruteforce ) {
+		return;
 	}
+	$sql = "CREATE TABLE $wpdb->secupress_bruteforce (
+		id varchar(32) NOT NULL,
+		timestamp bigint(20) NOT NULL,
+		hits bigint(20) DEFAULT 1 NOT NULL,
+		UNIQUE KEY id (id)
+	);";
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+	update_option( 'secupress_bruteforce_installed', 1 );
+	wp_schedule_event( time() + HOUR_IN_SECONDS, 'hourly', 'secupress_bruteforce_purge_old_timestamps' );
 }
 
 /**
@@ -45,7 +47,7 @@ function secupress_bruteforce_activation() {
  **/
 function secupress_bruteforce_purge_old_timestamps() {
 	global $wpdb;
-	$wpdb->get_row( $wpdb->prepare( 'DELETE FROM ' . SECUPRESS_BRUTEFORCE_TABLE . ' WHERE timestamp < %s', time() ) );
+	$wpdb->get_row( $wpdb->prepare( "DELETE FROM $wpdb->secupress_bruteforce WHERE timestamp < %s", time() ) );
 }
 
 /**
@@ -59,7 +61,10 @@ function secupress_bruteforce_purge_old_timestamps() {
 add_action( 'secupress_deactivate_plugin_' . basename( __FILE__, '.php' ), 'secupress_bruteforce_deactivation' );
 function secupress_bruteforce_deactivation() {
 	global $wpdb;
-	$wpdb->query( 'DROP TABLE ' . SECUPRESS_BRUTEFORCE_TABLE );
+
+	if ( $wpdb->get_var( "SHOW TABLES LIKE '$wpdb->secupress_bruteforce'" ) === $wpdb->secupress_bruteforce ) {
+		$wpdb->query( "DROP TABLE $wpdb->secupress_bruteforce" );
+	}
 	delete_option( 'secupress_bruteforce_installed' );
 	wp_unschedule_event( time(), 'secupress_bruteforce_purge_old_timestamps' );
 }
@@ -72,9 +77,9 @@ function secupress_bruteforce_deactivation() {
  **/
 add_action( 'secupress_plugins_loaded', 'secupress_check_bruteforce' );
 function secupress_check_bruteforce() {
-	global $wpdb;
+	global $wpdb, $pagenow;
 
-	if ( current_user_can( 'administrator' ) || ! get_option( 'secupress_bruteforce_installed' ) ) {
+	if ( current_user_can( 'administrator' ) || ! get_option( 'secupress_bruteforce_installed' ) || defined( 'DOING_AJAX' ) || ( is_admin() && 'admin-post.php' == $pagenow ) ) {
 		return;
 	}
 
@@ -83,8 +88,8 @@ function secupress_check_bruteforce() {
 	$id   = md5( $IP . $time . wp_salt( 'nonce' ) );
 	$hits = secupress_get_module_option( 'bruteforce_request_number', 9, 'firewall' );
 
-	$wpdb->query( $wpdb->prepare( 'INSERT INTO ' . SECUPRESS_BRUTEFORCE_TABLE . ' ( id, timestamp ) VALUES ( %s, %d ) ON DUPLICATE KEY UPDATE hits = hits+1', $id, $time ) );
-	$result = $wpdb->get_row( $wpdb->prepare( 'SELECT * FROM ' . SECUPRESS_BRUTEFORCE_TABLE . ' WHERE id = %s AND timestamp = %d AND hits >= %d LIMIT 1', $id, $time, $hits ) );
+	$wpdb->query( $wpdb->prepare( "INSERT INTO $wpdb->secupress_bruteforce ( id, timestamp ) VALUES ( %s, %d ) ON DUPLICATE KEY UPDATE hits = hits+1", $id, $time ) );
+	$result = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->secupress_bruteforce WHERE id = %s AND timestamp = %d AND hits >= %d LIMIT 1", $id, $time, $hits ) );
 
 	if ( $result ) {
 		/**
@@ -93,7 +98,7 @@ function secupress_check_bruteforce() {
 		 * @since 1.0
 		 */		
 		do_action( 'secupress.plugin.bruteforce.triggered', $IP, $hits, $id );
-		$wpdb->delete( SECUPRESS_BRUTEFORCE_TABLE, array( 'id' => $id ) );
+		$wpdb->delete( $wpdb->secupress_bruteforce, array( 'id' => $id ) );
 		$time_ban = secupress_get_module_option( 'bruteforce_time_ban', 5, 'firewall' );
 		secupress_ban_ip( $time_ban );
 	}
