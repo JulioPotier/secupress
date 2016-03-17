23d39ef12e313dae9eb5e5cc9cc973881eeedcee
Julio Potier <juliobosk@gmail.com>
2016-03-17 16:09:36 +0100

    Remove options for bruteforce module
    Décisions, not options.

--- inc/modules/firewall/settings/bruteforce.php
+++ inc/modules/firewall/settings/bruteforce.php
@@ -10,7 +10,7 @@ $main_field_name = $this->get_field_name( 'activated' );
 
 $this->add_field( array(
 	'title'             => __( 'Use the Anti Bruteforce', 'secupress' ),
-	'description'       => __( 'When a single visitor (IP Address) is hitting hard on your website, like 10 pages per second, we should tell him to go slowly, and if it continues, lock its IP Address.', 'secupress' ),
+	'description'       => __( 'When a single visitor (IP Address) is hitting hard on your website (10 pages per second), we should tell him to go slowly, and if it continues, lock its IP Address.', 'secupress' ),
 	'label_for'         => $main_field_name,
 	'plugin_activation' => true,
 	'type'              => 'checkbox',
@@ -20,40 +20,14 @@ $this->add_field( array(
 		array(
 			'type'        => 'description',
 			'description' => __( 'This will be used on your front-end, back-end and the login form.', 'secupress' ),
+		),		
+		array(
+			'type'        => 'help',
+			'description' => __( 'Requests done by logged in administrators and AJAX requests are not blocked.', 'secupress' ),
 		),
 		array(
 			'type'        => 'warning',
 			'description' => sprintf( __( 'This is NOT an anti login bruteforce, if you want this kind of module, just activate the <a href="%s#logincontrol">Login Attempts Blocker</a>.', 'secupress' ), secupress_admin_url( 'modules', 'users-login' ) ),
 		),
-	),
-) );
-
-
-$this->add_field( array(
-	'title'        => __( 'How many requests per second minimum before blocking?', 'secupress' ),
-	'description'  => sprintf( __( 'Recommended: %s', 'secupress' ), '9' ),
-	'depends'      => $main_field_name,
-	'label_for'    => $this->get_field_name( 'request_number' ),
-	'type'         => 'number',
-	'label_after'  => __( 'requests per second', 'secupress' ),
-	'default'      => '9',
-	'attributes'   => array(
-		'min' => 3,
-		'max' => 1000,
-	),
-) );
-
-
-$this->add_field( array(
-	'title'        => __( 'How long should we ban?', 'secupress' ),
-	'description'  => sprintf( __( 'Recommended: %s', 'secupress' ), '5 - 15' ),
-	'depends'      => $main_field_name,
-	'label_for'    => $this->get_field_name( 'time_ban' ),
-	'type'         => 'number',
-	'label_after'  => _x( 'min', 'minute', 'secupress' ),
-	'default'      => '5',
-	'attributes'   => array(
-		'min' => 1,
-		'max' => 60,
 	),
 ) );
