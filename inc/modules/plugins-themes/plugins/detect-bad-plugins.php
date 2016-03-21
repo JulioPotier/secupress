d0455de28b8b479640bac65216f3fc6f0046dd36
Julio Potier <juliobosk@gmail.com>
2016-03-21 15:50:24 +0100

    New plugin "Detect bad plugins"

---
+++ inc/modules/plugins-themes/plugins/detect-bad-plugins.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Module Name: Detect Bad Plugins
+Description: Detect if a plugin you're using is known as vulnerable
+Main Module: plugins_themes
+Author: SecuPress
+Version: 1.0
+*/
+defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
