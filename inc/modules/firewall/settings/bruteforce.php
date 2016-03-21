440c283a3a4c2d6f07821fe1c682fd23ca3dd411
Screenfeed <git@screenfeed.fr>
2016-03-21 16:09:49 +0100

    Typos + better i18n (I think).

--- inc/modules/firewall/settings/bruteforce.php
+++ inc/modules/firewall/settings/bruteforce.php
@@ -3,24 +3,24 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 
 $this->set_current_section( 'brutefoce' );
-$this->add_section( __( 'Anti Bruteforce Managment', 'secupress' ) );
+$this->add_section( __( 'Anti Bruteforce Management', 'secupress' ) );
 
 
 $main_field_name = $this->get_field_name( 'activated' );
 
 $this->add_field( array(
 	'title'             => __( 'Use the Anti Bruteforce', 'secupress' ),
-	'description'       => __( 'When a single visitor (IP Address) is hitting hard on your website (10 pages per second), we should tell him to go slowly, and if it continues, lock its IP Address.', 'secupress' ),
+	'description'       => __( 'When a single visitor (IP Address) is hitting hard on your website (10 times per second), we should tell him to go slowly, and if he continues, lock his IP Address.', 'secupress' ),
 	'label_for'         => $main_field_name,
 	'plugin_activation' => true,
 	'type'              => 'checkbox',
 	'value'             => (int) secupress_is_submodule_active( 'firewall', 'bruteforce' ),
-	'label'             => __( 'Yes, i want to use the Anti-Bruteforce on my website', 'secupress' ),
+	'label'             => __( 'Yes, I want to use the Anti-Bruteforce on my website', 'secupress' ),
 	'helpers'           => array(
 		array(
 			'type'        => 'description',
 			'description' => __( 'This will be used on your front-end, back-end and the login form.', 'secupress' ),
-		),		
+		),
 		array(
 			'type'        => 'help',
 			'description' => __( 'Requests done by logged in administrators and AJAX requests are not blocked.', 'secupress' ),
