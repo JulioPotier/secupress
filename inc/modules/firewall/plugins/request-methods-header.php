347f22a5093a8db660895d0bf725f259efb9ed19
Julio Potier <juliobosk@gmail.com>
2016-03-21 15:50:08 +0100

    in_array -> ! isset

--- inc/modules/firewall/plugins/request-methods-header.php
+++ inc/modules/firewall/plugins/request-methods-header.php
@@ -11,11 +11,11 @@ defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
 if ( ! defined( 'DOING_CRON' ) ) :
 
 // Block Bad request methods
-$basic_methods = array( 'GET', 'POST', 'HEAD' );
-$rest_methods  = array( 'PUT', 'PATCH', 'DELETE' );
+$basic_methods = array( 'GET' => true, 'POST' => true, 'HEAD' => true );
+$rest_methods  = array( 'PUT' => true, 'PATCH' => true, 'DELETE' => true );
 $methods       = secupress_is_submodule_active( 'sensitive-data', 'restapi' ) ? array_merge( $basic_methods, $rest_methods ) : $basic_methods;
 
-if ( ! in_array( $_SERVER['REQUEST_METHOD'], $methods ) ) {
+if ( ! isset( $methods[ $_SERVER['REQUEST_METHOD'] ] ) ) {
 	secupress_block( 'RMHM', 405 );
 }
 
