9b64fd1730c6c43b532baf15a98f7baf84b5e880
Julio Potier <juliobosk@gmail.com>
2016-03-31 00:58:07 +0200

    Functions for "detect bad plugins/themes"
    Should be used for the scan now

---
+++ inc/modules/plugins-themes/tools.php
@@ -0,0 +1,113 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin\' uh?' );
+
+/**
+ * Get the plugins removed from repo from our local file
+ *
+ * @return array The plugins removed from the repo
+ * @since 1.0
+ **/
+function secupress_get_removed_plugins() {
+	static $removed_plugins;
+
+	if ( isset( $removed_plugins ) ) {
+		return $removed_plugins;
+	}
+
+	if ( false !== ( $from_transient = get_site_transient( __FUNCTION__ ) ) ) {
+		return $from_transient;
+	}
+
+	$plugins_list_file = SECUPRESS_INC_PATH . 'data/no-longer-in-directory-plugin-list.data';
+	if ( is_readable( $plugins_list_file ) ) {
+		$removed_plugins = array_flip( array_map( 'trim', file( $plugins_list_file ) ) );
+	}
+	$all_plugins = array_keys( get_plugins() );
+	$all_plugins = array_map( 'dirname', $all_plugins );
+	$all_plugins = array_flip( $all_plugins );
+	$all_plugins = array_intersect_key( $all_plugins, $removed_plugins );
+
+	$removed_plugins = $all_plugins;
+	set_site_transient( __FUNCTION__, $removed_plugins, 6 * HOUR_IN_SECONDS );
+
+	return $removed_plugins;
+}
+
+/**
+ * Get the plugins not update since 2 years from repo from our local file
+ *
+ * @return array The plugins not update since 2 years from the repo
+ * @since 1.0
+ **/
+function secupress_get_notupdated_plugins() {
+	static $notupdated_plugins;
+
+	if ( isset( $notupdated_plugins ) ) {
+		return $notupdated_plugins;
+	}
+
+	if ( false !== ( $from_transient = get_site_transient( __FUNCTION__ ) ) ) {
+		return $from_transient;
+	}
+
+	$plugins_list_file = SECUPRESS_INC_PATH . 'data/not-updated-in-over-two-years-plugin-list.data';
+	if ( is_readable( $plugins_list_file ) ) {
+		$notupdated_plugins = array_flip( array_map( 'trim', file( $plugins_list_file ) ) );
+	}
+
+	$all_plugins = array_keys( get_plugins() );
+	$all_plugins = array_map( 'dirname', $all_plugins );
+	$all_plugins = array_flip( $all_plugins );
+	$all_plugins = array_intersect_key( $all_plugins, $notupdated_plugins );
+
+	$notupdated_plugins = $all_plugins;
+	set_site_transient( __FUNCTION__, $notupdated_plugins, 6 * HOUR_IN_SECONDS );
+
+	return $notupdated_plugins;
+}
+
+/**
+ * Get the plugins vulnerable from an option, from our option, set by secupress_refresh_vulnerable_plugins()
+ *
+ * @return array The vulnerables plugins
+ * @since 1.0
+ **/
+function secupress_get_vulnerable_plugins() {
+	static $vulnerable_plugins;
+
+	if ( isset( $vulnerable_plugins ) ) {
+		return $vulnerable_plugins;
+	}
+
+	if ( false !== ( $from_transient = get_site_transient( __FUNCTION__ ) ) ) {
+		return $from_transient;
+	}
+
+	$temp = get_site_option( 'secupress_bad_plugins' );
+	$temp = $temp ? (array) json_decode( $temp ) : array();
+
+	if ( $temp ) {
+		$vulnerable_plugins = $temp;
+		set_site_transient( __FUNCTION__, $vulnerable_plugins, 6 * HOUR_IN_SECONDS );
+		return $vulnerable_plugins;
+	}
+
+	return array();
+}
+
+/**
+ * Get the plugins vulnerable from an option, set 
+ *
+ * @return array The plugins removed from the repo
+ * @since 1.0
+ **/
+function secupress_refresh_vulnerable_plugins() { //// GO PRO
+	$plugins  = get_plugins();
+	$plugins  = wp_list_pluck( $plugins, 'Version' );
+	$args     = array( 'body' => array( 'plugins' => $plugins ), 'headers' => array( 'X-Secupress' => SECUPRESS_VERSION ) ); //// use client licence here
+
+	$response = wp_remote_post( SECUPRESS_WEB_DEMO . '/wpvulndb.php', $args ); //// url temp
+	if ( ! is_wp_error( $response ) && 200 === wp_remote_retrieve_response_code( $response ) ) {
+		update_site_option( 'secupress_bad_plugins', wp_remote_retrieve_body( $response ) );
+	}
+}
