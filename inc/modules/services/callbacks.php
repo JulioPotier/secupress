457abc93eec19830a7055cb2411bb0bca6490dc7
Julio Potier <juliobosk@gmail.com>
2016-04-06 14:07:48 +0200

    Add the callbacks for "services"
    //// todo send support form

---
+++ inc/modules/services/callbacks.php
@@ -0,0 +1,30 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+/*------------------------------------------------------------------------------------------------*/
+/* ON MODULE SETTINGS SAVE ====================================================================== */
+/*------------------------------------------------------------------------------------------------*/
+
+/**
+ * Callback to filter, sanitize, validate and de/activate submodules.
+ *
+ * @since 1.0
+ *
+ * @param (array) $settings The module settings.
+ *
+ * @return (array) The sanitized and validated settings.
+ */
+function __secupress_services_settings_callback( $settings ) {
+	$modulenow = 'alerts';
+	$settings  = $settings ? $settings : array();
+
+	if ( isset( $settings['sanitized'] ) ) {
+		return $settings;
+	}
+
+	$settings = array( 'sanitized' => 1 );
+
+	//// Send support request here
+
+	return $settings;
+}
