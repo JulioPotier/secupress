934b7bc4eb2b39f0680c5fef95e5796d9033057e
Screenfeed <git@screenfeed.fr>
2016-04-07 01:11:38 +0200

    `array_map()` is less dumb >_>

--- uninstall.php
+++ uninstall.php
@@ -6,50 +6,25 @@ global $wpdb;
 
 // Transients.
 $transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_transient_secupress_%'" );
-
-if ( $transients ) {
-	foreach ( $transients as $option_name ) {
-		delete_transient( $option_name );
-	}
-}
+array_map( 'delete_transient', $transients );
 
 // Site transients.
 $transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_site_transient_secupress_%'" );
-
-if ( $transients ) {
-	foreach ( $transients as $option_name ) {
-		delete_site_transient( $option_name );
-	}
-}
+array_map( 'delete_site_transient', $transients );
 
 if ( is_multisite() ) {
 	$transients = $wpdb->get_col( "SELECT meta_key FROM $wpdb->sitemeta WHERE meta_key LIKE '_site_transient_secupress_%'" );
-
-	if ( $transients ) {
-		foreach ( $transients as $option_name ) {
-			delete_site_transient( $option_name );
-		}
-	}
+	array_map( 'delete_site_transient', $transients );
 }
 
 // Options.
 $options = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE 'secupress_%'" );
-
-if ( $options ) {
-	foreach ( $options as $option_name ) {
-		delete_option( $option_name );
-	}
-}
+array_map( 'delete_option', $options );
 
 if ( is_multisite() ) {
 	// Site options
 	$options = $wpdb->get_col( "SELECT meta_key FROM $wpdb->sitemeta WHERE meta_key LIKE 'secupress_%'" );
-
-	if ( $options ) {
-		foreach ( $options as $option_name ) {
-			delete_site_option( $option_name );
-		}
-	}
+	array_map( 'delete_site_option', $options );
 }
 
 // User metas.
