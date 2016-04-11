4a2a83013908b01502ce73465ff78549eb7e6155
Screenfeed <git@screenfeed.fr>
2016-04-11 18:07:23 +0200

    Uninstall: also remove transients beginning with `secupress-`.

--- uninstall.php
+++ uninstall.php
@@ -5,15 +5,15 @@ defined( 'WP_UNINSTALL_PLUGIN' ) or die( 'Cheatin&#8217; uh?' );
 global $wpdb;
 
 // Transients.
-$transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_transient_secupress_%'" );
+$transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_transient_secupress_%' OR option_name LIKE '_transient_secupress-%'" );
 array_map( 'delete_transient', $transients );
 
 // Site transients.
-$transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_site_transient_secupress_%'" );
+$transients = $wpdb->get_col( "SELECT option_name FROM $wpdb->options WHERE option_name LIKE '_site_transient_secupress_%' OR option_name LIKE '_site_transient_secupress-%'" );
 array_map( 'delete_site_transient', $transients );
 
 if ( is_multisite() ) {
-	$transients = $wpdb->get_col( "SELECT meta_key FROM $wpdb->sitemeta WHERE meta_key LIKE '_site_transient_secupress_%'" );
+	$transients = $wpdb->get_col( "SELECT meta_key FROM $wpdb->sitemeta WHERE meta_key LIKE '_site_transient_secupress_%' OR meta_key LIKE '_site_transient_secupress-%'" );
 	array_map( 'delete_site_transient', $transients );
 }
 
