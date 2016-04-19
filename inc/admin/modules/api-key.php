8fe890083c53e1581d53766a34bd9af8f4edc6b1
Screenfeed <git@screenfeed.fr>
2016-04-19 16:22:52 +0200

    Improved Pro API key fields.

--- inc/admin/modules/api-key.php
+++ inc/admin/modules/api-key.php
@@ -2,21 +2,18 @@
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 
-if ( ! function_exists( '__secupress_how_to_get_licence' ) ) {
-	function __secupress_how_to_get_licence() {
-		$description = array(
-			'<strong>' . __( 'Why an API key?', 'secupress' ) . '</strong>',
-			__( 'The API Key is needed for our module linked to the services depending of our servers, like <em>website monitoring</em> or <em>plugins vulnerability check discovery</em> in live.', 'secupress' ),
-			sprintf( __( '%s or just <strong>enter your email address</strong> below and <strong>save</strong> to get a free account.', 'secupress' ), '<a href="" target="_blank">' . __( 'Buy a licence to unlock all the features', 'secupress' ) . '</a>' ),
-		);
-
-		return implode( "<br/>\n", $description );
-	}
-}
+$description = implode( "<br/>\n", array(
+	__( 'Some features, like <em>Website Monitoring</em> need a free account to be created, because they are linked to services provided with our servers.', 'secupress' ),
+	sprintf(
+		/* translators: %s is a "Buy a licence" link */
+		__( 'You can %s, or only <strong>enter your email address</strong> below and <strong>save</strong> to get your free account.', 'secupress' ),
+		'<a href="#" target="_blank">' . __( 'buy a licence to unlock all the features', 'secupress' ) . '</a>'////
+	),
+) );
 
 
 $this->set_current_section( 'secupress_display_apikey_options' );
-$this->set_section_description( __secupress_how_to_get_licence() );
+$this->set_section_description( $description );
 $this->add_section( __( 'License validation', 'secupress' ) );
 
 
@@ -24,25 +21,30 @@ $this->add_field( array(
 	'title'        => __( 'E-mail Address', 'secupress' ),
 	'label_for'    => 'consumer_email',
 	'type'         => 'email',
-	'attributes'   => array( 'autocomplete' => 'off', ),
+	'attributes'   => array(
+		'required'      => 'required',
+		'aria-required' => 'true',
+	),
 	'helpers'      => array(
 		array(
 			'type'        => 'help',
-			'description' => __( 'The one you got with your account or enter your own and save to get a free account.', 'secupress' ),
+			'description' => __( 'The one you used for your Pro account. Or provide a new one to create a free account.', 'secupress' ),
 		),
 	),
 ) );
 
 
 $this->add_field( array(
-	'title'        => __( 'API Key', 'secupress' ),
+	'title'        => __( 'Pro API Key', 'secupress' ),
 	'label_for'    => 'consumer_key',
 	'type'         => 'text',
-	'attributes'   => array( 'autocomplete' => 'off', ),
+	'attributes'   => array(
+		'autocomplete' => 'off',
+	),
 	'helpers'      => array(
 		array(
 			'type'        => 'help',
-			'description' => __( 'Please enter the API key obtained with your account or leave blank and save to get a free account.', 'secupress' ),
+			'description' => __( 'The API key obtained with your Pro account. Leave empty and save to get a free account.', 'secupress' ),
 		),
 	),
 ) );
