7a3a9154fe1766ee525034ca93b9678c3a86089c
Screenfeed <git@screenfeed.fr>
2016-04-22 19:58:34 +0200

    PHPCS 2: - `inc/front/` root.

--- inc/front/common.php
+++ inc/front/common.php
@@ -1,13 +1,12 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
+add_action( 'plugins_loaded', 'secupress_check_ban_ips' );
 /**
  * Will remove expired banned IPs, then block the remaining ones. A form will be displayed to allow clumsy Administrators to unlock themselves.
  *
  * @since 1.0
- **/
-add_action( 'plugins_loaded', 'secupress_check_ban_ips' );
-
+ */
 function secupress_check_ban_ips() {
 	$ban_ips  = get_site_option( SECUPRESS_BAN_IP );
 	$time_ban = (int) secupress_get_module_option( 'login-protection_time_ban', 5, 'users-login' );
@@ -17,9 +16,9 @@ function secupress_check_ban_ips() {
 	// If we got banned ips.
 	if ( $ban_ips && is_array( $ban_ips ) ) {
 		// The link to be unlocked?
-		if ( ! empty( $_GET['action'] ) && 'secupress_self-unban-ip' === $_GET['action'] ) {
+		if ( ! empty( $_GET['action'] ) && 'secupress_self-unban-ip' === $_GET['action'] ) { // WPCS: CSRF ok.
 			$ip     = secupress_get_ip();
-			$result = ! empty( $_GET['_wpnonce'] ) ? wp_verify_nonce( $_GET['_wpnonce'], 'secupress_self-unban-ip-' . $ip ) : false;
+			$result = ! empty( $_GET['_wpnonce'] ) ? wp_verify_nonce( $_GET['_wpnonce'], 'secupress_self-unban-ip-' . $ip ) : false; // WPCS: CSRF ok.
 
 			if ( $result ) {
 				// You're good to go.
@@ -92,7 +91,7 @@ function secupress_check_ban_ips() {
 function secupress_check_ban_ips_maybe_send_unban_email( $ip ) {
 	global $wpdb;
 
-	if ( ! isset( $_POST['email'] ) ) {
+	if ( ! isset( $_POST['email'] ) ) { // WPCS: CSRF ok.
 		return array(
 			'message'      => '',
 			'display_form' => true,
@@ -100,8 +99,8 @@ function secupress_check_ban_ips_maybe_send_unban_email( $ip ) {
 	}
 	// Check nonce and referer.
 	$siteurl = strtolower( set_url_scheme( site_url() ) );
-	$referer = strtolower( wp_unslash( $_POST['_wp_http_referer'] ) );
-	$result  = ! empty( $_POST['_wpnonce'] ) ? wp_verify_nonce( $_POST['_wpnonce'], 'secupress-unban-ip-' . $ip ) : false;
+	$referer = strtolower( wp_unslash( $_POST['_wp_http_referer'] ) ); // WPCS: CSRF ok.
+	$result  = ! empty( $_POST['_wpnonce'] ) ? wp_verify_nonce( $_POST['_wpnonce'], 'secupress-unban-ip-' . $ip ) : false; // WPCS: CSRF ok.
 
 	if ( strpos( $referer, 'http' ) !== 0 ) {
 		$port    = (int) $_SERVER['SERVER_PORT'];
@@ -196,10 +195,10 @@ function secupress_check_ban_ips_maybe_send_unban_email( $ip ) {
  *
  * @since 1.0
  *
- * @param (array) An array with the following:
- *                - (string) $ip       The user IP.
- *                - (int)    $time_ban Banishment duration in minutes. 0 means forever.
- *                - (string) $error    An error text.
+ * @param (array) $args An array with the following:
+ *                      - (string) $ip       The user IP.
+ *                      - (int)    $time_ban Banishment duration in minutes. 0 means forever.
+ *                      - (string) $error    An error text.
  *
  * @return (string) The form.
  */
@@ -230,6 +229,7 @@ function secupress_check_ban_ips_form( $args ) {
 }
 
 
+add_filter( 'secupress.plugin.blacklist_logins_list', 'secupress_maybe_remove_admin_from_blacklist' );
 /**
  * If user registrations are open, the "admin" user should not be blacklisted.
  * This is to avoid a conflict between "admin should exist" and "admin is a blacklisted username".
@@ -240,8 +240,6 @@ function secupress_check_ban_ips_form( $args ) {
  *
  * @return (array) List of usernames minus "admin" if registrations are open.
  */
-add_filter( 'secupress.plugin.blacklist_logins_list', 'secupress_maybe_remove_admin_from_blacklist' );
-
 function secupress_maybe_remove_admin_from_blacklist( $list ) {
 	if ( secupress_users_can_register() ) {
 		$list = array_diff( $list, array( 'admin' ) );
@@ -256,11 +254,11 @@ add_action( 'plugins_loaded', 'secupress_rename_admin_username_logout', 50 );
  * Will rename the "admin" account after the rename-admin-username manual fix
  *
  * @since 1.0
- **/
+ */
 function secupress_rename_admin_username_logout() {
 	global $current_user, $pagenow, $wpdb;
 
-	if ( ! empty( $_POST ) || defined( 'DOING_AJAX' ) || defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_CRON' ) || 'admin-post.php' === $pagenow || ! is_user_logged_in() ) {
+	if ( ! empty( $_POST ) || defined( 'DOING_AJAX' ) || defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_CRON' ) || 'admin-post.php' === $pagenow || ! is_user_logged_in() ) { // WPCS: CSRF ok.
 		return;
 	}
 
@@ -277,7 +275,7 @@ function secupress_rename_admin_username_logout() {
 
 	$current_user = wp_get_current_user();
 
-	if ( (int) $data['ID'] !== (int) $current_user->ID || 'admin' !== $current_user->user_login ) {
+	if ( (int) $current_user->ID !== (int) $data['ID'] || 'admin' !== $current_user->user_login ) {
 		return;
 	}
 
@@ -295,7 +293,8 @@ function secupress_rename_admin_username_logout() {
 
 	// Current user auth cookie is now invalid, log in again is mandatory.
 	wp_clear_auth_cookie();
-	if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min
+
+	if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min.
 		wp_destroy_current_session();
 	}
 
@@ -321,134 +320,166 @@ add_action( 'plugins_loaded', 'secupress_add_cookiehash_muplugin', 50 );
  * Will create a mu plugin to modify the COOKIEHASH constant
  *
  * @since 1.0
- * @return void
- **/
+ */
 function secupress_add_cookiehash_muplugin() {
 	global $current_user, $pagenow, $wpdb;
 
-	$current_user    = wp_get_current_user();
-	$current_user_ID = $current_user->ID;
-
-	if ( empty( $_POST ) && ( ! isset( $pagenow ) || 'admin-post.php' != $pagenow ) && ! defined( 'DOING_AJAX' ) && ! defined( 'DOING_AUTOSAVE' ) && ! defined( 'DOING_CRON' ) &&
-		is_user_logged_in() && $data = secupress_get_site_transient( 'secupress-add-cookiehash-muplugin' )
-	) {
-		secupress_delete_site_transient( 'secupress-add-cookiehash-muplugin' );
-
-		if ( ! is_array( $data ) || ! isset( $data['ID'], $data['username'] ) || $current_user->ID != $data['ID'] ) {
-			return;
-		}
-
-		$contents  = '<?php // Added by SecuPress' . PHP_EOL;
-		$contents .= 'define( \'COOKIEHASH\', md5( __FILE__ . \'' . wp_generate_password( 64 ) . '\' ) );';
-		if ( secupress_create_mu_plugin( 'COOKIEHASH_' . uniqid(), $contents ) ) {
-			wp_clear_auth_cookie();
-			if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min
-				wp_destroy_current_session();
-			}
-
-			$token = md5( time() );
-			secupress_set_site_transient( 'secupress_auto_login_' . $token, array( $data['username'], 'WP_Config' ) );
-
-			wp_safe_redirect( esc_url_raw( add_query_arg( 'secupress_auto_login_token', $token, secupress_get_current_url( 'raw' ) ) ) );
-			die();
-		}
+	if ( ! empty( $_POST ) || defined( 'DOING_AJAX' ) || defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_CRON' ) || 'admin-post.php' === $pagenow || ! is_user_logged_in() ) { // WPCS: CSRF ok.
+		return;
 	}
+
+	$data = secupress_get_site_transient( 'secupress-add-cookiehash-muplugin' );
+
+	if ( ! $data ) {
+		return;
+	}
+
+	if ( ! is_array( $data ) || ! isset( $data['ID'], $data['username'] ) ) {
+		secupress_delete_site_transient( 'secupress-add-cookiehash-muplugin' );
+		return;
+	}
+
+	if ( get_current_user_id() !== (int) $data['ID'] ) {
+		return;
+	}
+
+	secupress_delete_site_transient( 'secupress-add-cookiehash-muplugin' );
+
+	$contents  = '<?php // Added by SecuPress' . PHP_EOL;
+	$contents .= 'define( \'COOKIEHASH\', md5( __FILE__ . \'' . wp_generate_password( 64 ) . '\' ) );';
+
+	if ( ! secupress_create_mu_plugin( 'COOKIEHASH_' . uniqid(), $contents ) ) {
+		return;
+	}
+
+	wp_clear_auth_cookie();
+
+	if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min.
+		wp_destroy_current_session();
+	}
+
+	$token = md5( time() );
+	secupress_set_site_transient( 'secupress_auto_login_' . $token, array( $data['username'], 'WP_Config' ) );
+
+	wp_safe_redirect( esc_url_raw( add_query_arg( 'secupress_auto_login_token', $token, secupress_get_current_url( 'raw' ) ) ) );
+	die();
 }
+
 
 add_action( 'plugins_loaded', 'secupress_add_salt_muplugin', 50 );
 /**
  * Will create a mu plugin to early set the salt keys
  *
  * @since 1.0
- * @return void
- **/
+ */
 function secupress_add_salt_muplugin() {
 	global $current_user, $pagenow, $wpdb;
 
-	$current_user    = wp_get_current_user();
-	$current_user_ID = $current_user->ID;
-	if ( ! defined( 'SECUPRESS_SALT_KEYS_ACTIVE' ) && empty( $_POST ) && ( ! isset( $pagenow ) || 'admin-post.php' != $pagenow ) && ! defined( 'DOING_AJAX' ) && ! defined( 'DOING_AUTOSAVE' ) && ! defined( 'DOING_CRON' ) &&
-		is_user_logged_in() && $data = secupress_get_site_transient( 'secupress-add-salt-muplugin' )
-	) {
-		secupress_delete_site_transient( 'secupress-add-salt-muplugin' );
-
-		$wpconfig_filename = secupress_find_wpconfig_path();
-		if ( ! is_writable( $wpconfig_filename ) || ! is_array( $data ) || ! isset( $data['ID'], $data['username'] ) || $current_user->ID != $data['ID'] ) {
-			return;
-		}
-
-		$keys = array( 'AUTH_KEY', 'SECURE_AUTH_KEY', 'LOGGED_IN_KEY', 'NONCE_KEY', 'AUTH_SALT', 'SECURE_AUTH_SALT', 'LOGGED_IN_SALT', 'NONCE_SALT', );
-
-		foreach ( $keys as $constant ) {
-			secupress_replace_content( $wpconfig_filename, "/define\(.*('" . $constant . "'|\"" . $constant . "\").*,/", "/*Commented by SecuPress*/ // $0" );
-		}
-
-		$alicia_keys = file_get_contents( SECUPRESS_INC_PATH . 'data/salt-keys.phps' );
-		$alicia_keys = str_replace( array( '{{HASH1}}', '{{HASH2}}' ), array( wp_generate_password( 64, true, true ), wp_generate_password( 64, true, true ) ), $alicia_keys );
-
-		if ( ! $alicia_keys || ! secupress_create_mu_plugin( 'salt_keys_' . uniqid(), $alicia_keys ) ) {
-			return;
-		}
-
-		wp_clear_auth_cookie();
-		if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min
-			wp_destroy_current_session();
-		}
-
-		foreach ( $keys as $constant ) {
-			delete_site_option( $constant );
-		}
-
-		$token = md5( time() );
-		secupress_set_site_transient( 'secupress_auto_login_' . $token, array( $data['username'], 'Salt_Keys' ) );
-
-		wp_safe_redirect( esc_url_raw( add_query_arg( 'secupress_auto_login_token', $token, secupress_get_current_url( 'raw' ) ) ) );
-		die();
+	if ( ! empty( $_POST ) || defined( 'SECUPRESS_SALT_KEYS_ACTIVE' ) || defined( 'DOING_AJAX' ) || defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_CRON' ) || 'admin-post.php' === $pagenow || ! is_user_logged_in() ) { // WPCS: CSRF ok.
+		return;
 	}
+
+	$data = secupress_get_site_transient( 'secupress-add-salt-muplugin' );
+
+	if ( ! $data ) {
+		return;
+	}
+
+	if ( ! is_array( $data ) || ! isset( $data['ID'], $data['username'] ) ) {
+		secupress_delete_site_transient( 'secupress-add-salt-muplugin' );
+		return;
+	}
+
+	if ( get_current_user_id() !== (int) $data['ID'] ) {
+		return;
+	}
+
+	secupress_delete_site_transient( 'secupress-add-salt-muplugin' );
+
+	$wpconfig_filename = secupress_find_wpconfig_path();
+
+	if ( ! is_writable( $wpconfig_filename ) ) {
+		return;
+	}
+
+	$keys = array( 'AUTH_KEY', 'SECURE_AUTH_KEY', 'LOGGED_IN_KEY', 'NONCE_KEY', 'AUTH_SALT', 'SECURE_AUTH_SALT', 'LOGGED_IN_SALT', 'NONCE_SALT' );
+
+	foreach ( $keys as $constant ) {
+		secupress_replace_content( $wpconfig_filename, '/define\(.*(\'' . $constant . '\'|"' . $constant . '").*,/', '/*Commented by SecuPress*/ // $0' );
+	}
+
+	$alicia_keys = file_get_contents( SECUPRESS_INC_PATH . 'data/salt-keys.phps' );
+	$alicia_keys = str_replace( array( '{{HASH1}}', '{{HASH2}}' ), array( wp_generate_password( 64, true, true ), wp_generate_password( 64, true, true ) ), $alicia_keys );
+
+	if ( ! $alicia_keys || ! secupress_create_mu_plugin( 'salt_keys_' . uniqid(), $alicia_keys ) ) {
+		return;
+	}
+
+	wp_clear_auth_cookie();
+	if ( function_exists( 'wp_destroy_current_session' ) ) { // WP 4.0 min.
+		wp_destroy_current_session();
+	}
+
+	foreach ( $keys as $constant ) {
+		delete_site_option( $constant );
+	}
+
+	$token = md5( time() );
+	secupress_set_site_transient( 'secupress_auto_login_' . $token, array( $data['username'], 'Salt_Keys' ) );
+
+	wp_safe_redirect( esc_url_raw( add_query_arg( 'secupress_auto_login_token', $token, secupress_get_current_url( 'raw' ) ) ) );
+	die();
 }
+
 
 add_action( 'plugins_loaded', 'secupress_auto_username_login', 60 );
 /**
  * Will autologin the user found in the transient 'secupress_auto_login_' . $_GET['secupress_auto_login_token']
  *
  * @since 1.0
- * @return void
  */
-
 function secupress_auto_username_login() {
-
-	if ( isset( $_GET['secupress_auto_login_token'] ) ) {
-
-		list( $username, $action ) = secupress_get_site_transient( 'secupress_auto_login_' . $_GET['secupress_auto_login_token'] );
-
-		secupress_delete_site_transient( 'secupress_auto_login_' . $_GET['secupress_auto_login_token'] );
-
-		if ( $username ) {
-
-			add_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
-			$user = wp_signon( array( 'user_login' => $username ) );
-			remove_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
-
-			if ( is_a( $user, 'WP_User' ) ) {
-				wp_set_current_user( $user->ID, $user->user_login );
-				wp_set_auth_cookie( $user->ID );
-			}
-
-			if ( $action ) {
-				secupress_scanit( $action );
-			}
-			wp_safe_redirect( esc_url_raw( remove_query_arg( 'secupress_auto_login_token', secupress_get_current_url( 'raw' ) ) ) );
-			die();
-		}
+	if ( ! isset( $_GET['secupress_auto_login_token'] ) ) {
+		return;
 	}
+
+	list( $username, $action ) = secupress_get_site_transient( 'secupress_auto_login_' . $_GET['secupress_auto_login_token'] );
+
+	secupress_delete_site_transient( 'secupress_auto_login_' . $_GET['secupress_auto_login_token'] );
+
+	if ( ! $username ) {
+		return;
+	}
+
+	add_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
+	$user = wp_signon( array( 'user_login' => $username ) );
+	remove_filter( 'authenticate', '__secupress_give_him_a_user', 1, 2 );
+
+	if ( is_a( $user, 'WP_User' ) ) {
+		wp_set_current_user( $user->ID, $user->user_login );
+		wp_set_auth_cookie( $user->ID );
+	}
+
+	if ( $action ) {
+		secupress_scanit( $action );
+	}
+
+	wp_safe_redirect( esc_url_raw( remove_query_arg( 'secupress_auto_login_token', secupress_get_current_url( 'raw' ) ) ) );
+	die();
 }
+
 
 /**
  * Used in secupress_rename_admin_username_login() to force a user when auto authenticating
  *
- * @return WP_User/false
  * @since 1.0
- **/
+ *
+ * @param (null|object) $user     WP_User object if the user is authenticated.
+ *                                WP_Error object or null otherwise.
+ * @param (string)      $username Username or email address.
+ *
+ * @return (object|bool) A WP_User object or false.
+ */
 function __secupress_give_him_a_user( $user, $username ) {
 	return get_user_by( 'login', $username );
 }
@@ -459,14 +490,13 @@ add_action( 'plugins_loaded', 'secupress_downgrade_author_administrator', 70 );
  * Admin As Author fix: a new Administrator account has been created, now we need to downgrade the old one.
  *
  * @since 1.0
- * @return void
- **/
+ */
 function secupress_downgrade_author_administrator() {
 	if ( ! is_admin() ) {
 		return;
 	}
 
-	// "{$new_user_id}|{$old_user_id}"
+	// "{$new_user_id}|{$old_user_id}".
 	$data = secupress_get_site_transient( 'secupress-admin-as-author-administrator' );
 
 	// Nope.
@@ -475,7 +505,7 @@ function secupress_downgrade_author_administrator() {
 	}
 
 	if ( ! is_string( $data ) ) {
-		// Dafuk
+		// Dafuk.
 		secupress_delete_site_transient( 'secupress-admin-as-author-administrator' );
 		return;
 	}
@@ -483,19 +513,19 @@ function secupress_downgrade_author_administrator() {
 	list( $new_user_id, $old_user_id ) = array_map( 'absint', explode( '|', $data ) );
 
 	if ( ! isset( $new_user_id, $old_user_id ) || ! $new_user_id || ! $old_user_id || $new_user_id === $old_user_id ) {
-		// Dafuk
+		// Dafuk.
 		secupress_delete_site_transient( 'secupress-admin-as-author-administrator' );
 		return;
 	}
 
 	if ( ! file_exists( secupress_class_path( 'scan', 'Admin_As_Author' ) ) ) {
-		// Dafuk
+		// Dafuk.
 		secupress_delete_site_transient( 'secupress-admin-as-author-administrator' );
 		return;
 	}
 
 	// These aren't the droids you're looking for.
-	if ( $new_user_id !== get_current_user_id() ) {
+	if ( get_current_user_id() !== $new_user_id ) {
 		return;
 	}
 
@@ -517,14 +547,14 @@ function secupress_downgrade_author_administrator() {
 
 	$role = SecuPress_Scan_Admin_As_Author::get_new_role();
 
-	/*
+	/**
 	 * No suitable user role: create one (who the fuck deleted it?!).
 	 */
 	if ( ! $role ) {
 		$role = SecuPress_Scan_Admin_As_Author::create_editor_role();
 
 		if ( ! $role ) {
-			// the user role could not be created.
+			// The user role could not be created.
 			return;
 		}
 
@@ -549,7 +579,11 @@ function secupress_downgrade_author_administrator() {
 
 
 add_action( 'secupress_loaded', '__secupress_process_file_monitoring_tasks' );
-
+/**
+ * Launch file monitoring in background.
+ *
+ * @since 1.0
+ */
 function __secupress_process_file_monitoring_tasks() {
 	if ( false === secupress_get_site_transient( 'secupress_toggle_file_scan' ) ) {
 		return;
