77ee67cff9867364cdab0da215c7e3e9ccd2577a
Screenfeed <git@screenfeed.fr>
2016-04-22 20:45:57 +0200

    - PHPCS 3: `inc/functions/admin.php/`. - Removed useless `inc/functions/index.php` file. - Removed unused functions `secupress_renew_all_boxes()`, `secupress_renew_box()`, `secupress_dismiss_box()`, `secupress_generate_password()`, and `secupress_generate_backupcodes()`.

--- inc/functions/admin.php
+++ inc/functions/admin.php
@@ -2,9 +2,13 @@
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 /**
- * Get SecuPress scanner counter(s)
+ * Get SecuPress scanner counter(s).
  *
  * @since 1.0
+ *
+ * @param (string) $type Info to retrieve: good, warning, bad, notscannedyet, grade, total.
+ *
+ * @return (string|array) The desired counter info if `$type` is provided and the info exists. An array of all counters otherwise.
  */
 function secupress_get_scanner_counts( $type = '' ) {
 	$tests_by_status = secupress_get_tests();
@@ -40,12 +44,17 @@ function secupress_get_scanner_counts( $type = '' ) {
 
 
 /**
- * Add SecuPress informations into USER_AGENT
+ * Add SecuPress informations into USER_AGENT.
  *
  * @since 1.0
+ *
+ * @param (string) $user_agent A User Agent.
+ *
+ * @return (string)
  */
-function secupress_user_agent( $user_agent ) {////
-	$bonus  = ! secupress_is_white_label() ? '' : '*';
+function secupress_user_agent( $user_agent ) {
+	// ////.
+	$bonus  = ! secupress_is_white_label()        ? '' : '*';
 	$bonus .= ! secupress_get_option( 'do_beta' ) ? '' : '+';
 	$new_ua = sprintf( '%s;SecuPress|%s%s|%s|;', $user_agent, SECUPRESS_VERSION, $bonus, esc_url( home_url() ) );
 
@@ -54,74 +63,11 @@ function secupress_user_agent( $user_agent ) {////
 
 
 /**
- * Renew all boxes for everyone if $uid is missing
- *
- * @since 1.0
- *
- * @param (int|null)$uid : a User id, can be null, null = all users
- * @param (string|array)$keep_this : which box have to be kept
- * @return void
- */
-function secupress_renew_all_boxes( $uid = null, $keep_this = array() ) {
-	// Delete a user meta for 1 user or all at a time
-	delete_metadata( 'user', $uid, 'secupress_boxes', null == $uid );
-
-	// $keep_this works only for the current user
-	if ( ! empty( $keep_this ) && null != $uid ) {
-		if ( is_array( $keep_this ) ) {
-			foreach ( $keep_this as $kt ) {
-				secupress_dismiss_box( $kt );
-			}
-		} else {
-			secupress_dismiss_box( $keep_this );
-		}
-	}
-}
-
-
-/**
- * Renew a dismissed error box admin side
- *
- * @since 1.0
- *
- * @return void
- */
-function secupress_renew_box( $function, $uid = 0 ) {
-	global $current_user;
-
-	$uid    = $uid == 0 ? $current_user->ID : $uid;
-	$actual = get_user_meta( $uid, 'secupress_boxes', true );
-
-	if ( $actual && false !== array_search( $function, $actual ) ) {
-		unset( $actual[ array_search( $function, $actual ) ] );
-		update_user_meta( $uid, 'secupress_boxes', $actual );
-	}
-}
-
-
-/**
- * Dismissed 1 box, wrapper of rocket_dismiss_boxes()
- *
- * @since 1.0
- *
- * @return void
- */
-function secupress_dismiss_box( $function ) {
-	// secupress_dismiss_boxes(
-	//  array(
-	//      'box'      => $function,
-	//      '_wpnonce' => wp_create_nonce( 'secupress_ignore_' . $function ),
-	//      'action'   => 'secupress_ignore'
-	//  )
-	// );
-}
-
-
-/**
  * Is this version White Labeled?
  *
- * @return string
  * @since 1.0
+ *
+ * @return (string)
  */
 function secupress_is_white_label() {
 	$names   = array( 'wl_plugin_name', 'wl_plugin_URI', 'wl_description', 'wl_author', 'wl_author_URI' );
@@ -131,16 +77,17 @@ function secupress_is_white_label() {
 		$options .= ! is_array( secupress_get_option( $value ) ) ? secupress_get_option( $value ) : reset( ( secupress_get_option( $value ) ) );
 	}
 
-	return false; ////
-	return 'a509cac94e0cd8238b250074fe802b90' != md5( $options ); ////
+	return false; // ////.
+	return 'a509cac94e0cd8238b250074fe802b90' !== md5( $options ); // ////.
 }
 
 
 /**
- * Create a unique id for some secupress options and functions
+ * Create a unique id for some secupress options and functions.
  *
  * @since 1.0
- * @return string
+ *
+ * @return (string)
  */
 function secupress_create_uniqid() {
 	return str_replace( '.', '', uniqid( '', true ) );
@@ -148,9 +95,34 @@ function secupress_create_uniqid() {
 
 
 /**
- * Die with SecuPress format
+ * Generate a random key.
  *
  * @since 1.0
+ *
+ * @param (int) $length Length of the key.
+ *
+ * @return (string)
+ */
+function secupress_generate_key( $length = 16 ) {
+	$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+	$key = '';
+	for ( $i = 0; $i < $length; $i++ ) {
+		$key .= $chars[ wp_rand( 0, 31 ) ];
+	}
+
+	return $key;
+}
+
+
+/**
+ * Die with SecuPress format.
+ *
+ * @since 1.0
+ *
+ * @param (string) $message Guess what.
+ * @param (string) $title   Window title.
+ * @param (array)  $args    An array of arguments.
  */
 function secupress_die( $message = '', $title = '', $args = array() ) {
 	$has_p   = strpos( $message, '<p>' ) !== false;
@@ -185,15 +157,13 @@ function secupress_die( $message = '', $title = '', $args = array() ) {
 
 
 /**
- * Block a request and die with more informations
+ * Block a request and die with more informations.
  *
  * @since 1.0
  *
- * @param (string)           $module The related module
+ * @param (string)           $module The related module.
  * @param (array|int|string) $args   Contains the "code" (def. 403) and a "content" (def. empty), this content will replace the default message.
  *                                   $args can be used only for the "code" or "content" or both using an array.
- *
- * @return (string)
  */
 function secupress_block( $module, $args = array( 'code' => 403 ) ) {
 
@@ -265,58 +235,14 @@ function secupress_action_page( $title, $content, $args = array() ) {
 }
 
 
-function secupress_generate_key( $length = 16 ) {
-	$chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-
-	$key = '';
-	for ( $i = 0; $i < $length; $i++ ) {
-		$key .= $chars[ wp_rand( 0, 31 ) ];
-	}
-
-	return $key;
-}
-
-
-function secupress_generate_backupcodes() {
-	$keys = array();
-
-	for ( $k = 1; $k <= 10; $k++ ) { // 10 codes
-		$max = 99999999;
-		$keys[ $k ] = str_pad( wp_rand( floor( $max / 10 ), $max ), strlen( (string) $max ), '0', STR_PAD_RIGHT );
-	}
-
-	return $keys;
-}
-
-
-function secupress_generate_password( $length = 12, $args = array() ) {
-	$defaults = array( 'min' => true, 'maj' => true, 'num' => true, 'special' => false, 'extra' => false, 'custom' => '' );
-	$args     = wp_parse_args( $args, $defaults );
-	$chars    = array();
-
-	$chars['min']     = 'abcdefghijklmnopqrstuvwxyz';
-	$chars['maj']     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-	$chars['num']     = '0123456789';
-	$chars['special'] = '!@#$%^&*()';
-	$chars['extra']   = '-_ []{}<>~`+=,.;:/?|';
-	$chars['custom']  = $args['custom'];
-
-	$usable_chars = '';
-
-	foreach ( $args as $key => $arg ) {
-		$usable_chars .= $args[ $key ] ? $chars[ $key ] : '';
-	}
-
-	$password = '';
-
-	for ( $i = 0; $i < $length; $i++ ) {
-		$password .= substr( $usable_chars, wp_rand( 0, strlen( $usable_chars ) - 1 ), 1 );
-	}
-
-	return $password;
-}
-
-
+/**
+ * Invert the roles values in settings.
+ *
+ * @since 1.0
+ *
+ * @param (array)  $settings The settings passed by reference.
+ * @param (string) $plugin   The plugin.
+ */
 function secupress_manage_affected_roles( &$settings, $plugin ) {
 	static $roles;
 
@@ -335,9 +261,17 @@ function secupress_manage_affected_roles( &$settings, $plugin ) {
 }
 
 
-function secupress_get_ip() { // find the best order
+/**
+ * Get the IP address of the current user.
+ *
+ * @since 1.0
+ *
+ * @return (string)
+ */
+function secupress_get_ip() {
+	// Find the best order ////.
 	$keys = array(
-		'HTTP_CF_CONNECTING_IP', // CF = CloudFlare
+		'HTTP_CF_CONNECTING_IP', // CF = CloudFlare.
 		'HTTP_CLIENT_IP',
 		'HTTP_X_FORWARDED_FOR',
 		'HTTP_X_FORWARDED',
@@ -363,6 +297,16 @@ function secupress_get_ip() { // find the best order
 }
 
 
+/**
+ * Ban an IP address if not whitelisted.
+ * Will add the IP to the list of banned IPs. Will maybe write the IPs in the `.htaccess` file. Will maybe forbid access to the user by displaying a message.
+ *
+ * @since 1.0
+ *
+ * @param (int)    $time_ban Ban duration in minutes. Only used in the message.
+ * @param (string) $ip       The IP to ban.
+ * @param (bool)   $die      True to forbid access to the user by displaying a message.
+ */
 function secupress_ban_ip( $time_ban = 5, $ip = null, $die = true ) {
 	$ip = $ip ? $ip : secupress_get_ip();
 
@@ -372,10 +316,7 @@ function secupress_ban_ip( $time_ban = 5, $ip = null, $die = true ) {
 
 	$time_ban = (int) $time_ban > 0 ? (int) $time_ban : 5;
 	$ban_ips  = get_site_option( SECUPRESS_BAN_IP );
-
-	if ( ! is_array( $ban_ips ) ) {
-		$ban_ips = array();
-	}
+	$ban_ips  = is_array( $ban_ips ) ? $ban_ips : array();
 
 	$ban_ips[ $ip ] = time();
 
@@ -414,6 +355,8 @@ function secupress_ban_ip( $time_ban = 5, $ip = null, $die = true ) {
  */
 function secupress_write_in_htaccess_on_ban() {
 	/**
+	 * Filter to write in the file.
+	 *
 	 * @since 1.0
 	 *
 	 * @param (bool) $write False by default.
@@ -426,22 +369,51 @@ function secupress_write_in_htaccess_on_ban() {
  * Return a <table> containing 2 strings displayed with the Diff_Renderer from WP Core.
  *
  * @since 1.0
- * @return string
- **/
-function secupress_text_diff( $left_string, $right_string, $args = null ) {
+ *
+ * @param (string) $left_string  1st text to compare.
+ * @param (string) $right_string 2nd text to compare.
+ * @param (array)  $args         An array of arguments (titles).
+ *
+ * @return (string)
+ */
+function secupress_text_diff( $left_string, $right_string, $args = array() ) {
 	global $wp_local_package;
 
 	if ( ! class_exists( 'WP_Text_Diff_Renderer_Table' ) ) {
 		require( ABSPATH . WPINC . '/wp-diff.php' );
 	}
 
-	class SecuPress_Text_Diff_Renderer_Table extends WP_Text_Diff_Renderer_Table {
-		public $_leading_context_lines  = 0;
-		public $_trailing_context_lines = 0;
+	if ( ! class_exists( 'SecuPress_Text_Diff_Renderer_Table' ) ) {
+
+		/**
+		 * Table renderer to display the diff lines.
+		 *
+		 * @since 1.0
+		 * @uses WP_Text_Diff_Renderer_Table Extends
+		 */
+		class SecuPress_Text_Diff_Renderer_Table extends WP_Text_Diff_Renderer_Table {
+			/**
+			 * Number of leading context "lines" to preserve.
+			 * @var int
+			 * @access public
+			 * @since 1.0
+			 */
+			public $_leading_context_lines  = 0;
+			/**
+			 * Number of trailing context "lines" to preserve.
+			 * @var int
+			 * @access public
+			 * @since 1.0
+			 */
+			public $_trailing_context_lines = 0;
+		}
 	}
 
-	$defaults     = array( 'title' => __( 'File Differences', 'secupress' ), 'title_left' => __( 'Real file', 'secupress' ), 'title_right' => __( 'Your file', 'secupress' ) );
-	$args         = wp_parse_args( $args, $defaults );
+	$args         = wp_parse_args( $args, array(
+		'title'       => __( 'File Differences', 'secupress' ),
+		'title_left'  => __( 'Real file', 'secupress' ),
+		'title_right' => __( 'Your file', 'secupress' ),
+	) );
 	$left_string  = normalize_whitespace( $left_string );
 	$right_string = normalize_whitespace( $right_string );
 	$left_lines   = explode( "\n", $left_string );
@@ -450,36 +422,35 @@ function secupress_text_diff( $left_string, $right_string, $args = null ) {
 	$renderer     = new SecuPress_Text_Diff_Renderer_Table( $args );
 	$diff         = $renderer->render( $text_diff );
 
-	if ( $wp_local_package &&  ( ! $diff ||  '&nbsp;&nbsp;$wp_local_package = \'' . $wp_local_package . '\';' == trim( strip_tags( $diff ) ) ) ) {
+	if ( $wp_local_package && ( ! $diff || trim( strip_tags( $diff ) ) === '&nbsp;&nbsp;$wp_local_package = \'' . $wp_local_package . '\';' ) ) {
 		return __( 'No differences', 'secupress' );
 	}
 
-	$r  = '<table class="diff">' . "\n";
+	$r  = "<table class=\"diff\">\n";
 		$r .= '<col class="content diffsplit left" /><col class="content diffsplit middle" /><col class="content diffsplit right" />';
 		$r .= '<thead>';
-			$r .= '<tr class="diff-title"><th colspan="4">' . $args['title'] . '</th></tr>' . "\n";
-		$r .= '</thead>' . "\n";
+			$r .= '<tr class="diff-title"><th colspan="4">' . $args['title'] . "</th></tr>\n";
+		$r .= "</thead>\n";
 		$r .= '<tbody>';
-		$r .= '<tr class="diff-sub-title">' . "\n";
-			$r .= "\t" . '<th>' . $args['title_left'] . '</th><td></td>' . "\n";
-			$r .= "\t" . '<th>' . $args['title_right'] . '</th><td></td>' . "\n";
-		$r .= '</tr>'  ."\n";
+		$r .= "<tr class=\"diff-sub-title\">\n";
+			$r .= "\t<th>" . $args['title_left'] . "</th><td></td>\n";
+			$r .= "\t<th>" . $args['title_right'] . "</th><td></td>\n";
+		$r .= "</tr>\n";
 		$r .= $diff;
-		$r .= '</tbody>' . "\n";
-	$r .= '</table>';
+		$r .= "</tbody>\n";
+	$r .= "</table>\n";
 
 	return $r;
 }
 
+
 /**
- * WIll load the async classes
+ * Will load the async classes.
  *
  * @since 1.0
- * @return void
- **/
+ */
 function secupress_require_class_async() {
 	/* https://github.com/A5hleyRich/wp-background-processing v1.0 */
 	secupress_require_class( 'Admin', 'wp-async-request' );
 	secupress_require_class( 'Admin', 'wp-background-process' );
-	/* */
 }
