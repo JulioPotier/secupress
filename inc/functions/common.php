881ef26f76456f43c2e8a2ac2190226aa9503084
Julio Potier <juliobosk@gmail.com>
2016-05-23 23:16:20 +0200

    Use the new secupress_is_user() function

--- inc/functions/common.php
+++ inc/functions/common.php
@@ -773,7 +773,7 @@ function secupress_is_affected_role( $module, $submodule, $user ) {
 		return -1;
 	}
 
-	return is_a( $user, 'WP_User' ) && user_can( $user, 'exist' ) && ! array_intersect( $roles, $user->roles ) );
+	return secupress_is_user( $user ) && ! array_intersect( $roles, $user->roles ) );
 }
 
 
