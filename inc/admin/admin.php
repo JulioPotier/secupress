8c701e2bcc8753ea4a65f643428e64ddcf1cfad5
Screenfeed <git@screenfeed.fr>
2016-06-06 23:50:12 +0200

    Moved `__secupress_is_jarvis()` to the right file.

--- inc/admin/admin.php
+++ inc/admin/admin.php
@@ -19,3 +19,17 @@ function __secupress_add_own_ua( $r, $url ) {
 
 	return $r;
 }
+
+
+add_filter( 'admin_page_access_denied', '__secupress_is_jarvis', 9 );
+/**
+ * Easter egg when you visit a "secupress" page with a typo in it, or just don't have access (not under white label).
+ *
+ * @since 1.0
+ * @author Tony Stark
+ */
+function __secupress_is_jarvis() {
+	if ( ! secupress_is_white_label() && isset( $_GET['page'] ) && strpos( $_GET['page'], 'secupress' ) !== false ) { // Do not use SECUPRESS_PLUGIN_SLUG, we don't want that in white label.
+		wp_die( '[J.A.R.V.I.S.] You are not authorized to access this area.<br/>[Christine Everhart] Jesus ...<br/>[Pepper Potts] That\'s Jarvis, he runs the house.', 403 );
+	}
+}
