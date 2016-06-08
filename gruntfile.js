e0d6cef09e8bb85b1077405e52b107357abf4d97
Screenfeed <git@screenfeed.fr>
2016-06-08 23:18:02 +0200

    Now use grunt-uglify and grunt-newer. Minified our 5 JS files.

--- gruntfile.js
+++ gruntfile.js
@@ -8,8 +8,40 @@ module.exports = function( grunt ) {
 			},
 			"all": {
 				"files": {
-					"src": [ "assets/admin/js/secupress-common.js", "assets/admin/js/secupress-modules.js", "assets/admin/js/secupress-notices.js", "assets/admin/js/secupress-scanner.js", "assets/admin/js/secupress-scanner.js", "assets/admin/js/secupress-wordpress.js" ]
+					"src": [
+						"assets/admin/js/secupress-common.js",
+						"assets/admin/js/secupress-modules.js",
+						"assets/admin/js/secupress-notices.js",
+						"assets/admin/js/secupress-scanner.js",
+						"assets/admin/js/secupress-wordpress.js"
+					]
 				}
+			}
+		},
+		"uglify": {
+			"all": {
+				"files": [
+					{
+						"src":  "assets/admin/js/secupress-common.js",
+						"dest": "assets/admin/js/secupress-common.min.js"
+					},
+					{
+						"src":  "assets/admin/js/secupress-modules.js",
+						"dest": "assets/admin/js/secupress-modules.min.js"
+					},
+					{
+						"src":  "assets/admin/js/secupress-notices.js",
+						"dest": "assets/admin/js/secupress-notices.min.js"
+					},
+					{
+						"src":  "assets/admin/js/secupress-scanner.js",
+						"dest": "assets/admin/js/secupress-scanner.min.js"
+					},
+					{
+						"src":  "assets/admin/js/secupress-wordpress.js",
+						"dest": "assets/admin/js/secupress-wordpress.min.js"
+					}
+				]
 			}
 		},
 		"devUpdate": {
@@ -29,7 +61,10 @@ module.exports = function( grunt ) {
 	} );
 
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
+	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-newer" );
 	grunt.loadNpmTasks( "grunt-dev-update" );
 
 	grunt.registerTask( "jsh", [ "jshint" ] );
+	grunt.registerTask( "minify", [ "newer:jshint", "newer:uglify" ] );
 };
