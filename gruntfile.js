495d177d96ec232e84818868d9188d2ccfe4dd1f
Screenfeed <git@screenfeed.fr>
2016-06-09 15:26:02 +0200

    Added `contrib-cssmin` to grunt. Minified our 10 css files. poke @geoffrey.

--- gruntfile.js
+++ gruntfile.js
@@ -44,6 +44,52 @@ module.exports = function( grunt ) {
 				]
 			}
 		},
+		"cssmin": {
+			"all": {
+				"files": [
+					{
+						"src":  "assets/admin/css/secupress-action-page.css",
+						"dest": "assets/admin/css/secupress-action-page.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-common.css",
+						"dest": "assets/admin/css/secupress-common.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-logs.css",
+						"dest": "assets/admin/css/secupress-logs.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-modules.css",
+						"dest": "assets/admin/css/secupress-modules.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-notices.css",
+						"dest": "assets/admin/css/secupress-notices.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-scanner.css",
+						"dest": "assets/admin/css/secupress-scanner.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-settings.css",
+						"dest": "assets/admin/css/secupress-settings.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-wordpress-3.7.css",
+						"dest": "assets/admin/css/secupress-wordpress-3.7.min.css"
+					},
+					{
+						"src":  "assets/admin/css/secupress-wordpress.css",
+						"dest": "assets/admin/css/secupress-wordpress.min.css"
+					},
+					{
+						"src":  "assets/admin/css/sweetalert2.css",
+						"dest": "assets/admin/css/sweetalert2.min.css"
+					}
+				]
+			}
+		},
 		"devUpdate": {
 			"check": {
 				"options": {
@@ -62,9 +108,10 @@ module.exports = function( grunt ) {
 
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
 	grunt.loadNpmTasks( "grunt-contrib-uglify" );
+	grunt.loadNpmTasks( "grunt-contrib-cssmin" );
 	grunt.loadNpmTasks( "grunt-newer" );
 	grunt.loadNpmTasks( "grunt-dev-update" );
 
 	grunt.registerTask( "jsh", [ "jshint" ] );
-	grunt.registerTask( "minify", [ "newer:jshint", "newer:uglify" ] );
+	grunt.registerTask( "minify", [ "newer:jshint", "newer:uglify", "newer:cssmin" ] );
 };
