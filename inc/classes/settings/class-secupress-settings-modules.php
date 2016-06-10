c4cae42528b3fbd705e09450a12b9e4d2238755f
Geoffrey Crofte <support@creativejuiz.com>
2016-06-10 17:13:37 +0200

    Right font in "SecuPress" word in sidebar + White label support.

--- inc/classes/settings/class-secupress-settings-modules.php
+++ inc/classes/settings/class-secupress-settings-modules.php
@@ -200,7 +200,16 @@ class SecuPress_Settings_Modules extends SecuPress_Settings {
 									<?php echo secupress_get_logo(); ?>
 								</div>
 								<div class="secupress-sh-name">
-									<p class="secupress-sh-title"><?php echo SECUPRESS_PLUGIN_NAME; ?></p>
+									<p class="secupress-sh-title">
+										<?php 
+											if ( SECUPRESS_PLUGIN_NAME === 'SecuPress' ) {
+												echo '<img src="' . SECUPRESS_ADMIN_IMAGES_URL . 'secupress-word.png" srcset="' . SECUPRESS_ADMIN_IMAGES_URL . 'secupress-word.svg 1x, ' . SECUPRESS_ADMIN_IMAGES_URL . 'secupress-word.svg 2x" alt="SecuPress" width="81" height="19">';
+											}
+											else {
+												echo SECUPRESS_PLUGIN_NAME;
+											}
+										?>
+									</p>
 								</div>
 							</div>
 						</div>
