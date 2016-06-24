b4cf5f82850c3647e1735eef001eb6a5a6312d60
Screenfeed <git@screenfeed.fr>
2016-06-24 15:20:20 +0200

    Removed useless title "Pages Logs" from the Logs settings page. Both log types fields are now in a single `logs.php` file.

--- inc/modules/logs/settings.php
+++ inc/modules/logs/settings.php
@@ -2,5 +2,4 @@
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 $this->load_plugin_settings( 'banned-ips' );
-$this->load_plugin_settings( 'action-logs' );
-$this->load_plugin_settings( '404-logs' );
+$this->load_plugin_settings( 'logs' );
