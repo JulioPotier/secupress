fe61f0be97e98409d70aa7c6da9ab069ce361817
Screenfeed <git@screenfeed.fr>
2016-07-20 23:26:51 +0200

    Step 3: improved the loops for selecting the scanners + print the "fix action" forms.

--- inc/admin/scanner-step-3.php
+++ inc/admin/scanner-step-3.php
@@ -1,13 +1,66 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
-// Actions the user needs to perform for a fix.
-$fix_actions = SecuPress_Scan::get_and_delete_fix_actions();
-$modules     = secupress_get_modules();
+// Keep only scans with "bad" and "warning" status.
+$fix_actions = array_merge( $bad_scans, $warning_scans );	// `array( $class_name_part_lower => $status )`, will become `array( $class_name_part_lower => array( $fix_action, $fix_action ) )`.
+
+// Keep only scans that need a manual fix + require the scan files + get the "fix actions".
+foreach ( $secupress_tests as $module_name => $class_name_parts ) {
+
+	$class_name_parts = array_combine( array_map( 'strtolower', $class_name_parts ), $class_name_parts );
+	$class_name_parts = array_intersect_key( $class_name_parts, $fix_actions );
+
+	// Only those with "bad" or "warning" status.
+	if ( ! $class_name_parts ) {
+		continue;
+	}
+
+	foreach ( $class_name_parts as $class_name_part_lower => $class_name_part ) {
+		if ( ! file_exists( secupress_class_path( 'scan', $class_name_part ) ) ) {
+			unset( $fix_actions[ $class_name_part_lower ] );
+			continue;
+		}
+
+		secupress_require_class( 'scan', $class_name_part );
+		$class_name       = 'SecuPress_Scan_' . $class_name_part;
+		$current_test     = $class_name::get_instance();
+		$this_fix_actions = $current_test->need_manual_fix();
+
+		// Only those that need a manual fix, and those that need the Pro Version.
+		if ( 'pro' === $current_test->is_fixable() && ! secupress_is_pro() ) {
+			// It needs the Pro Version.
+			$fix_actions[ $class_name_part_lower ] = false;
+		} elseif ( $this_fix_actions ) {
+			// Store the "fix actions".
+			$fix_actions[ $class_name_part_lower ] = $this_fix_actions;
+		} else {
+			unset( $fix_actions[ $class_name_part_lower ] );
+		}
+	}
+}
+
+// Move along, move along...
+if ( ! $fix_actions ) {
+	?>
+	<div class="secupress-step-content-header secupress-flex secupress-flex-spaced">
+		<p class="secupress-step-title"><?php _e( 'Nothing to do here' ); ?></p>
+		<p class="secupress-flex">
+			<a href="<?php echo esc_url( secupress_admin_url( 'scanners' ) ); ?>&amp;step=4" class="secupress-button shadow light">
+				<span class="icon">
+					<i class="icon-cross" aria-hidden="true"></i>
+				</span>
+				<span class="text"><?php _e( 'Next step', 'secupress' ); ?></span>
+			</a>
+		</p>
+	</div>
+	<?php
+	return;
+}
+
 ?>
 <div class="secupress-step-content-header secupress-flex secupress-flex-spaced">
 	<?php
-	$nb_actions    = count( $bad_scans ) + count( $warning_scans );
+	$nb_actions    = count( $fix_actions );
 	$page_title    = sprintf( _n( 'This action require your attention', 'These %d actions require your attention', $nb_actions, 'secupress' ), $nb_actions );
 	$steps_counter =
 		'<span class="secupress-step-by-step secupress-flex hide-if-no-js">
@@ -20,7 +73,7 @@ $modules     = secupress_get_modules();
 			<span class="icon">
 				<i class="icon-cross" aria-hidden="true"></i>
 			</span>
-			<span class="text">' . __( 'Ignore this step', 'secupress') . '</span>
+			<span class="text">' . __( 'Ignore this step', 'secupress' ) . '</span>
 		</a>';
 	?>
 	<p class="secupress-step-title"><?php echo $page_title; ?></p>
@@ -32,28 +85,19 @@ $modules     = secupress_get_modules();
 
 <div id="secupress-tests" class="secupress-tests">
 	<?php
+	$modules      = secupress_get_modules();
 	$hidden_class = '';
 
 	foreach ( $secupress_tests as $module_name => $class_name_parts ) {
 
 		$class_name_parts = array_combine( array_map( 'strtolower', $class_name_parts ), $class_name_parts );
+		$class_name_parts = array_intersect_key( $class_name_parts, $fix_actions );
 
-		foreach ( $class_name_parts as $option_name => $class_name_part ) {
-			if ( ! file_exists( secupress_class_path( 'scan', $class_name_part ) ) ) {
-				unset( $class_name_parts[ $option_name ] );
-				continue;
-			}
-
-			secupress_require_class( 'scan', $class_name_part );
+		if ( ! $class_name_parts ) {
+			continue;
 		}
 
-		// For this module, order the scans by status: 'warning', 'bad'.
-		$this_module_bad_scans     = array_intersect_key( $class_name_parts, $bad_scans );
-		$this_module_warning_scans = array_intersect_key( $class_name_parts, $warning_scans );
-		$class_name_parts          = array_merge( $this_module_warning_scans, $this_module_bad_scans );
-		unset( $this_module_bad_scans, $this_module_warning_scans );
-
-		foreach ( $class_name_parts as $option_name => $class_name_part ) :
+		foreach ( $class_name_parts as $class_name_part_lower => $class_name_part ) :
 			$class_name   = 'SecuPress_Scan_' . $class_name_part;
 			$current_test = $class_name::get_instance();
 			$referer      = urlencode( esc_url_raw( self_admin_url( 'admin.php?page=' . SECUPRESS_PLUGIN_SLUG . '_scanners&step=3#' . $class_name_part ) ) );
@@ -62,19 +106,13 @@ $modules     = secupress_get_modules();
 			$module_icon  = ! empty( $modules[ $module_name ]['icon'] ) ? $modules[ $module_name ]['icon'] : '';
 
 			// Scan.
-			$scanner        = isset( $scanners[ $option_name ] ) ? $scanners[ $option_name ] : array();
+			$scanner        = isset( $scanners[ $class_name_part_lower ] ) ? $scanners[ $class_name_part_lower ] : array();
 			$scan_status    = ! empty( $scanner['status'] ) ? $scanner['status'] : 'notscannedyet';
 			$scan_nonce_url = wp_nonce_url( admin_url( 'admin-post.php?action=secupress_scanner&test=' . $class_name_part . '&_wp_http_referer=' . $referer ), 'secupress_scanner_' . $class_name_part );
-
-			// Fix.
-			$fix           = ! empty( $fixes[ $option_name ] ) ? $fixes[ $option_name ] : array();
-			$fix_nonce_url = wp_nonce_url( admin_url( 'admin-post.php?action=secupress_fixit&test=' . $class_name_part . '&_wp_http_referer=' . $referer ), 'secupress_fixit_' . $class_name_part );
 
 			// Row css class.
 			$row_css_class  = ' status-' . sanitize_html_class( $scan_status );
 			$row_css_class .= $is_fixable ? ' fixable' : ' not-fixable';
-			$row_css_class .= ! empty( $fix['has_action'] ) ? ' status-hasaction' : '';
-			$row_css_class .= ! empty( $fix['status'] ) && empty( $fix['has_action'] ) ? ' has-fix-status' : ' no-fix-status';
 			?>
 			<div class="secupress-manual-fix secupress-manual-fix-<?php echo $module_name; ?> secupress-group-item-<?php echo $class_name_part; ?><?php echo $hidden_class; ?>">
 
@@ -105,7 +143,11 @@ $modules     = secupress_get_modules();
 
 				<div id="secupress-mf-content-<?php echo $class_name_part; ?>" class="secupress-mf-content secupress-item-<?php echo $class_name_part; ?> status-all <?php echo $row_css_class; ?>">
 
+					<?php if ( $is_fixable ) : ?>
+					<form class="secupress-item-content" method="post" action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>">
+					<?php else : ?>
 					<div class="secupress-item-content">
+					<?php endif; ?>
 
 						<p class="secupress-ic-title">
 							<?php _e( 'How to Fix this issue', 'secupress' ); ?>
@@ -115,12 +157,18 @@ $modules     = secupress_get_modules();
 							<?php echo wp_kses( $current_test->more_fix, $allowed_tags ); ?>
 						</p>
 
-						<div class="secupress-ic-fix-actions">
-							<?php
-							$fix_actions = $current_test->get_required_fix_action_template_parts( $fix_actions );
-							var_dump( $fix_actions ); //// toujours 0 :/ Normal, on ne peut plus utiliser ça, il faut créer un nouveau système.
-							?>
-						</div>
+						<?php if ( $fix_actions[ $class_name_part_lower ] ) : ?>
+							<div class="secupress-ic-fix-actions">
+								<?php
+								$fix_actions[ $class_name_part_lower ] = $current_test->get_required_fix_action_template_parts( $fix_actions[ $class_name_part_lower ] );
+
+								echo implode( '', $fix_actions[ $class_name_part_lower ] );
+								$current_test->get_fix_action_fields( array_keys( $fix_actions[ $class_name_part_lower ] ) );
+
+								unset( $fix_actions[ $class_name_part_lower ] );
+								?>
+							</div>
+						<?php endif; ?>
 
 						<div class="secupress-row-actions secupress-flex secupress-flex-spaced secupress-mt2">
 							<p class="secupress-action-doc">
@@ -146,20 +194,13 @@ $modules     = secupress_get_modules();
 									</span>
 									<span class="text"><?php _e( 'Ignore it', 'secupress' ); ?></span>
 								</a>
-								<?php if ( $is_fixable && $current_test->need_manual_fix() ) { ?>
-									<a href="<?php echo esc_url( $fix_nonce_url ); ?>" class="secupress-button secupress-button-primary secupress-button-fixit shadow">
+								<?php if ( $is_fixable ) { ?>
+									<button type="submit" class="secupress-button secupress-button-primary secupress-button-manual-fixit shadow">
 										<span class="icon">
 											<i class="icon-check" aria-hidden="true"></i>
 										</span>
 										<span class="text"><?php _e( 'Fix it', 'secupress' ); ?></span>
-									</a>
-								<?php } elseif ( $is_fixable && ! $current_test->need_manual_fix() ) { ?>
-									<a href="<?php echo esc_url( $fix_nonce_url ); ?>" class="secupress-button secupress-button-primary secupress-button-fixit shadow">
-										<span class="icon">
-											<i class="icon-check" aria-hidden="true"></i>
-										</span>
-										<span class="text"><?php _e( 'Retry to fix', 'secupress' ); ?></span>
-									</a>
+									</button>
 								<?php } elseif ( $is_only_pro ) { ?>
 									<a href="<?php echo esc_url( secupress_admin_url( 'get_pro' ) ); ?>" class="secupress-button secupress-button-tertiary secupress-button-getpro shadow">
 										<span class="icon">
@@ -171,7 +212,11 @@ $modules     = secupress_get_modules();
 							</p>
 						</div>
 
+					<?php if ( $is_fixable ) : ?>
+					</form><!-- .secupress-item-content -->
+					<?php else : ?>
 					</div><!-- .secupress-item-content -->
+					<?php endif; ?>
 
 					<div class="secupress-item-details" id="details-<?php echo $class_name_part; ?>">
 						<div class="secupress-flex">
