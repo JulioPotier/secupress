85ba7c70dabccd3a82167c5380a3f46cef3e5d2b
Julio Potier <juliobosk@gmail.com>
2016-07-22 14:06:14 +0200

    Step1 for new items
    With :bug2:

--- inc/admin/settings.php
+++ inc/admin/settings.php
@@ -338,9 +338,10 @@ function __secupress_modules() {
  * @since 1.0
  */
 function __secupress_scanners() {
-	$counts  = secupress_get_scanner_counts();
-	$items   = array_filter( (array) get_site_option( SECUPRESS_SCAN_TIMES ) );
-	$reports = array();
+	$counts      = secupress_get_scanner_counts();
+	$items       = array_filter( (array) get_site_option( SECUPRESS_SCAN_TIMES ) );
+	$reports     = array();
+	$last_report = '—';
 
 	if ( $items ) {
 		$last_percent = -1;
@@ -350,7 +351,8 @@ function __secupress_scanners() {
 			$last_percent = $item['percent'];
 		}
 
-		$reports = array_reverse( $reports );
+		$reports     = array_reverse( $reports );
+		$last_report = date_i18n( _x( 'M dS, Y \a\t h:ia', 'Latest scans', 'secupress' ), $items[0]['time'] );
 	}
 	?>
 	<div class="wrap">
@@ -380,7 +382,7 @@ function __secupress_scanners() {
 					<p class="secupress-label-with-icon secupress-last-scan-result">
 						<i class="icon-secupress" aria-hidden="true"></i>
 						<span class="secupress-upper"><?php _e( 'Result of the scan', 'secupress' ); ?></span>
-						<span class="secupress-primary">20.06.2016 • 13h37</span>
+						<span class="secupress-primary"><?php echo $last_report; ?></span>
 					</p>
 					<p class="secupress-text-end hide-if-no-js">
 						<a href="#secupress-more-info" class="secupress-link-icon secupress-open-moreinfo<?php echo $reports ? '' : ' secupress-activated dont-trigger-hide'; ?>" data-trigger="slidedown" data-target="secupress-more-info">
@@ -545,7 +547,7 @@ function __secupress_scanners() {
 									<span class="secupress-label-with-icon">
 										<i class="icon-secupress" aria-hidden="true"></i>
 										<span class="secupress-upper"><?php esc_html_e( 'Result of the scan', 'secupress' ); ?></span>
-										<span class="secupress-primary">20.06.2016 • 13h37</span>
+										<span class="secupress-primary"><?php echo $last_report; ?></span>
 									</span>
 								</a>
 							</li>
