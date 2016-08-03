5a0e9b7f96be22b0baffbb46ae419903c700d4c3
Screenfeed <git@screenfeed.fr>
2016-08-03 16:11:22 +0200

    PHP Disclosure scan class: - Improved init: easier to read and understand. - Improved messages: include the config file name directly inside `get_messages()`. - Fix for IIS7: get the path (for the rules) from the plugin error message.

--- inc/classes/scanners/class-secupress-scan-php-disclosure.php
+++ inc/classes/scanners/class-secupress-scan-php-disclosure.php
@@ -43,20 +43,21 @@ class SecuPress_Scan_PHP_Disclosure extends SecuPress_Scan implements SecuPress_
 		$this->title = __( 'Check if your WordPress site discloses the PHP modules <em>(know as PHP Easter Egg)</em>.', 'secupress' );
 		$this->more  = __( 'PHP contains a flaw that may lead to an unauthorized information disclosure. The issue is triggered when a remote attacker makes certain HTTP requests with crafted arguments, which will disclose PHP version and another sensitive information resulting in a loss of confidentiality.', 'secupress' );
 
-		if ( $is_apache ) {
-			$config_file = '.htaccess';
-		} elseif ( $is_iis7 ) {
-			$config_file = 'web.config';
-		} elseif ( ! $is_nginx ) {
-			$this->fixable = false;
+		if ( ! $is_apache && ! $is_nginx && ! $is_iis7 ) {
+			$this->more_fix = static::get_messages( 301 );
+			$this->fixable  = false;
+			return;
 		}
 
-		if ( $is_nginx ) {
-			$this->more_fix = sprintf( __( 'The %s file cannot be edited automatically, this will give you the rules to add into it manually, to avoid attackers to read sensitive informations from your installation.', 'secupress' ), '<code>nginx.conf</code>' );
-		} elseif ( $this->fixable ) {
-			$this->more_fix = sprintf( __( 'Add rules in your %s file to avoid attackers to read sensitive informations from your installation.', 'secupress' ), "<code>$config_file</code>" );
+		if ( $is_apache ) {
+			/** Translators: %s is a file name. */
+			$this->more_fix = sprintf( __( 'Add rules in your %s file to avoid attackers to read sensitive informations about your PHP modules.', 'secupress' ), '<code>.htaccess</code>' );
+		} elseif ( $is_iis7 ) {
+			/** Translators: %s is a file name. */
+			$this->more_fix = sprintf( __( 'Add rules in your %s file to avoid attackers to read sensitive informations about your PHP modules.', 'secupress' ), '<code>web.config</code>' );
 		} else {
-			$this->more_fix = static::get_messages( 301 );
+			/** Translators: %s is a file name. */
+			$this->more_fix = sprintf( __( 'The %s file cannot be edited automatically, you will be given the rules to add into this file manually, to avoid attackers to read sensitive informations about your PHP modules.', 'secupress' ), '<code>nginx.conf</code>' );
 		}
 	}
 
@@ -71,22 +72,27 @@ class SecuPress_Scan_PHP_Disclosure extends SecuPress_Scan implements SecuPress_
 	 * @return (string|array) A message if a message ID is provided. An array containing all messages otherwise.
 	 */
 	public static function get_messages( $message_id = null ) {
+		global $is_apache;
+		$config_file = $is_apache ? '.htaccess' : 'web.config';
+
 		$messages = array(
 			// "good"
 			0   => __( 'Your site does not reveal the PHP modules.', 'secupress' ),
-			1   => __( 'The rules forbidding access to the PHP Easter Egg have been successfully added to your %s file  Your %s file has been successfully edited.', 'secupress' ),
+			/** Translators: %s is a file name. */
+			1   => sprintf( __( 'The rules forbidding access to the PHP Easter Egg have been successfully added to your %s file.', 'secupress' ), "<code>$config_file</code>" ),
 			// "warning"
 			100 => __( 'Unable to determine if your homepage is disclosing PHP Easter Egg.', 'secupress' ),
 			// "bad"
-			200 => sprintf( __( '%s should not be accessible to anyone.', 'secupress' ), '<code>' . user_trailingslashit( home_url() ) . '?=PHPB8B5F2A0-3C92-11d3-A3A9-4C7B08C10000</code>' ),
+			/* translators: %s is an URL */
+			200 => sprintf( __( '%s should not be accessible to anyone.', 'secupress' ), '<code>' . esc_url( user_trailingslashit( home_url() ) . '?=PHPB8B5F2A0-3C92-11d3-A3A9-4C7B08C10000' ) . '</code>' ),
 			// "cantfix"
 			/* translators: 1 is a file name, 2 is some code */
-			300 => sprintf( __( 'Your server runs a nginx system, the sensitive information disclosure cannot be fixed automatically but you can do it yourself by adding the following code into your %1$s file: %2$s', 'secupress' ), '<code>nginx.conf</code>', '%s' ),
-			301 => __( 'Your server runs a non recognized system. The sensitive information disclosure cannot be fixed automatically.', 'secupress' ),
+			300 => sprintf( __( 'Your server runs a nginx system, PHP modules disclosure cannot be protected automatically but you can do it yourself by adding the following code into your %1$s file: %2$s', 'secupress' ), '<code>nginx.conf</code>', '%s' ),
+			301 => __( 'Your server runs a non recognized system. PHP modules disclosure cannot be protected automatically.', 'secupress' ),
 			/* translators: 1 is a file name, 2 is some code */
-			302 => __( 'Your %1$s file does not seem to be writable. Please add the following lines at the beginning of the file: %2$s', 'secupress' ),
+			302 => sprintf( __( 'Your %1$s file does not seem to be writable. Please add the following lines at the beginning of the file: %2$s', 'secupress' ), "<code>$config_file</code>", '%s' ),
 			/* translators: 1 is a file name, 2 is a folder path (kind of), 3 is some code */
-			303 => __( 'Your %1$s file does not seem to be writable. Please add the following lines inside the tags hierarchy %2$s (create it if does not exist): %3$s', 'secupress' ),
+			303 => sprintf( __( 'Your %1$s file does not seem to be writable. Please add the following lines inside the tags hierarchy %2$s (create it if does not exist): %3$s', 'secupress' ), "<code>$config_file</code>", '%1$s', '%2$s' ),
 		);
 
 		if ( isset( $message_id ) ) {
@@ -123,7 +129,7 @@ class SecuPress_Scan_PHP_Disclosure extends SecuPress_Scan implements SecuPress_
 				}
 			}
 		} elseif ( is_wp_error( $response ) ) {
-			// If it's not an error, then no disclose.
+			// If it's not an error, then no disclosure.
 			// "warning".
 			$this->add_message( 100 );
 		}
@@ -176,14 +182,15 @@ class SecuPress_Scan_PHP_Disclosure extends SecuPress_Scan implements SecuPress_
 		$last_error = is_array( $wp_settings_errors ) && $wp_settings_errors ? end( $wp_settings_errors ) : false;
 
 		if ( $last_error && 'general' === $last_error['setting'] && 'apache_manual_edit' === $last_error['code'] ) {
+			$rules = static::_get_rules_from_error( $last_error );
 			// "cantfix"
-			$this->add_fix_message( 302, array( '<code>.htaccess</code>', static::_get_rules_from_error( $last_error ) ) );
+			$this->add_fix_message( 302, array( $rules ) );
 			array_pop( $wp_settings_errors );
 			return;
 		}
 
 		// "good"
-		$this->add_fix_message( 1, array( '<code>.htaccess</code>' ) );
+		$this->add_fix_message( 1 );
 	}
 
 
@@ -201,14 +208,16 @@ class SecuPress_Scan_PHP_Disclosure extends SecuPress_Scan implements SecuPress_
 		$last_error = is_array( $wp_settings_errors ) && $wp_settings_errors ? end( $wp_settings_errors ) : false;
 
 		if ( $last_error && 'general' === $last_error['setting'] && 'iis7_manual_edit' === $last_error['code'] ) {
+			$rules = static::_get_rules_from_error( $last_error );
+			$path  = static::_get_code_tag_from_error( $last_error, 'secupress-iis7-path' );
 			// "cantfix"
-			$this->add_fix_message( 303, array( '<code>web.config</code>', '/configuration/system.webServer/rewrite/rules', static::_get_rules_from_error( $last_error ) ) );
+			$this->add_fix_message( 303, array( $path, $rules ) );
 			array_pop( $wp_settings_errors );
 			return;
 		}
 
 		// "good"
-		$this->add_fix_message( 1, array( '<code>web.config</code>' ) );
+		$this->add_fix_message( 1 );
 	}
 
 
