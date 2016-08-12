0cef61e9e6f160dbd18341edca4dbca5cb30a881
Julio Potier <juliobosk@gmail.com>
2016-08-12 23:32:52 +0200

    hoxfix: Plans are now dyn

--- inc/modules/get-pro/settings.php
+++ inc/modules/get-pro/settings.php
@@ -1,5 +1,16 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
+
+$plans = get_transient( 'secupress_pro_plans' );
+
+if ( false === $plans ) {
+	$response = wp_remote_get( SECUPRESS_WEB_MAIN . 'plans.php' );
+	if ( ! is_wp_error( $response ) && wp_remote_retrieve_response_code( $response ) === 200 ) {
+		$plans = wp_remote_retrieve_body( $response );
+		$plans = json_decode( $plans, true );
+		set_transient( 'secupress_pro_plans', $plans, DAY_IN_SECONDS );
+	}
+}
 ?>
 
 	<div class="secupress-section-dark secupress-settings-header secupress-flex">
@@ -31,96 +42,60 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 		</p>
 
 		<div id="secupress-pricing" class="secupress-pricing secupress-flex secupress-text-center">
-
+		<?php
+		foreach ( $plans as $plan ) {
+		?>
 			<div class="secupress-col-1-3 secupress-flex">
 				<div class="secupress-price secupress-box-shadow secupress-flex-col">
 					<div class="secupress-price-header">
-						<p class="secupress-price-name"><?php _e( 'Lite', 'secupress' ); ?></p>
+						<?php
+						if ( isset( $plan['names'][ get_locale() ] ) ) {
+							$plan_name = $plan['names'][ get_locale() ];
+						} else {	
+							$plan_name = $plan['names']['en_US'];
+						}
+						?>
+						<p class="secupress-price-name"><?php echo esc_html( $plan_name ); ?></p>
 						<p class="secupress-amounts secupress-hide-monthly">
 							<span class="secupress-dollars">$</span>
-							<ins>54</ins>
-							<del>$72</del>
+							<ins><?php echo esc_html( $plan['price_new'] ); ?></ins>
+							<del>$<?php echo esc_html( $plan['price_old'] ); ?></del>
 						</p>
 						<p class="secupress-amounts secupress-hide-yearly">
 							<span class="secupress-dollars">$</span>
-							<span class="price">5<small>.99</small></span>
+							<?php
+							$price = explode( '.', $plan['price'] );
+							if ( isset( $price[1] ) ) { 
+								$price = $price[0] . '<small>' . $price[1] . '</small>';
+							} else {
+								$price = $price[0];
+							}
+							?>
+							<span class="price"><?php echo $price; ?></span>
 						</p>
-						<p class="secupress-price-desc secupress-hide-monthly"><?php esc_html_e( '3 months for free', 'secupress' ); ?></p>
+						<p class="secupress-price-desc secupress-hide-monthly"><?php echo esc_html( sprintf( _n( '%d month for free', '%d months for free', (int) $plan['free'], 'secupress' ), (int) $plan['free'] ) ); ?></p>
 					</div>
 					<div class="secupress-price-details">
 						<p class="secupress-pd-info secupress-hide-monthly"><?php esc_html_e( 'Billed per year', 'secupress' ); ?></p>
 						<p class="secupress-pd-info secupress-hide-yearly"><?php esc_html_e( 'Billed per month', 'secupress' ); ?></p>
 						<p class="secupress-pd-benefits">
 							<?php esc_html_e( 'Secure & Protect', 'secupress' ); ?>
-							<strong><?php esc_html_e( '1 Website', 'secupress' ); ?></strong>
+							<strong><?php echo esc_html( sprintf( _n( '%d Website', '%d Websites', (int) $plan['websites'], 'secupress' ), (int) $plan['websites'] ) ); ?></strong>
 							<?php esc_html_e( 'Forever', 'secupress' ); ?>
 						</p>
 					</div>
 					<div class="secupress-price-cta">
-						<a href="https://secupress.me/checkout/?edd_action=add_to_cart&download_id=14&edd_options[price_id]=4" class="secupress-button secupress-button-primary shadow" target="_blank"><?php esc_html_e( 'Pre-Order', 'secupress' ); ?></a>
+						<?php
+						$order_button = 'preorder' == $plan['button'] ? esc_html__( 'Pre-Order', 'secupress' ) : esc_html__( 'Order', 'secupress' );
+						?>
+						<a href="<?php echo esc_url( $plan['url'] ); ?>" class="secupress-button secupress-button-primary shadow" target="_blank"><?php echo $order_button; ?></a>
 					</div>
 				</div>
 			</div>
-			<div class="secupress-col-1-3 secupress-flex">
-				<div class="secupress-price secupress-box-shadow secupress-flex-col">
-					<div class="secupress-price-header">
-						<p class="secupress-price-name"><?php _e( 'Standard', 'secupress' ); ?></p>
-						<p class="secupress-amounts secupress-hide-monthly">
-							<span class="secupress-dollars">$</span>
-							<ins>134</ins>
-							<del>$180</del>
-						</p>
-						<p class="secupress-amounts secupress-hide-yearly">
-							<span class="secupress-dollars">$</span>
-							<span class="price">14<small>.99</small></span>
-						</p>
-						<p class="secupress-price-desc secupress-hide-monthly"><?php esc_html_e( '3 months for free', 'secupress' ); ?></p>
-					</div>
-					<div class="secupress-price-details">
-						<p class="secupress-pd-info secupress-hide-monthly"><?php esc_html_e( 'Billed per year', 'secupress' ); ?></p>
-						<p class="secupress-pd-info secupress-hide-yearly"><?php esc_html_e( 'Billed per month', 'secupress' ); ?></p>
-						<p class="secupress-pd-benefits">
-							<?php esc_html_e( 'Secure & Protect', 'secupress' ); ?>
-							<strong><?php esc_html_e( '3 Websites', 'secupress' ); ?></strong>
-							<?php esc_html_e( 'Forever', 'secupress' ); ?>
-						</p>
-					</div>
-					<div class="secupress-price-cta">
-						<a href="https://secupress.me/checkout/?edd_action=add_to_cart&download_id=14&edd_options[price_id]=5" class="secupress-button secupress-button-primary shadow" target="_blank"><?php esc_html_e( 'Pre-Order', 'secupress' ); ?></a>
-					</div>
-				</div>
-			</div>
-			<div class="secupress-col-1-3 secupress-flex">
-				<div class="secupress-price secupress-box-shadow secupress-flex-col">
-					<div class="secupress-price-header">
-						<p class="secupress-price-name"><?php _e( 'Plus', 'secupress' ); ?></p>
-						<p class="secupress-amounts secupress-hide-monthly">
-							<span class="secupress-dollars">$</span>
-							<ins>269</ins>
-							<del>$360</del>
-						</p>
-						<p class="secupress-amounts secupress-hide-yearly">
-							<span class="secupress-dollars">$</span>
-							<span class="price">29<small>.99</small></span>
-						</p>
-						<p class="secupress-price-desc secupress-hide-monthly"><?php esc_html_e( '3 months for free', 'secupress' ); ?></p>
-					</div>
-					<div class="secupress-price-details">
-						<p class="secupress-pd-info secupress-hide-monthly"><?php esc_html_e( 'Billed per year', 'secupress' ); ?></p>
-						<p class="secupress-pd-info secupress-hide-yearly"><?php esc_html_e( 'Billed per month', 'secupress' ); ?></p>
-						<p class="secupress-pd-benefits">
-							<?php esc_html_e( 'Secure & Protect', 'secupress' ); ?>
-							<strong><?php esc_html_e( '10 Websites', 'secupress' ); ?></strong>
-							<?php esc_html_e( 'Forever', 'secupress' ); ?>
-						</p>
-					</div>
-					<div class="secupress-price-cta">
-						<a href="https://secupress.me/checkout/?edd_action=add_to_cart&download_id=14&edd_options[price_id]=6" class="secupress-button secupress-button-primary shadow" target="_blank"><?php esc_html_e( 'Pre-Order', 'secupress' ); ?></a>
-					</div>
-				</div>
-			</div>
+		<?php 
+		}
+		?>
 		</div><!-- #secupress-pricing -->
-
 		<p class="secupress-catchphrase"><?php _e( 'Included With All Plans', 'secupress' ); ?></p>
 
 		<div class="secupress-pro-crossed-offers secupress-flex secupress-text-center secupress-p2">
