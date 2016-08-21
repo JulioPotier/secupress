956e22d3e5026e9b0e4bc85fb1cc4744896cc7b9
Julio Potier <juliobosk@gmail.com>
2016-08-21 23:03:33 +0200

    hotfix: services buttons are not yet clickable, tell them it's coming soon

--- inc/modules/services/settings/services.php
+++ inc/modules/services/settings/services.php
@@ -8,8 +8,10 @@ $this->add_field( array(
 	'description'  => __( 'You may need help to configure the plugin perfectly, we might do this for you for $200.', 'secupress' ),
 	'name'         => $this->get_field_name( 'proconfig' ),
 	'field_type'   => 'field_button',
-	'label'        => __( 'Request a Professional Configuration', 'secupress' ),
-	'url'          => wp_nonce_url( admin_url( 'admin-post.php?action=secupress_get_pro_config' ), 'secupress_get_pro_config' ),
+	'disabled'     => true,
+	//// 'label'        => __( 'Request a Professional Configuration', 'secupress' ),
+	//// 'url'          => wp_nonce_url( admin_url( 'admin-post.php?action=secupress_get_pro_config' ), 'secupress_get_pro_config' ),
+	'label'        => __( 'Coming Soon', 'secupress' ),
 ) );
 
 $this->add_field( array(
@@ -17,6 +19,8 @@ $this->add_field( array(
 	'description'  => __( 'We may help you to recover your website after a cleansing, we charge $500.', 'secupress' ),
 	'name'         => $this->get_field_name( 'got-hacked' ),
 	'field_type'   => 'field_button',
-	'label'        => __( 'Request a Website cleansing', 'secupress' ),
-	'url'          => wp_nonce_url( admin_url( 'admin-post.php?action=secupress_get_hacked' ), 'secupress_get_hacked' ),
+	'disabled'     => true,
+	//// 'label'        => __( 'Request a Website cleansing', 'secupress' ),
+	//// 'url'          => wp_nonce_url( admin_url( 'admin-post.php?action=secupress_get_hacked' ), 'secupress_get_hacked' ),
+	'label'        => __( 'Coming Soon', 'secupress' ),
 ) );
