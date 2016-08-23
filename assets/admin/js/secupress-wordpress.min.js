48cf6ce2a2c4ada4aacbe178d2ce77e16a4d92a8
Screenfeed <git@screenfeed.fr>
2016-08-23 13:33:15 +0200

    Fixes JavaScript errors related to the admin notice asking for a recovery email + code improvements.

--- assets/admin/js/secupress-wordpress.min.js
+++ assets/admin/js/secupress-wordpress.min.js
@@ -60,48 +60,42 @@ jQuery( document ).ready( function( $ ) {
 					type:              "warning"
 				} ) );
 			}
-		});
+		} );
 
-		// Click on retry will play with visibility only
+		// Click on retry will play with visibility only.
 		$( "#secupress_recovery_email_parent" ).on( "click", "#secupress_recovery_email_retry", function( e ) {
-
 			$( "#secupress_recovery_email_result" ).text( "" );
 			$( "#secupress_recovery_email_retry" ).hide();
-			$( "#secupress_recovery_email_retry" ).hide();
-
 			$( "#secupress_recovery_email_submit" ).show();
 			$( "#secupress_recovery_email" ).show().focus().select();
+		} );
 
-		});
+		// Click on submit or hit "enter" inside the input field.
+		$( "#secupress_recovery_email_parent" ).on( "click keyup", "#secupress_recovery_email, #secupress_recovery_email_submit", function( e ) {
+			var email, $_spin;
 
-		// Click on submit or hit "enter" inside the input field
-		$( "#secupress_recovery_email_parent" ).on( "keypress click", "#secupress_recovery_email, #secupress_recovery_email_submit", function( e ) {
-
-			if ( ( "keypress" === e.type && secupressIsEnterKey( e ) ) || ( "click" === e.type && typeof e.toElement.id != "undefined" && "secupress_recovery_email_submit" === e.toElement.id ) ) {
-
-				var $_this = $( "#secupress_recovery_email" );
-				var $_spin = $( "#secupress_recovery_email_spinner" );
-
-				$_this.hide();
-				$_spin.show();
-				$( "#secupress_recovery_email_submit" ).hide();
-
-				$.post( ajaxurl, {secupress_recovery_email: $_this.val(), action:"secupress_recovery_email"} )
-				.always(
-					function( data ) {
-
-						$_spin.hide();
-
-						$( "#secupress_recovery_email_result" ).html( data );
-
-						// 1 char length is the "valid check" emoji, else, show the retry button.
-						if ( 1 < data.length ) {
-							$( "#secupress_recovery_email_retry" ).show().focus();
-						}
-
-					} );
+			if ( "keyup" === e.type && ! secupressIsEnterKey( e ) ) {
+				return;
 			}
-		});
+
+			if ( "click" === e.type && ( ! e.target || ! e.target.id || "secupress_recovery_email_submit" !== e.target.id ) ) {
+				return;
+			}
+
+			$( "#secupress_recovery_email_submit" ).hide();
+			email  = $( "#secupress_recovery_email" ).hide().val();
+			$_spin = $( "#secupress_recovery_email_spinner" ).show();
+
+			$.post( ajaxurl, { secupress_recovery_email: email, action: "secupress_recovery_email" } )
+			.always( function( data ) {
+				$_spin.hide();
+				$( "#secupress_recovery_email_result" ).html( data );
+				// 1 char length is the "valid check" emoji, else, show the retry button.
+				if ( 1 < data.length ) {
+					$( "#secupress_recovery_email_retry" ).show().focus();
+				}
+			} );
+		} );
 
 	} )(jQuery, document, window);
 
