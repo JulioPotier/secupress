9b1d8fb8ee8ae91f9d9be818db8b3aa2923775e8
Screenfeed <git@screenfeed.fr>
2016-09-08 18:22:55 +0200

    Hotfix: added a context to 3 translatable strings "None so far.".

--- inc/classes/settings/class-secupress-settings-modules.php
+++ inc/classes/settings/class-secupress-settings-modules.php
@@ -445,7 +445,7 @@ class SecuPress_Settings_Modules extends SecuPress_Settings {
 	 * @since 1.0
 	 */
 	protected function scheduled_backups() {
-		_e( 'None so far.', 'secupress' );
+		_ex( 'None so far.', 'scheduled backups', 'secupress' );
 	}
 
 
@@ -455,7 +455,7 @@ class SecuPress_Settings_Modules extends SecuPress_Settings {
 	 * @since 1.0
 	 */
 	protected function scheduled_scan() {
-		_e( 'None so far.', 'secupress' );
+		_ex( 'None so far.', 'scheduled scans', 'secupress' );
 	}
 
 
@@ -465,7 +465,7 @@ class SecuPress_Settings_Modules extends SecuPress_Settings {
 	 * @since 1.0
 	 */
 	protected function scheduled_monitoring() {
-		_e( 'None so far.', 'secupress' );
+		_ex( 'None so far.', 'scheduled file monitoring', 'secupress' );
 	}
 
 
