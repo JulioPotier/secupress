7b4251e65ef4235612f0f92b9de8d0ea72eae470
Screenfeed <git@screenfeed.fr>
2016-09-28 12:05:04 +0200

    Add a message after the license fields in case the key is not valid.

--- inc/admin/modules/api-key.php
+++ inc/admin/modules/api-key.php
@@ -3,7 +3,7 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
 
 $this->set_current_section( 'secupress_display_apikey_options' );
-$this->add_section( __( 'License Validation', 'secupress' ) );
+$this->add_section( __( 'License Validation', 'secupress' ), array( 'with_save_button' => false ) );
 
 
 $this->add_field( array(
@@ -39,3 +39,27 @@ $this->add_field( array(
 		),
 	),
 ) );
+
+
+add_action( 'secupress.settings.after_section_secupress_display_apikey_options', 'secupress_apikey_fields_submit_button' );
+/**
+ * Print a warning message and a submit button to activate the license key.
+ *
+ * @since 1.0.5
+ * @author Grégory Viguier
+ *
+ * @param (bool) $with_save_button True if a "Save All Changes" button will be printed.
+ */
+function secupress_apikey_fields_submit_button( $with_save_button ) {
+	if ( $with_save_button ) {
+		return;
+	}
+
+	$values = get_site_option( SECUPRESS_SETTINGS_SLUG );
+
+	if ( is_array( $values ) && ! empty( $values['consumer_email'] ) && ! empty( $values['consumer_key'] ) && empty( $values['site_is_pro'] ) ) {
+		echo '<p style="color:#CB234F">' . __( 'Your License Key is inactive or invalid.', 'secupress' ) . '</p>';
+	}
+
+	echo '<p class="submit"><button type="submit" class="secupress-button-primary" name="secupress_display_apikey_options_submit">' . __( 'Save All Changes', 'secupress' ) . '</button></p>';
+}
