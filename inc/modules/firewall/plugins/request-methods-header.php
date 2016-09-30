9b52b8ed018dbc4a76ca847a38c013964aaf7c2e
Screenfeed <git@screenfeed.fr>
2016-10-01 00:30:28 +0200

    Hotfix: reversed logics can be tricky. Used the right logic for "Bad request methods" scan + sub-module.

--- inc/modules/firewall/plugins/request-methods-header.php
+++ inc/modules/firewall/plugins/request-methods-header.php
@@ -9,10 +9,13 @@ Version: 1.0
 defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
 
 if ( ! defined( 'DOING_CRON' ) ) {
-	// Block Bad request methods.
-	$basic_methods = array( 'GET' => true, 'POST' => true, 'HEAD' => true );
-	$rest_methods  = array( 'PUT' => true, 'PATCH' => true, 'DELETE' => true );
-	$methods       = secupress_is_submodule_active( 'sensitive-data', 'restapi' ) ? array_merge( $basic_methods, $rest_methods ) : $basic_methods;
+	// Allow these request methods.
+	$methods = array( 'GET' => true, 'POST' => true, 'HEAD' => true );
+
+	if ( ! secupress_is_submodule_active( 'sensitive-data', 'restapi' ) ) {
+		// Sub-module not activated === REST API enabled === these methods are also allowed.
+		$methods = array_merge( $methods, array( 'PUT' => true, 'PATCH' => true, 'DELETE' => true ) );
+	}
 
 	if ( ! isset( $methods[ $_SERVER['REQUEST_METHOD'] ] ) ) {
 		secupress_block( 'RMHM', 405 );
