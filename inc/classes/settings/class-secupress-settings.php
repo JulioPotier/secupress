2e9d89dc823aaa7e35e67cf09e8a4e25ac62e4d7
Screenfeed <git@screenfeed.fr>
2016-10-12 16:00:16 +0200

    Hotfix: add a `html_entity_decode()` before `esc_textarea()` to prevent displaying things like `&nbsp;` in the textarea field.

--- inc/classes/settings/class-secupress-settings.php
+++ inc/classes/settings/class-secupress-settings.php
@@ -583,7 +583,7 @@ abstract class SecuPress_Settings extends SecuPress_Singleton {
 
 			case 'textarea' :
 
-				$value       = esc_textarea( implode( "\n" , (array) $value ) );
+				$value       = esc_textarea( html_entity_decode( implode( "\n" , (array) $value ), ENT_QUOTES ) );
 				$attributes .= empty( $args['attributes']['cols'] ) ? ' cols="50"' : '';
 				$attributes .= empty( $args['attributes']['rows'] ) ? ' rows="5"'  : '';
 
