7303bcda7d5a5f277c4519dcc3051f356a9dfa66
Screenfeed <git@screenfeed.fr>
2016-10-17 15:10:34 +0200

    Explosion! 💥

--- inc/modules/services/callbacks.php
+++ inc/modules/services/callbacks.php
@@ -46,8 +46,11 @@ function secupress_services_settings_callback( $settings ) {
 		'support_scanner'     => '',
 	), $settings );
 
-	$summary     = preg_replace( "@[\r\n]+@", ' ', strip_tags( wp_unslash( trim( html_entity_decode( $settings['support_summary'], ENT_QUOTES ) ) ) ) );
-	$description = str_replace( "\r\n", "\n", wp_unslash( trim( html_entity_decode( $settings['support_description'], ENT_QUOTES ) ) ) );
+	$summary     = trim( html_entity_decode( $settings['support_summary'], ENT_QUOTES ) );
+	$summary     = strip_tags( wp_unslash( $settings['support_summary'] ) );
+	$summary     = preg_replace( "@[\r\n]+@", ' ', $settings['support_summary'] );
+	$description = trim( html_entity_decode( $settings['support_description'], ENT_QUOTES ) );
+	$description = str_replace( "\r\n", "\n", wp_unslash( $settings['support_description'], ENT_QUOTES ) );
 
 	$def_message = __( 'Please provide the specific url(s) where we can see each issue. e.g. the request doesn\'t work on this page: example.com/this-page', 'secupress' ) . "\n\n" .
 	               __( 'Please let us know how we will recognize the issue or can reproduce the issue. What is supposed to happen, and what is actually happening instead?', 'secupress' );
