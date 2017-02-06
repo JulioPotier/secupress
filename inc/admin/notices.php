53e10dd43d359fb7eca68874c4c876b8bf1dc41b
Git I Hate You <git@screenfeed.fr>
2017-02-06 17:14:29 +0100

    Hotfix: for the update message under the plugin row, use the real readme.txt for the free plugin.

--- inc/admin/notices.php
+++ inc/admin/notices.php
@@ -444,8 +444,7 @@ function secupress_updates_message( $plugin_data ) {
 	if ( ! isset( $body[ $slug ] ) ) {
 
 		$urls = array(
-			'secupress'     => SECUPRESS_WEB_MAIN . '/api/plugin/readme-free.php',
-			// 'secupress'     => 'https://plugins.svn.wordpress.org/secupress/trunk/readme.txt', // Ok when on repo ////.
+			'secupress'     => 'https://plugins.svn.wordpress.org/secupress/trunk/readme.txt',
 			'secupress-pro' => SECUPRESS_WEB_MAIN . '/api/plugin/readme-pro.php',
 		);
 		$response = wp_remote_get( $urls[ $plugin_data['slug'] ] );
