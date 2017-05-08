300c0fbeacc0758ca7a19bf556ac1166aa9db3cc
Git I Hate You <git@screenfeed.fr>
2017-05-09 01:51:57 +0200

    Introduced `secupress_move_login_maybe_deny_login_redirect()`, that filters `wp_redirect()`'s location. If the redirection location is one of our "login" URLs, prevent it, it is simpler than the 2 previous filters and works for all redirections (like when someone tries to reach `wp-admin/customize.php`). Also, use a new filter `secupress.plugin.move-login.login_redirect_location` and deprecate the hook `secupress.plugin.move-login.deny_login_redirect`.

--- inc/modules/users-login/plugins/inc/php/move-login/redirections-and-dies.php
+++ inc/modules/users-login/plugins/inc/php/move-login/redirections-and-dies.php
@@ -135,97 +135,79 @@ function secupress_move_login_deny_login_access() {
 /** DO NOT REDIRECT TO THE NEW LOGIN PAGE ======================================================= */
 /** --------------------------------------------------------------------------------------------- */
 
-add_action( 'after_setup_theme', 'secupress_move_login_maybe_deny_admin_redirect', 12 );
+add_filter( 'wp_redirect', 'secupress_move_login_maybe_deny_login_redirect', 1 );
 /**
- * When a logged out user tries to access the admin area, deny access.
+ * Filters the redirect location.
+ * When a logged out user is being redirected to the new login page, deny access.
  * Does nothing if the user is logged in.
- * `admin-post.php` and `admin-ajax.php` are white listed.
  *
- * @since 1.0
+ * @since 1.3
  * @author Grégory Viguier
+ *
+ * @param (string) $location The path to redirect to.
+ *
+ * @return (string)
  */
-function secupress_move_login_maybe_deny_admin_redirect() {
-	global $pagenow;
-	// If it's not the administration area, or if it's an ajax call, no need to go further.
-	if ( ! ( is_admin() && ! ( ( defined( 'DOING_AJAX' ) && DOING_AJAX ) || ( 'admin-post.php' === $pagenow && ! empty( $_REQUEST['action'] ) ) ) ) ) {
-		return;
+function secupress_move_login_maybe_deny_login_redirect( $location ) {
+	global $is_apache;
+
+	if ( is_user_logged_in() ) {
+		return $location;
 	}
 
-	if ( is_user_admin() ) {
-		$scheme = 'logged_in';
+	$slugs  = secupress_move_login_get_slugs();
+	$wp_dir = secupress_get_wp_directory();
+
+	if ( secupress_is_subfolder_install() ) {
+		$base  = wp_parse_url( trailingslashit( secupress_get_main_url() ) );
+		$base  = ltrim( $base['path'], '/' );
+		$base .= $wp_dir ? '[_0-9a-zA-Z-]+/' : '([_0-9a-zA-Z-]+/)?';
 	} else {
-		/** This filter is documented in wp-includes/pluggable.php */
-		$scheme = apply_filters( 'auth_redirect_scheme', '' );
+		$base  = wp_parse_url( trailingslashit( get_option( 'home' ) ) );
+		$base  = ltrim( $base['path'], '/' );
+		$base .= $wp_dir ? ltrim( $wp_dir, '/' ) : '';
 	}
 
-	if ( wp_validate_auth_cookie( '', $scheme ) ) {
-		return;
+	$regex  = '^' . $base . '(' . implode( '|', $slugs ) . ')$';
+	$parsed = wp_parse_url( $location );
+	$parsed = ! empty( $parsed['path'] ) ? $parsed['path'] : '';
+	$parsed = trim( $parsed, '/' );
+
+	if ( ! preg_match( "@{$regex}@", $parsed ) ) {
+		return $location;
 	}
 
-	// Nice try. But no.
-	secupress_move_login_deny_login_redirect();
-}
-
-
-add_filter( 'register_url', 'secupress_move_login_maybe_deny_signup_redirect' );
-/**
- * When a logged out user tries to access `wp-signup.php` or `wp-register.php`, deny access.
- * Does nothing if the user is logged in.
- * Does nothing in multi-site.
- *
- * @since 1.0
- * @author Grégory Viguier
- *
- * @param (string) $url The URL.
- */
-function secupress_move_login_maybe_deny_signup_redirect( $url ) {
-	if ( empty( $_SERVER['REQUEST_URI'] ) ) {
-		return $url;
-	}
-	if ( false === strpos( $_SERVER['REQUEST_URI'], '/wp-signup.php' ) && false === strpos( $_SERVER['REQUEST_URI'], '/wp-register.php' ) ) {
-		return $url;
-	}
-	if ( is_multisite() || is_user_logged_in() ) {
-		return $url;
-	}
-
-	// Nope!
-	secupress_move_login_deny_login_redirect();
-}
-
-
-/**
- * Perform the action set for redirections to login page: die or redirect.
- *
- * @since 1.0
- * @author Grégory Viguier
- */
-function secupress_move_login_deny_login_redirect() {
+	$redirect = false;
 	/**
 	 * If you want to trigger a custom action (redirect, message, die...), add it here.
-	 * Don't forget to exit/die.
 	 *
-	 * @since 1.0
+	 * @since 1.3
 	 * @author Grégory Viguier
+	 *
+	 * @param (string|bool) $redirect A custom URL to redirect to. Default is false.
 	 */
-	do_action( 'secupress.plugin.move-login.deny_login_redirect' );
+	$redirect = apply_filters( 'secupress.plugin.move-login.login_redirect_location', $redirect );
+
+	if ( $redirect ) {
+		return esc_url_raw( $redirect );
+	}
+
+	do_action_deprecated( 'secupress.plugin.move-login.deny_login_redirect', array(), '1.3', 'secupress.plugin.move-login.login_redirect_location' );
 
 	$do = secupress_get_module_option( 'move-login_login-redirect', 'redir-login', 'users-login' );
 
 	switch ( $do ) {
 		case 'redir-login':
 			// Ok, let WordPress redirect the user to the login page.
-			return;
+			return $location;
 		case 'error':
 			wp_die( __( 'Cheatin&#8217; uh?' ), __( 'Lost?', 'secupress' ), array( 'response' => 403 ) );
 		case 'redir_404':
 			/** This filter is documented in inc/modules/users-login/plugins/inc/php/move-login/redirections-and-dies.php */
 			$redirect = apply_filters( 'sfml_404_error_page', home_url( '404' ) );
-			wp_redirect( esc_url_raw( user_trailingslashit( $redirect ) ) );
-			exit;
+			return esc_url_raw( user_trailingslashit( $redirect ) );
 		case 'redir_home':
-			wp_redirect( esc_url_raw( user_trailingslashit( home_url() ) ) );
-			exit;
+			return esc_url_raw( user_trailingslashit( home_url() ) );
 	}
 
 	die();
