69336f489a2efa274c2e998ee5e776cce3884f46
Julio Potier <juliobosk@gmail.com>
2018-04-12 00:22:02 +0200

    hotfix: stop author pages on front

--- inc/modules/users-login/plugins/stop-user-enumeration.php
+++ inc/modules/users-login/plugins/stop-user-enumeration.php
@@ -9,10 +9,38 @@
 
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
+add_action( 'template_redirect', 'secupress_set_404_for_author_pages' );
+/**
+ * Returns a 404 when an author page is loaded
+ *
+ * @since 1.0
+ * @return void
+ * @author Julio Potier
+ **/
+function secupress_set_404_for_author_pages() {
+	global $wp_query;
+	if ( is_author() ) {
+		$wp_query->set_404();
+		status_header( 404 );
+	}
+}
+
+add_filter( 'author_link', 'secupress_replace_author_link' );
+/**
+ * Replace the author links with homepage to prevent linking on 404 pages
+ *
+ * @since 1.0
+ * @return (string) home_url()
+ * @author Julio Potier
+ **/
+function secupress_replace_author_link() {
+	return home_url();
+}
+
 /**
  * Protected means that only descendants of the Code class can access that property so it must be public.
  *
- * @since 1.3
+ * @since 1.0
  * @author Julio Potier
  **/
 class Secupress_WP_REST_Users_Controller extends WP_REST_Users_Controller {
@@ -32,7 +60,7 @@ add_action( 'init', 'secupress_stop_user_enumeration_front', SECUPRESS_INT_MAX )
 /**
  * Block the author page on front
  *
- * @since 1.3
+ * @since 1.0
  * @author Julio Potier
  **/
 function secupress_stop_user_enumeration_front() {
@@ -46,7 +74,7 @@ add_filter( 'rest_request_before_callbacks', 'secupress_stop_user_enumeration_re
 /**
  * Block the author page for REST API
  *
- * @since 1.3
+ * @since 1.0
  * @author Julio Potier
  **/
 function secupress_stop_user_enumeration_rest() {
