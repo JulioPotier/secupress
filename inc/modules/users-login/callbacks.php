56eaa144e1e6bd73dc8b48ad3e269d9c295ae9e2
Julio Potier <juliobosk@gmail.com>
2018-06-14 00:25:59 +0200

    remove the condition

--- inc/modules/users-login/callbacks.php
+++ inc/modules/users-login/callbacks.php
@@ -105,10 +105,9 @@ function secupress_login_protection_settings_callback( $modulenow, &$settings, $
 	// (De)Activation.
 	if ( ! empty( $activate['login-protection_type'] ) ) {
 		$activate['login-protection_type'] = array_flip( $activate['login-protection_type'] );
-
-		secupress_manage_submodule( $modulenow, 'limitloginattempts', isset( $activate['login-protection_type']['limitloginattempts'] ) );
-		secupress_manage_submodule( $modulenow, 'bannonexistsuser',   isset( $activate['login-protection_type']['bannonexistsuser'] ) );
 	}
+	secupress_manage_submodule( $modulenow, 'limitloginattempts', isset( $activate['login-protection_type']['limitloginattempts'] ) );
+	secupress_manage_submodule( $modulenow, 'bannonexistsuser',   isset( $activate['login-protection_type']['bannonexistsuser'] ) );
 
 	// Settings.
 	$settings['login-protection_number_attempts']  = ! empty( $settings['login-protection_number_attempts'] ) ? secupress_validate_range( $settings['login-protection_number_attempts'], 3, 99, 10 ) : 10;
