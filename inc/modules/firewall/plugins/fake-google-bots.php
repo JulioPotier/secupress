5c53c5da362d8368a73d900792ab761039f1263b
Julio Potier <juliobosk@gmail.com>
2018-06-14 07:13:40 +0200

    add whitelist

--- inc/modules/firewall/plugins/fake-google-bots.php
+++ inc/modules/firewall/plugins/fake-google-bots.php
@@ -25,17 +25,29 @@ function secupress_check_fake_bot() {
 	// Is a bot if true.
 	$user_agent = isset( $_SERVER['HTTP_USER_AGENT'] ) ? trim( $_SERVER['HTTP_USER_AGENT'] ) : '';
 
-	$user_agent_regex_list = [ 'yandexbot', 'duckduckbot', 'slurp', 'baiduspider', 'facebot', 'facebook', 'ia_archiver', 'google', 'bingbot', 'msnbot' ];
+	$user_agent_regex_test_list = [ 'yandexbot', 'duckduckbot', 'slurp', 'baiduspider', 'facebot', 'facebook', 'ia_archiver', 'google', 'bingbot', 'msnbot' ];
 	/**
-	 * Filter to modify the user agents list
+	 * Filter to modify the user agents test list
 	 *
 	 * @since 1.4.3
 	 *
-	 * @param (array) $user_agent_regex_list The list to be filtered.
+	 * @param (array) $user_agent_regex_test_list The list to be filtered.
 	 */
-	$user_agent_regex_list = apply_filters( 'secupress.fake_bot_ua_list', $user_agent_regex_list );
+	$user_agent_regex_test_list = apply_filters( 'secupress.fake_bot_ua_list', $user_agent_regex_test_list );
 
-	if ( ! preg_match( '/' . implode( '|', $user_agent_regex_list ) . '/i', $user_agent ) ) {
+	$user_agent_regex_not_list = [ 'facebookexternalhit' ];
+	/**
+	 * Filter to modify the user agents not ok list
+	 *
+	 * @since 1.4.4
+	 *
+	 * @param (array) $user_agent_regex_not_list The list to be filtered.
+	 */
+	$user_agent_regex_not_list = apply_filters( 'secupress.fake_bot_ua_not_list', $user_agent_regex_test_list );
+
+	if ( ! preg_match( '/' . implode( '|', $user_agent_regex_test_list ) . '/i', $user_agent )
+	 || preg_match( '/' . implode( '|', $user_agent_regex_not_list ) . '/i', $user_agent )
+		) {
 		return;
 	}
 
