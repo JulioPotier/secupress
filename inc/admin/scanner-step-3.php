3f8cccdebd24b15db30c16a2f9fd9e309d7d36be
Julio Potier <juliobosk@gmail.com>
2018-06-16 00:34:43 +0200

    remove the link, less support.

--- inc/admin/scanner-step-3.php
+++ inc/admin/scanner-step-3.php
@@ -269,12 +269,6 @@ if ( ! $secupress_tests ) {
 									</span>
 									<span class="text"><?php _e( 'Read the documentation', 'secupress' ); ?></span>
 								</a>
-								<a href="<?php echo esc_url( secupress_admin_url( 'modules', 'services' ) . '&scanner=' . $class_name_part_lower ); ?>" class="secupress-button secupress-button-mini secupress-button-support light shadow" target="_blank" title="<?php esc_attr_e( 'Open in a new window.', 'secupress' ); ?>">
-									<span class="icon">
-										<i class="secupress-icon-ask" aria-hidden="true"></i>
-									</span>
-									<span class="text"><?php _e( 'Ask for support', 'secupress' ); ?></span>
-								</a>
 							</p>
 						<?php } ?>
 						<p class="secupress-actions">
