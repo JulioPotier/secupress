289458ef524c7a3f05bf901bf036954429420ae7
Julio Potier <juliobosk@gmail.com>
2018-06-18 14:42:55 +0200

    hotfix: hide WL settings if not pro

--- inc/modules/welcome/settings.php
+++ inc/modules/welcome/settings.php
@@ -1,7 +1,7 @@
 <?php
 defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
 
-if ( defined( 'WP_SWL' ) && WP_SWL ) {
+if ( secupress_is_pro() && defined( 'WP_SWL' ) && WP_SWL ) {
 	$this->load_plugin_settings( 'wl' );
 }
 $this->load_plugin_settings( 'api-key' );
