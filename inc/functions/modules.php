7b31ec6477fcc208e90f4ec43ff451788cbaf937
BoiteAWeb <juliobosk@gmail.com>
2019-05-01 22:19:31 +0200

    don't trugger the hook activation if not activated, you dumbass

--- inc/functions/modules.php
+++ inc/functions/modules.php
@@ -268,28 +268,26 @@ function secupress_activate_submodule( $module, $submodule, $incompatible_submod
 		require_once( $file_path );
 
 		secupress_add_module_notice( $module, $submodule, 'activation' );
-	}
 
-	/**
-	 * Fires once a sub-module is activated, even if it was already active.
-	 *
-	 * @since 1.0
-	 *
-	 * @param (bool) $is_active True if the sub-module was already active.
-	 */
-	do_action( 'secupress.modules.activate_submodule_' . $submodule, $is_active );
+		/**
+		 * Fires once a sub-module is activated, even if it was already active.
+		 *
+		 * @since 1.0
+		 *
+		 * @param (bool) $is_active True if the sub-module was already active.
+		 */
+		do_action( 'secupress.modules.activate_submodule_' . $submodule, $is_active );
 
-	/**
-	 * Fires once any sub-module is activated, even if it was already active.
-	 *
-	 * @since 1.0
-	 *
-	 * @param (string) $submodule The sub-module slug.
-	 * @param (bool)   $is_active True if the sub-module was already active.
-	 */
-	do_action( 'secupress.modules.activate_submodule', $submodule, $is_active );
+		/**
+		 * Fires once any sub-module is activated, even if it was already active.
+		 *
+		 * @since 1.0
+		 *
+		 * @param (string) $submodule The sub-module slug.
+		 * @param (bool)   $is_active True if the sub-module was already active.
+		 */
+		do_action( 'secupress.modules.activate_submodule', $submodule, $is_active );
 
-	if ( ! $is_active ) {
 		secupress_delete_site_transient( SECUPRESS_ACTIVE_SUBMODULES );
 	}
 
