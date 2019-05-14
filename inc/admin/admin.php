7cd6b15af6bf7f9ef16143abe6d3f8245ceec29e
BoiteAWeb <juliobosk@gmail.com>
2019-05-15 00:18:27 +0200

    new feat

--- inc/admin/admin.php
+++ inc/admin/admin.php
@@ -113,3 +113,159 @@ if ( secupress_is_expert_mode() ) {
 	add_filter( 'secupress.settings.help', '__return_empty_string' );
 	add_filter( 'secupress.settings.description', '__return_empty_string' );
 }
+
+
+add_action( 'load-plugins.php', 'secupress_plugins_add_update_date_hooks' );
+/**
+ * Link each plugin to the update message hook
+ *
+ * @since 1.4.9
+ * @author Julio Potier
+ *
+ * @hook load-plugins.php
+ **/
+function secupress_plugins_add_update_date_hooks() {
+	foreach ( get_plugins() as $path => $content ) {
+		add_action( 'in_plugin_update_message-' . $path, 'secupress_plugins_add_update_date', 10, 2 );
+	}
+}
+
+/**
+ * Display a spinner of the last update date
+ *
+ * @since 1.4.9
+ * @author Julio Potier
+ *
+ * @see secupress_plugins_add_update_date_hooks
+ *
+ * @param (object) $plugin_data The installed plugin
+ * @param (object) $new_plugin_data The future updated plugin
+ * @return Prints our content
+ **/
+function secupress_plugins_add_update_date( $plugin_data, $new_plugin_data ) {
+	$plugin_updated = get_site_transient( 'last_updated_plugin-' . $plugin_data['plugin'] );
+	echo '</p><p>';
+	if ( ! $plugin_updated ) {
+		printf( '<img src="%1$s" alt="%2$s" width="16" height="16" title="%2$s" />',
+			admin_url( '/images/wpspin_light.gif' ),
+			__( 'Loading&hellip;' )
+		);
+	} elseif ( isset( $plugin_updated[ $plugin_data['new_version'] ] ) ) {
+		$strtotime = strtotime( $plugin_updated[ $plugin_data['new_version'] ] );
+		$count     = count( $plugin_updated );
+		printf( __( 'Last Update v%s, it was %s ago. ' ),
+			esc_html( $plugin_data['new_version'] ),
+			human_time_diff( time(), $strtotime )
+		);
+		if ( $count > 1 ) {
+			echo '</p><p>⚠️';
+			printf( __( 'You are late of <abbr title="v%s" style="cursor:help">%d versions</abbr> since %s.' ),
+				implode( ', v', array_keys( $plugin_updated ) ),
+				$count,
+				date_i18n( get_option( 'date_format' ), $strtotime )
+			);
+		}
+	}
+}
+
+add_action( 'admin_footer-plugins.php', 'secupress_plugins_get_more_info' );
+/**
+ * If needed, do an async call to cearte our transient
+ *
+ * @since 1.4.9
+ * @author Julio Potier
+ *
+ * @see secupress_plugins_add_update_date
+ *
+ * @hook admin_footer-plugins.php
+ **/
+function secupress_plugins_get_more_info() {
+	$update_plugins = get_site_transient( 'update_plugins' );
+	if ( ! isset( $update_plugins->response ) ) {
+		return $update_plugins;
+	}
+	foreach ( $update_plugins->response as $path => $content ) {
+		$tr_temp = get_site_transient( 'last_updated_plugin-' . $path );
+		if ( isset( $tr_temp[ $update_plugins->response[ $path ]->new_version ] ) ) {
+			continue;
+		}
+		$url = admin_url( 'admin-ajax.php?path=%s&slug=%s&action=%s' );
+		$url = sprintf( $url, $path, $content->slug, __FUNCTION__ );
+		$url = wp_nonce_url( $url, $path . $content->slug . __FUNCTION__ );
+		$url = str_replace( '&amp;', '&', $url );
+		wp_remote_get( $url, [ 'blocking' => false, 'timeout' => 0.01 ] );
+	}
+}
+
+add_action( 'wp_ajax_secupress_plugins_get_more_info', 'secupress_plugins_get_more_info_ajax_cb' );
+/**
+ * Update or create ou transient to get more info on plugins rows
+ *
+ * @since 1.4.9
+ * @author Julio Potier
+ *
+ * @see secupress_plugins_get_more_info
+ *
+ * @hook wp_ajax_secupress_plugins_get_more_info
+ * @return (json) Error or Success, but only for debug because it's done async
+ **/
+function secupress_plugins_get_more_info_ajax_cb() {
+	if ( ! isset( $_GET['path'], $_GET['path'], $_GET['_wpnonce'] ) ||
+		! wp_verify_nonce( $_GET['_wpnonce'], $_GET['path'] . $_GET['slug'] . $_GET['action'] )
+	) {
+		wp_send_json_error( 'WRONG' );
+	}
+	$_CLEAN = $_GET;
+	require_once( ABSPATH . '/wp-admin/includes/plugin-install.php' );
+	$api = plugins_api(
+		'plugin_information',
+		[
+			'slug'   => $_CLEAN['slug'],
+			'fields' => [ 'last_updated' => true, 'rating' => false, 'ratings' => false, 'reviews' => false, 'banners' => false, 'icons' => false, 'active_installs' => false, 'group' => false, 'contributors' => false, 'requires' => false, 'compatibility' => false, 'screenshots' => false, 'sections' => false, 'short_description' => false, 'tested' => false, 'downloaded' => false, 'download_link' => false, 'versions' => false, 'homepage' => false, 'donate_link' => false, 'tags' => false, 'added' => false, 'template' => false, 'screenshot_url' => false ],
+		]
+	);
+	if ( is_wp_error( $api ) ) {
+		wp_send_json_error( 'API' );
+	}
+	$_transient                  = get_site_transient( 'last_updated_plugin-' . $_CLEAN['path'] );
+	$_transient                  = $_transient ?: [];
+	$_transient[ $api->version ] = $api->last_updated;
+	set_site_transient( 'last_updated_plugin-' . $_CLEAN['path'], $_transient );
+	wp_send_json_success( [ $api->version => $api->last_updated ] );
+}
+
+add_action( 'deleted_plugin', 'secupress_plugins_delete_transient_on_delete', 10, 2 );
+/**
+ * Delete the transient if the plugin is deleted
+ *
+ * @since 1.4.9
+ * @author Julio Potier
+ *
+ * @hook deleted_plugin
+ * @param (string) The path to the plugin, like "folder/file.php"
+ * @param (bool)   Tell if the plugin has been deleted
+ * @return
+ **/
+function secupress_plugins_delete_transient_on_delete( $plugin_file, $deleted ) {
+	if ( $deleted ) {
+		delete_site_transient( 'last_updated_plugin-' . $plugin_file );
+	}
+}
+
+add_action( 'upgrader_process_complete', 'secupress_plugins_delete_transient_on_update', 10, 2 );
+/**
+ * Delete the transient if the plugin is updated
+ *
+ * @since 1.4.9
+ * @author  Julio Potier
+ *
+ * @hook upgrader_process_complete
+ * @param (void) $dummy .
+ * @param (array) $options Infos on the update (theme, plugin, etc)
+ * @return
+ **/
+function secupress_plugins_delete_transient_on_update( $dummy, $options ) {
+	if ( 'plugin' === $options['type'] ) {
+		delete_site_transient( 'last_updated_plugin-' . $options['plugin'] );
+	}
+}
