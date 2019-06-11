654e4809373702a74b0ece1d7d6dcab7b8745a59
BoiteAWeb <juliobosk@gmail.com>
2019-06-11 14:33:42 +0200

    useless callback, don't remove now

--- inc/modules/logs/callbacks.php
+++ inc/modules/logs/callbacks.php
@@ -11,35 +11,6 @@ defined( 'ABSPATH' ) or die( 'Cheatin&#8217; uh?' );
  * @return (array) The sanitized and validated settings.
  */
 function secupress_logs_settings_callback( $settings ) {
-	$modulenow = 'logs';
-	$settings  = $settings && is_array( $settings ) ? $settings : array();
-
-	if ( isset( $settings['sanitized'] ) ) {
-		return $settings;
-	}
-	$settings['sanitized'] = 1;
-
-	// Sanitization.
-	if ( ! empty( $settings['banned-ips_whitelist'] ) ) {
-		$settings['banned-ips_whitelist'] = explode( "\n", $settings['banned-ips_whitelist'] );
-		$settings['banned-ips_whitelist'] = array_map( 'secupress_ip_is_valid', $settings['banned-ips_whitelist'] );
-		$settings['banned-ips_whitelist'] = array_filter( $settings['banned-ips_whitelist'] );
-		$settings['banned-ips_whitelist'] = array_map( 'sanitize_text_field', $settings['banned-ips_whitelist'] );
-
-		if ( $settings['banned-ips_whitelist'] ) {
-			// Remove whitelisted IPs from the banned IPs list.
-			$ban_ips = get_site_option( SECUPRESS_BAN_IP );
-
-			if ( $ban_ips && is_array( $ban_ips ) ) {
-				$whitelist = array_flip( $settings['banned-ips_whitelist'] );
-				$ban_ips   = array_diff_key( $ban_ips, $whitelist );
-				update_site_option( SECUPRESS_BAN_IP, $ban_ips );
-			}
-		}
-
-		$settings['banned-ips_whitelist'] = implode( "\n", $settings['banned-ips_whitelist'] );
-	}
-
 	return $settings;
 }
 
