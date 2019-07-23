2adf5241cadd603dfd9b5d61fb810f96efe722a8
BoiteAWeb <juliobosk@gmail.com>
2019-07-23 14:50:49 +0200

    Add some esc

--- inc/admin/functions/modules.php
+++ inc/admin/functions/modules.php
@@ -136,9 +136,9 @@ function secupress_plugin_in_usage_string( $plugin_basename, $settings_page = ''
 	$plugin_basename = path_join( WP_PLUGIN_DIR, $plugin_basename );
 	$plugin = get_plugin_data( $plugin_basename, false, false );
 
-	$content = sprintf( __( 'You can not use this feature now because you are using the plugin %s which is already doing this job well.', 'secupress' ), '<strong>' . $plugin['Name'] . '</strong>' );
+	$content = sprintf( __( 'You can not use this feature now because you are using the plugin %s which is already doing this job well.', 'secupress' ), '<strong>' . esc_html( $plugin['Name'] ) . '</strong>' );
 	if ( $settings_page ) {
-		$content .= sprintf( '<br><a href="%s">' . __( 'Open the %s settings page', 'secupress' ) . '.</a>', admin_url( $settings_page ), '<strong>' . $plugin['Name'] . '</strong>' );
+		$content .= sprintf( '<br><a href="%s">' . __( 'Open the %s settings page', 'secupress' ) . '.</a>', esc_url( admin_url( $settings_page ) ), '<strong>' . esc_html( $plugin['Name'] ) . '</strong>' );
 	}
 
 	return $content;
