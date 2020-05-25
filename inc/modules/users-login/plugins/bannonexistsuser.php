9629d3a43169b16229e89083a0c20eea2e654054
JulioPotier <juliobosk@gmail.com>
2020-05-25 20:58:27 +0200

    don't ban, just block

--- inc/modules/users-login/plugins/bannonexistsuser.php
+++ inc/modules/users-login/plugins/bannonexistsuser.php
@@ -4,7 +4,7 @@
  * Description: If someone tries to log in using a username that doesn'y exists on your website, he will be banned for x minutes.
  * Main Module: users_login
  * Author: SecuPress
- * Version: 1.1
+ * Version: 1.4.12
  */
 
 defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
@@ -13,6 +13,7 @@ add_action( 'authenticate', 'secupress_bannonexistsuser_auth', 100, 2 );
 /**
  * Ban users who try to log in with a non existing username.
  *
+ * @since 1.4.12 Don't ban, just block.
  * @since 1.0
  *
  * @param (null|object) $raw_user WP_User if the user is authenticated.
@@ -34,7 +35,7 @@ function secupress_bannonexistsuser_auth( $raw_user, $username ) {
 		$errors = array_flip( $errors );
 
 		if ( isset( $errors['invalid_username'] ) || isset( $errors['invalid_email'] ) ) {
-			secupress_ban_ip( (int) secupress_get_module_option( 'login-protection_time_ban', 5, 'users-login' ) );
+			secupress_block( 'NOUSER', [ 'code' => 403, 'b64' => [ 'data' => $username ] ] );
 		}
 	}
 
--- inc/modules/users-login/plugins/limitloginattempts.php
+++ inc/modules/users-login/plugins/limitloginattempts.php
@@ -4,7 +4,7 @@
  * Description: Limit login attempts and ban if too many tries have been done.
  * Main Module: users_login
  * Author: SecuPress
- * Version: 1.1
+ * Version: 1.4.12
  */
 
 defined( 'SECUPRESS_VERSION' ) or die( 'Cheatin&#8217; uh?' );
@@ -13,6 +13,7 @@ add_action( 'authenticate', 'secupress_limitloginattempts', SECUPRESS_INT_MAX, 2
 /**
  * Check the number of attemps.
  *
+ * @since 1.4.12 Don't ban, just block.
  * @since 1.0
  *
  * @param (null|object) $raw_user WP_User if the user is authenticated.
@@ -43,7 +44,7 @@ function secupress_limitloginattempts( $raw_user, $username ) {
 
 	if ( $user_attempts >= $max_attempts ) {
 		delete_user_meta( $uid, '_secupress_limitloginattempts' );
-		secupress_ban_ip( (int) secupress_get_module_option( 'login-protection_time_ban', 5, 'users-login' ) );
+		secupress_block( 'LIMITATTEMPTS', [ 'code' => 403, 'b64' => [ 'data' => [ $username, $user_attempts ] ] ] );
 	}
 
 	update_user_meta( $uid, '_secupress_limitloginattempts', $user_attempts );
